66fe7c06b5a03eabecbd46129f2889df92e63a12
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2008-05-16 04:13:51 +0000

    Modify the build script to reflect the change in file structure
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@75 d7f09016-e345-0410-b530-edf29a71df78

--- DocumentBuild.bat
+++ DocumentBuild.bat
@@ -1,2 +1,2 @@
-C:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe Emgu.CV_VS2008.sln /property:Configuration=Debug
+C:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe Solution\VS2008\Emgu.CV_VS2008.sln /property:Configuration=Debug
 "C:\Program Files\EWSoftware\Sandcastle Help File Builder\SandcastleBuilderConsole.exe" Document\Emgu.CV.shfb
--- ReleaseBuild.bat
+++ ReleaseBuild.bat
@@ -1,4 +1,4 @@
-C:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe Emgu.CV_VS2008.sln /property:Configuration=Release
+C:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe Solution\VS2008\Emgu.CV_VS2008.sln /property:Configuration=Release
 rm -f Emgu.CV.Windows.Binary.zip
 rm -rf Emgu.CV.Windows.Binary
 mkdir Emgu.CV.Windows.Binary
