7cc18d08d88069e731d9b9bfe087741f6d703ec4
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2008-05-19 15:10:33 +0000

    Update the build script such that the release zip file includes version number
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@77 d7f09016-e345-0410-b530-edf29a71df78

--- ReleaseBuild.bat
+++ ReleaseBuild.bat
@@ -1,6 +1,8 @@
+set RELEASE-NAME=Emgu.CV.Windows-1.2.2-Binary
 C:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe Solution\VS2008\Emgu.CV_VS2008.sln /property:Configuration=Release
-rm -f Emgu.CV.Windows.Binary.zip
-rm -rf Emgu.CV.Windows.Binary
-mkdir Emgu.CV.Windows.Binary
-cp Emgu.CV\README.txt Emgu.CV\Emgu.CV.License.txt Emgu.CV\bin\Release\Emgu.CV.dll Emgu.Utils\bin\Release\Emgu.Utils.dll lib\zlib.net.dll lib\zlib.net.license.txt Emgu.CV.Windows.Binary
-zip -r Emgu.CV.Windows.Binary.zip Emgu.CV.Windows.Binary 
+rm -f %RELEASE-NAME%.zip
+rm -rf %RELEASE-NAME%
+mkdir %RELEASE-NAME%
+cp Emgu.CV\README.txt Emgu.CV\Emgu.CV.License.txt Emgu.CV\bin\Release\Emgu.CV.dll Emgu.Utils\bin\Release\Emgu.Utils.dll lib\zlib.net.dll lib\zlib.net.license.txt %RELEASE-NAME%
+zip -r %RELEASE-NAME%.zip %RELEASE-NAME% 
+set RELEASE-NAME=
