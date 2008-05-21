396f1b251a43363420ca0676dabd64e4db1f08cb
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2008-05-21 04:40:19 +0000

    Update script
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@80 d7f09016-e345-0410-b530-edf29a71df78

--- ReleaseBuild.bat
+++ ReleaseBuild.bat
@@ -1,4 +1,4 @@
-set RELEASE-NAME=Emgu.CV.Windows-1.2.2-Binary
+set RELEASE-NAME=Emgu.CV.Windows-1.2.2.0-Binary
 C:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe Solution\VS2008\Emgu.CV_VS2008.sln /property:Configuration=Release
 rm -f %RELEASE-NAME%.zip
 rm -rf %RELEASE-NAME%
