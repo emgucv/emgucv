1500c61c4291257ef4cf3041507d608a29d940d8
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2008-05-24 02:30:12 +0000

    Tracker [ 1969719 ] Support for cvFloodFill, patch provided by Albert Gordo
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@90 d7f09016-e345-0410-b530-edf29a71df78

---
+++ Emgu.CV/PInvoke/CvType/MCvConnectedComp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Emgu.CV
+{
+    /// <summary>
+    /// Managed structure equivalent to CvConnectedComp
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MCvConnectedComp
+    {
+        /// <summary>
+        /// area of the segmented component
+        /// </summary>
+        public double area;
+
+        /// <summary>
+        /// gray scale value of the segmented component
+        /// </summary>
+        public float value;
+
+        /// <summary>
+        /// ROI of the segmented component
+        /// </summary>
+        public MCvRect rect;
+    }
+
+}
