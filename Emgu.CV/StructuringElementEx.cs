fb9931081a9b44f0eb4dde73cb18a7b44def8a3e
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2008-08-26 13:07:58 +0000

    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@215 d7f09016-e345-0410-b530-edf29a71df78

---
+++ Emgu.CV/StructuringElementEx.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Emgu.Util;
+using System.Diagnostics;
+
+namespace Emgu.CV
+{
+   /// <summary>
+   /// A wrapper for the CvStructuringElementEx structure in opencv
+   /// </summary>
+   public class StructuringElementEx : UnmanagedObject
+   {
+      /// <summary>
+      /// Create a custome shape Structuring Element
+      /// </summary>
+      /// <param name="values">The structuring element data, a plane array, representing row-by-row scanning of the element matrix. Non-zero values indicate points that belong to the element.</param>
+      /// <param name="anchorX">Relative horizontal offset of the anchor point</param>
+      /// <param name="anchorY">Relative vertical offset of the anchor point</param>
+      public StructuringElementEx(int[,] values, int anchorX, int anchorY)
+      {
+         _ptr = CvInvoke.cvCreateStructuringElementEx(
+                   values.GetLength(1),
+                   values.GetLength(0),
+                   anchorX,
+                   anchorY,
+                   CvEnum.CV_ELEMENT_SHAPE.CV_SHAPE_CUSTOM,
+                   values);
+      }
+
+      /// <summary>
+      /// Create a structuring element of the specific type
+      /// </summary>
+      /// <param name="cols">Number of columns in the structuring element</param>
+      /// <param name="rows">Number of rows in the structuring element</param>
+      /// <param name="anchorX">Relative horizontal offset of the anchor point</param>
+      /// <param name="anchorY">Relative vertical offset of the anchor point</param>
+      /// <param name="shape">Shape of the structuring element</param>
+      public StructuringElementEx(int cols, int rows, int anchorX, int anchorY, CvEnum.CV_ELEMENT_SHAPE shape)
+      {
+         Debug.Assert(shape != Emgu.CV.CvEnum.CV_ELEMENT_SHAPE.CV_SHAPE_CUSTOM, "For custome shape please use a different constructor");
+         _ptr = CvInvoke.cvCreateStructuringElementEx(cols, rows, anchorX, anchorY, shape, null);
+      }
+
+      /// <summary>
+      /// Release the unmanaged memory associated to this object
+      /// </summary>
+      protected override void DisposeObject()
+      {
+         CvInvoke.cvReleaseStructuringElement(ref _ptr);
+      }
+   }
+}
