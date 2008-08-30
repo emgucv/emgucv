65953d217863b1b3c98292c60de0fd5952d72165
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2008-08-30 02:55:09 +0000

    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@229 d7f09016-e345-0410-b530-edf29a71df78

--- ReleaseBuild.bat
+++ ReleaseBuild.bat
@@ -4,10 +4,10 @@ set RELEASE-ZIP=%RELEASE-NAME%.zip
 
 C:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe Solution\VS2008\Emgu.CV.sln /property:Configuration=Release
 if exist %RELEASE-FOLDER% rm -rf %RELEASE-FOLDER%
-if exist %RELEASE-NAME% rm -f %RELEASE-NAME%
+if exist %RELEASE-NAME% rm -rf %RELEASE-NAME%
 
 mkdir %RELEASE-FOLDER%
 mkdir %RELEASE-NAME%
-cp Emgu.CV\README.txt Emgu.CV\Emgu.CV.License.txt Emgu.CV\bin\Release\Emgu.CV.dll Emgu.Utils\bin\Release\Emgu.Util.dll lib\zlib.net.dll lib\zlib.net.license.txt lib\ZedGraph.dll lib\ZedGraph.license.txt %RELEASE-NAME%
+cp Emgu.CV\README.txt Emgu.CV\Emgu.CV.License.txt bin\Emgu.CV.dll bin\Emgu.Util.dll lib\zlib.net.dll lib\zlib.net.license.txt lib\ZedGraph.dll lib\ZedGraph.license.txt %RELEASE-NAME%\
 zip -r %RELEASE-FOLDER%/%RELEASE-ZIP% %RELEASE-NAME% 
 set RELEASE-NAME=
