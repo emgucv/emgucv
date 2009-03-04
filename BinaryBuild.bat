3bfaf109fdff83f7ce9d34032adb3069c3c11bfc
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2009-03-04 02:26:11 +0000

    Updated script to detect Visual Studio 2008 as well
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@402 d7f09016-e345-0410-b530-edf29a71df78

--- BinaryBuild.bat
+++ BinaryBuild.bat
@@ -2,6 +2,6 @@ REM == FORCE OVERWRITE ==
 SET COPYCMD=/Y
 
 "C:\Program Files\CMake 2.6\bin\cmake.exe" -DVARIABLE:OPENCV_ENABLE_OPENMP=ON .
-IF EXIST "C:\Program Files\Microsoft Visual Studio 8\Common7\IDE\devenv.exe" (SET DEVENV="C:\Program Files\Microsoft Visual Studio 8\Common7\IDE\devenv.exe")
+IF EXIST "C:\Program Files\Microsoft Visual Studio 8\Common7\IDE\devenv.exe" (SET DEVENV="C:\Program Files\Microsoft Visual Studio 8\Common7\IDE\devenv.exe") ELSE (IF EXIST "C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\devenv.exe" (SET DEVENV="C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\devenv.exe"))
 %DEVENV% /Build Release emgucv.sln
 xcopy bin\release\*.dll bin\
