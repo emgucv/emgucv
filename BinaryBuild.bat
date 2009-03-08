d4564892a73b50692cf1a7a54ba72ca0bf040e8b
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2009-03-08 02:26:00 +0000

    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@412 d7f09016-e345-0410-b530-edf29a71df78

--- BinaryBuild.bat
+++ BinaryBuild.bat
@@ -1,7 +1,7 @@
 REM == FORCE OVERWRITE ==
 SET COPYCMD=/Y
 
-"C:\Program Files\CMake 2.6\bin\cmake.exe" -DVARIABLE:OPENCV_ENABLE_OPENMP=ON .
+"C:\Program Files\CMake 2.6\bin\cmake.exe" -G "Visual Studio 9 2008" -D VARIABLE:OPENCV_ENABLE_OPENMP=ON .
 IF EXIST "C:\Program Files\Microsoft Visual Studio 8\Common7\IDE\devenv.exe" (SET DEVENV="C:\Program Files\Microsoft Visual Studio 8\Common7\IDE\devenv.exe") ELSE (IF EXIST "C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\devenv.exe" (SET DEVENV="C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\devenv.exe"))
 %DEVENV% /Build Release emgucv.sln
 xcopy bin\release\*.dll bin\
