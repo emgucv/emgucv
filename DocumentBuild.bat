03bf358f8f9ac67e42ca8466eeecfc15b9f30168
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2009-06-05 00:08:21 +0000

    Upgrade sandcastle document builder project
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@487 d7f09016-e345-0410-b530-edf29a71df78

--- DocumentBuild.bat
+++ DocumentBuild.bat
@@ -1,7 +1,4 @@
-call BinaryBuild.bat
-
-C:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe Solution\VS2008\Emgu.CV.sln /property:Configuration=Debug
-"C:\Program Files\EWSoftware\Sandcastle Help File Builder\SandcastleBuilderConsole.exe" Document\Emgu.CV.shfb
-find Document\Help\html -name "*.htm" -type f -exec sed -i -f C:\Users\canming\Desktop\cruisecontrol\emgucv\Cruisecontrol\adscript.txt  {} ;
+"C:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe" Solution\VS2008\Emgu.CV.sln /property:Configuration=Debug
+"C:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe" Emgu.CV.shfbproj
 zip -r Document-2.0.0.0.zip Document\Help
 mv Document-2.0.0.0.zip Document\Help\
