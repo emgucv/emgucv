2ab43f681e78dfa478b5130ed81ed02868d64688
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2009-06-11 01:51:41 +0000

    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@493 d7f09016-e345-0410-b530-edf29a71df78

--- DocumentBuild.bat
+++
@@ -1,4 +0,0 @@
-"C:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe" Solution\VS2008\Emgu.CV.sln /property:Configuration=Debug
-"C:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe" Emgu.CV.shfbproj
-zip -r Document-2.0.0.0.zip Document\Help
-mv Document-2.0.0.0.zip Document\Help\
