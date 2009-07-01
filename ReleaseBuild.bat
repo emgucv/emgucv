a2c18e55b95320a3a41ad5a93a6bc2a50f9a3ddb
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2009-07-01 23:31:05 +0000

    set absolute path for zip command
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@513 d7f09016-e345-0410-b530-edf29a71df78

--- ReleaseBuild.bat
+++ ReleaseBuild.bat
@@ -19,7 +19,7 @@ xcopy lib\3rdParty\* %RELEASE-NAME%\
 xcopy bin\*.dll %RELEASE-NAME%\ /Y
 FOR %%F IN (Emgu.CV\README.txt Emgu.CV\Emgu.CV.License.txt bin\Emgu.CV.dll.config bin\Emgu.CV.ML.XML bin\Emgu.CV.ML.dll bin\Emgu.CV.ML.dll.config bin\Emgu.CV.XML bin\Emgu.CV.UI.XML bin\Emgu.Util.dll bin\Emgu.Util.XML) DO COPY %%F %RELEASE-NAME%\
 
-zip -r %RELEASE-FOLDER%/%RELEASE-ZIP% %RELEASE-NAME% 
+c:\cygwin\bin\zip.exe -r %RELEASE-FOLDER%/%RELEASE-ZIP% %RELEASE-NAME% 
 set RELEASE-NAME=
 set RELEASE-FOLDER=
 set RELEASE-ZIP=
