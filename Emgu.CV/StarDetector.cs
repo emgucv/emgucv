253464b9277d9b5b6b2970e74bf0c39e292bdeb3
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2009-10-10 17:29:03 +0000

    Better comments for StarDetector
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@584 d7f09016-e345-0410-b530-edf29a71df78

--- Emgu.CV/StarDetector.cs
+++ Emgu.CV/StarDetector.cs
@@ -11,19 +11,27 @@ namespace Emgu.CV
    public struct StarDetector
    {
       /// <summary>
-      /// 
+      /// Maximum size of the features. The following
+      /// values of the parameter are supported:
+      /// 4, 6, 8, 11, 12, 16, 22, 23, 32, 45, 46, 64, 90, 128
       /// </summary>
       public int MaxSize;
       /// <summary>
-      /// 
+      /// Threshold for the approximated laplacian,
+      /// used to eliminate weak features. The larger it is,
+      /// the less features will be retrieved
       /// </summary>
       public int ResponseThreshold;
       /// <summary>
-      /// 
+      /// Another threshold for the laplacian to
+      /// eliminate edges.
+      /// The larger the threshold, the more points you get.
       /// </summary>
       public int LineThresholdProjected;
       /// <summary>
-      /// 
+      /// Another threshold for the feature
+      /// size to eliminate edges. 
+      /// The larger the threshold, the more points you get.
       /// </summary>
       public int LineThresholdBinarized;
       /// <summary>
