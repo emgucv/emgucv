a4148508f68b811c96dfbf64c5790e85bba4a107
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2009-11-10 02:47:18 +0000

    Update cmake file. Added key in AssemblyInfo.cs
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@629 d7f09016-e345-0410-b530-edf29a71df78

--- Emgu.CV.UI/Properties/AssemblyInfo.cs
+++ Emgu.CV.UI/Properties/AssemblyInfo.cs
@@ -33,3 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("2.1.0.0")]
 [assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyKeyFile("../Emgu.CV.snk")]
