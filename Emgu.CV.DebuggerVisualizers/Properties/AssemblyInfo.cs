f85a99d5adfff0023fb7d83d8345ca9ad2f6c8b1
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2009-11-13 01:43:45 +0000

    change support email to support@emgu.com Let DebuggerVisualizer use CommonAssemblyInfo.
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@635 d7f09016-e345-0410-b530-edf29a71df78

--- Emgu.CV.DebuggerVisualizers/Properties/AssemblyInfo.cs
+++ Emgu.CV.DebuggerVisualizers/Properties/AssemblyInfo.cs
@@ -6,13 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Emgu.CV.DebuggerVisualizers")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Emgu")]
-[assembly: AssemblyProduct("Emgu.CV.DebuggerVisualizers")]
-[assembly: AssemblyCopyright("Copyright ©  2009")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyDescription("A Debugger Visualizer for Emgu CV")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -22,14 +16,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f7f5a84a-fc6e-4a4e-b2a4-3d0f84d46464")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
