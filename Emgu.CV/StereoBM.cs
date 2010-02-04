527d94aec762570df412ed3044e1440bdf1629ef
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2010-02-04 04:22:27 +0000

    Add float depth support for StereoBM.FindStereoCorrespondence
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@711 d7f09016-e345-0410-b530-edf29a71df78

--- Emgu.CV/StereoBM.cs
+++ Emgu.CV/StereoBM.cs
@@ -34,7 +34,20 @@ namespace Emgu.CV
       /// <param name="left">The left single-channel, 8-bit image</param>
       /// <param name="right">The right image of the same size and the same type</param>
       /// <param name="disparity">The output single-channel 16-bit signed disparity map of the same size as input images. Its elements will be the computed disparities, multiplied by 16 and rounded to integer's</param>
+      /// <remarks>Invalid pixels (for which disparity can not be computed) are set to (state-&gt;minDisparity-1)*16</remarks>
       public void FindStereoCorrespondence(Image<Gray, Byte> left, Image<Gray, Byte> right, Image<Gray, Int16> disparity)
+      {
+         CvInvoke.cvFindStereoCorrespondenceBM(left, right, disparity, ref State);
+      }
+
+      /// <summary>
+      /// Computes disparity map for the input rectified stereo pair.
+      /// </summary>
+      /// <param name="left">The left single-channel, 8-bit image</param>
+      /// <param name="right">The right image of the same size and the same type</param>
+      /// <param name="disparity">The output single-channel 16-bit signed disparity map of the same size as input images. Its elements will be the computed disparities, multiplied by 16 and rounded to integer's</param>
+      /// <remarks>Invalid pixels (for which disparity can not be computed) are set to state-&gt;minDisparity - 1 </remarks>
+      public void FindStereoCorrespondence(Image<Gray, Byte> left, Image<Gray, Byte> right, Image<Gray, float> disparity)
       {
          CvInvoke.cvFindStereoCorrespondenceBM(left, right, disparity, ref State);
       }
