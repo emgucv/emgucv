066a1a549239a637fb3ad4eb7406fc96e5c4bb27
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2010-02-10 13:41:37 +0000

    Added Cuboid class
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@725 d7f09016-e345-0410-b530-edf29a71df78

---
+++ Emgu.CV/PInvoke/CvType/Cuboid.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Emgu.CV.Structure
+{
+   /// <summary>
+   /// A solid resembling a cube, with the rectangular faces not all equal; a rectangular parallelepiped.
+   /// </summary>
+   public struct Cuboid
+   {
+      /// <summary>
+      /// The corrdinate of the lower corner
+      /// </summary>
+      public MCvPoint3D64f Max;
+
+      /// <summary>
+      /// The coordinate of the upper corner
+      /// </summary>
+      public MCvPoint3D64f Min;
+
+      /// <summary>
+      /// Check if the specific point is in the Cuboid
+      /// </summary>
+      /// <param name="point">The point to be checked</param>
+      /// <returns>True if the point is in the cuboid</returns>
+      public bool PointInCuboid(MCvPoint3D64f point)
+      {
+         return point.x >= Min.x && point.y >= Min.y && point.z >= Min.z
+            && point.x <= Max.x && point.y <= Max.y && point.z <= Max.z;
+      }
+   }
+}
