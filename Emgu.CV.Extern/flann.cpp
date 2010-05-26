d53f6ea1177f4ed80e002fe7c68e82e82d746c6f
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2010-05-26 12:29:46 +0000

    renamed flan to cvflann as per opencv change
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@815 d7f09016-e345-0410-b530-edf29a71df78

--- Emgu.CV.Extern/flann.cpp
+++ Emgu.CV.Extern/flann.cpp
@@ -1,63 +1,63 @@
 #include "cvaux.h"
 
-CVAPI(int) CvHierarchicalClustering(CvMat* features, CvMat* centers, cv::flann::KMeansIndexParams* params)
+CVAPI(int) CvHierarchicalClustering(CvMat* features, CvMat* centers, cv::cvflann::KMeansIndexParams* params)
 {
    cv::Mat f = cv::cvarrToMat(features);
    cv::Mat c = cv::cvarrToMat(centers);
-   return cv::flann::hierarchicalClustering(f, c, *params);
+   return cv::cvflann::hierarchicalClustering(f, c, *params);
 }
 
-CVAPI(cv::flann::Index*) CvFlannIndexCreateKDTree(CvMat* features, int trees)
+CVAPI(cv::cvflann::Index*) CvFlannIndexCreateKDTree(CvMat* features, int trees)
 {
-   cv::flann::KDTreeIndexParams param = cv::flann::KDTreeIndexParams(trees);
+   cv::cvflann::KDTreeIndexParams param = cv::cvflann::KDTreeIndexParams(trees);
    cv::Mat f = cv::cvarrToMat(features);
-   return new cv::flann::Index(f, param);
+   return new cv::cvflann::Index(f, param);
 }
 
-CVAPI(cv::flann::Index*) CvFlannIndexCreateLinear(CvMat* features)
+CVAPI(cv::cvflann::Index*) CvFlannIndexCreateLinear(CvMat* features)
 {
-   cv::flann::LinearIndexParams param;
+   cv::cvflann::LinearIndexParams param;
    cv::Mat f = cv::cvarrToMat(features);
-   return new cv::flann::Index(f, param);
+   return new cv::cvflann::Index(f, param);
 }
 
-CVAPI(cv::flann::Index*) CvFlannIndexCreateKMeans(CvMat* features, int branching_, int iterations_, cv::flann::flann_centers_init_t centers_init_, float cb_index_)
+CVAPI(cv::cvflann::Index*) CvFlannIndexCreateKMeans(CvMat* features, int branching_, int iterations_, cv::cvflann::flann_centers_init_t centers_init_, float cb_index_)
 {
-   cv::flann::KMeansIndexParams param = cv::flann::KMeansIndexParams(branching_, iterations_, centers_init_, cb_index_);
+   cv::cvflann::KMeansIndexParams param = cv::cvflann::KMeansIndexParams(branching_, iterations_, centers_init_, cb_index_);
    cv::Mat f = cv::cvarrToMat(features);
-   return new cv::flann::Index(f, param);
+   return new cv::cvflann::Index(f, param);
 }
 
-CVAPI(cv::flann::Index*) CvFlannIndexCreateComposite(CvMat* features, int trees, int branching_, int iterations_, cv::flann::flann_centers_init_t centers_init_, float cb_index_)
+CVAPI(cv::cvflann::Index*) CvFlannIndexCreateComposite(CvMat* features, int trees, int branching_, int iterations_, cv::cvflann::flann_centers_init_t centers_init_, float cb_index_)
 {
-   cv::flann::CompositeIndexParams param = cv::flann::CompositeIndexParams(trees, branching_, iterations_, centers_init_, cb_index_);
+   cv::cvflann::CompositeIndexParams param = cv::cvflann::CompositeIndexParams(trees, branching_, iterations_, centers_init_, cb_index_);
    cv::Mat f = cv::cvarrToMat(features);
-   return new cv::flann::Index(f, param);
+   return new cv::cvflann::Index(f, param);
 }
 
-CVAPI(cv::flann::Index*) CvFlannIndexCreateAutotuned(CvMat* features, float target_precision, float build_weight, float memory_weight, float sample_fraction)
+CVAPI(cv::cvflann::Index*) CvFlannIndexCreateAutotuned(CvMat* features, float target_precision, float build_weight, float memory_weight, float sample_fraction)
 {
-	cv::flann::AutotunedIndexParams param = cv::flann::AutotunedIndexParams(target_precision, build_weight, memory_weight, sample_fraction);
+	cv::cvflann::AutotunedIndexParams param = cv::cvflann::AutotunedIndexParams(target_precision, build_weight, memory_weight, sample_fraction);
 	cv::Mat f = cv::cvarrToMat(features);
-	return new cv::flann::Index(f, param);
+	return new cv::cvflann::Index(f, param);
 }
 
-CVAPI(void) CvFlannIndexKnnSearch(cv::flann::Index* index, CvMat* queries, CvMat* indices, CvMat* dists, int knn, int checks)
+CVAPI(void) CvFlannIndexKnnSearch(cv::cvflann::Index* index, CvMat* queries, CvMat* indices, CvMat* dists, int knn, int checks)
 {
-   cv::flann::SearchParams p = cv::flann::SearchParams(checks);
+   cv::cvflann::SearchParams p = cv::cvflann::SearchParams(checks);
    cv::Mat queriesMat = cv::cvarrToMat(queries); 
    cv::Mat indicesMat = cv::cvarrToMat(indices);
    cv::Mat distsMat = cv::cvarrToMat(dists);
    index->knnSearch(queriesMat, indicesMat, distsMat, knn, p);
 }
 
-CVAPI(int) CvFlannIndexRadiusSearch(cv::flann::Index* index, CvMat* queries, CvMat* indices, CvMat* dists, float radius, int checks)
+CVAPI(int) CvFlannIndexRadiusSearch(cv::cvflann::Index* index, CvMat* queries, CvMat* indices, CvMat* dists, float radius, int checks)
 {
-   cv::flann::SearchParams p = cv::flann::SearchParams(checks);
+   cv::cvflann::SearchParams p = cv::cvflann::SearchParams(checks);
    cv::Mat queriesMat = cv::cvarrToMat(queries); 
    cv::Mat indicesMat = cv::cvarrToMat(indices);
    cv::Mat distsMat = cv::cvarrToMat(dists);
    return index->radiusSearch(queriesMat, indicesMat, distsMat, radius, p);
 }
 
-CVAPI(void) CvFlannIndexRelease(cv::flann::Index* index) { delete index; }
+CVAPI(void) CvFlannIndexRelease(cv::cvflann::Index* index) { delete index; }
