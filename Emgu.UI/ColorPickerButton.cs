514988ec02a83e8713570e122df9f642e95ffd5c
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2010-07-13 13:10:29 +0000

    Added color picker
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@835 d7f09016-e345-0410-b530-edf29a71df78

---
+++ Emgu.UI/ColorPickerButton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Emgu.UI
+{
+   public class ColorPickerButton : Button
+   {
+      public ColorPickerButton()
+         : base()
+      {
+         InitializeComponent();
+      }
+
+      private ColorDialog colorDialog1;
+
+      private void InitializeComponent()
+      {
+         this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+         this.SuspendLayout();
+         this.ResumeLayout(false);
+
+         this.Click += new EventHandler(ColorPicker_Click);
+      }
+
+      void ColorPicker_Click(object sender, EventArgs e)
+      {
+         if (colorDialog1.ShowDialog() == DialogResult.OK)
+         {
+            BackColor = colorDialog1.Color;
+         }
+      }
+   }
+}
