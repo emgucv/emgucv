dd4827571dbf5cbf08b32df8d28df024843236d0
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2010-09-07 02:35:58 +0000

    update geotiff to a more generic TiffWriter
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@874 d7f09016-e345-0410-b530-edf29a71df78

---
+++ Emgu.CV/Geotiff/TiffWriter.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using Emgu.CV.Geodetic;
+using Emgu.CV.Structure;
+using Emgu.CV;
+using Emgu.Util;
+using System.Drawing;
+using System.Diagnostics;
+
+namespace Emgu.CV.Tiff
+{
+   /// <summary>
+   /// A class that can be used for writing geotiff
+   /// </summary>
+   public class TiffWriter : UnmanagedObject
+   {
+      #region PInvoke
+      [DllImport(CvInvoke.EXTERN_LIBRARY)]
+      private extern static IntPtr tiffWriterOpen(
+         [MarshalAs(CvInvoke._stringMarshalType)]
+         string fileSpec);
+
+      [DllImport(CvInvoke.EXTERN_LIBRARY)]
+      private extern static void tiffWriterClose(ref IntPtr pTiff);
+
+      [DllImport(CvInvoke.EXTERN_LIBRARY)]
+      private extern static void tiffWriteGeoTag(IntPtr pTiff, IntPtr modelTiepoint, IntPtr ModelPixelScale);
+
+      [DllImport(CvInvoke.EXTERN_LIBRARY)]
+      private extern static void tiffWriteImage(IntPtr pTiff, IntPtr image);
+      #endregion
+
+      /// <summary>
+      /// Create a tiff writer to save an image
+      /// </summary>
+      /// <param name="fileName">The file name to be saved</param>
+      public TiffWriter(String fileName)
+      {
+         _ptr = tiffWriterOpen(fileName);
+      }
+
+      /// <summary>
+      /// 
+      /// </summary>
+      /// <typeparam name="TColor"></typeparam>
+      /// <typeparam name="TDepth"></typeparam>
+      /// <param name="image"></param>
+      public virtual void WriteImage<TColor, TDepth>(Image<TColor, TDepth> image)
+         where TColor : struct, IColor
+         where TDepth : new()
+      {
+         if (image is Image<Gray, Byte>)
+         {
+            tiffWriteImage(_ptr, image.Ptr);
+         }
+         else if (image is Image<Bgra, Byte>)
+         {
+            //swap the B and R channel since geotiff assume RGBA for 4 channels image of depth Byte
+            using (Image<Bgra, Byte> clone = (image as Image<Bgra, Byte>).Clone())
+            using (Image<Gray, Byte> b = clone[0])
+            using (Image<Gray, Byte> r = clone[2])
+            {
+               clone[2] = b;
+               clone[0] = r;
+               tiffWriteImage(_ptr, clone.Ptr);
+            }
+         }
+         else if (image is Image<Bgr, Byte>)
+         {         
+            //swap the B and R channel since geotiff assume RGB for 3 channels image of depth Byte
+            using (Image<Bgr, Byte> clone = (image as Image<Bgr, Byte>).Clone())
+            using (Image<Gray, Byte> b = clone[0])
+            using (Image<Gray, Byte> r = clone[2])
+            {
+               clone[2] = b;
+               clone[0] = r;
+               tiffWriteImage(_ptr, clone);
+            }
+         }
+         else
+         {
+            throw new NotImplementedException("The specific image type is not supported");
+         }
+      }
+
+      /// <summary>
+      /// Write the geo information into the tiff file
+      /// </summary>
+      /// <param name="originCoordinate">The coordinate of the origin. To be specific, this is the coordinate of the upper left corner of the pixel in the origin</param>
+      /// <param name="pixelResolution">The resolution of the pixel in meters</param>
+      /// <param name="imageSize">The size of the image</param>
+      public void WriteGeoTag(GeodeticCoordinate originCoordinate, Size imageSize, MCvPoint2D64f pixelResolution)
+      {
+         GeodeticCoordinate lowerRight = TransformationWGS84.NED2Geodetic(
+            new MCvPoint3D64f(pixelResolution.x * imageSize.Height, pixelResolution.y * imageSize.Width, 0.0),
+            originCoordinate);
+         MCvPoint3D64f res = new MCvPoint3D64f(
+            (lowerRight.Longitude - originCoordinate.Longitude) * (180.0 / Math.PI) / imageSize.Width,
+            (lowerRight.Latitude - originCoordinate.Latitude) * (180.0 / Math.PI) / imageSize.Height,
+            0.0);
+
+         double latitude = lowerRight.Latitude * (180.0 / Math.PI);
+         double longitude = lowerRight.Longitude * (180.0 / Math.PI);
+         double[] ModelTiepoint = { 
+            0, 0, 0, 
+            longitude, latitude, 0 };
+         double[] ModelPixelScale = { pixelResolution.x, pixelResolution.y, 0.0 };
+      }
+
+      /// <summary>
+      /// Write the geo information into the tiff file
+      /// </summary>
+      /// <param name="modelTiepoint"></param>
+      /// <param name="modelPixelScale"></param>
+      public void WriteGeoTag(double[] modelTiepoint, double[] modelPixelScale)
+      {
+         Debug.Assert(modelTiepoint.Length == 6, "Model Tiepoint should have length of 6");
+         Debug.Assert(modelPixelScale.Length == 3, "Model Pixel Scale should have length of 3");
+
+         GCHandle tiepointHandle = GCHandle.Alloc(modelTiepoint, GCHandleType.Pinned);
+         GCHandle pixelScaleHandle = GCHandle.Alloc(modelPixelScale, GCHandleType.Pinned);
+
+         tiffWriteGeoTag(_ptr, tiepointHandle.AddrOfPinnedObject(), pixelScaleHandle.AddrOfPinnedObject());
+
+         tiepointHandle.Free();
+         pixelScaleHandle.Free();
+      }
+
+      /// <summary>
+      /// Release the writer and write all data on to disk.
+      /// </summary>
+      protected override void DisposeObject()
+      {
+         tiffWriterClose(ref _ptr);
+      }
+   }
+}
---
+++ Emgu.CV/Geotiff/TileTiffWriter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using Emgu.CV.Structure;
+
+namespace Emgu.CV.Tiff
+{
+   public class TileTiffWriter : TiffWriter
+   {
+      #region PInvoke
+      [DllImport(CvInvoke.EXTERN_LIBRARY)]
+      private extern static void tiffWriteTileInfo(IntPtr pTiff, Size tileSize);
+      #endregion
+
+      public TileTiffWriter(String fileName, Size tileSize)
+         : base(fileName)
+      {
+         tiffWriteTileInfo(_ptr, tileSize);
+      }
+
+      public override void WriteImage<TColor, TDepth>(Image<TColor, TDepth> image)
+      {
+         throw new NotImplementedException("TODO:// implement this");
+      }
+   }
+}
