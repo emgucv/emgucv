d72e7ac2a0a226cbdfacd4c58e8f3c7b9f4ae995
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2010-10-07 02:54:27 +0000

    Better documentation for generic EventArgs class
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@905 d7f09016-e345-0410-b530-edf29a71df78

--- Emgu.Util/EventArgs.cs
+++ Emgu.Util/EventArgs.cs
@@ -12,12 +12,19 @@ namespace Emgu.Util
    {
       private T _value;
 
+      /// <summary>
+      /// Create a generic EventArgs with the specific value
+      /// </summary>
+      /// <param name="value">The value</param>
       public EventArgs(T value)
          : base()
       {
          _value = value;
       }
 
+      /// <summary>
+      /// The value of the EventArgs
+      /// </summary>
       public T Value
       {
          get { return _value; }
@@ -27,12 +34,18 @@ namespace Emgu.Util
    /// <summary>
    /// A generic EventArgs
    /// </summary>
-   /// <typeparam name="T">The type of arguments</typeparam>
+   /// <typeparam name="T1">The type of the first value</typeparam>
+   /// <typeparam name="T2">The type of the second value</typeparam>
    public class EventArgs<T1, T2> : EventArgs
    {
       private T1 _value1;
       private T2 _value2;
 
+      /// <summary>
+      /// Create a generic EventArgs with two values
+      /// </summary>
+      /// <param name="value1">The first value</param>
+      /// <param name="value2">The second value</param>
       public EventArgs(T1 value1, T2 value2)
          : base()
       {
@@ -40,11 +53,18 @@ namespace Emgu.Util
          _value2 = value2;
       }
 
+      /// <summary>
+      /// The first value
+      /// </summary>
       public T1 Value1
       {
          get { return _value1; }
       }
 
+
+      /// <summary>
+      /// The second value
+      /// </summary>
       public T2 Value2
       {
          get { return _value2; }
