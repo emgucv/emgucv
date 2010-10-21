277517b20c5b3b0e1f5a8559e5ef82f6db404b85
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2010-10-21 02:55:39 +0000

    Fixed a bug in compiling cvextern_test when EMGU_ENABLE_SSE is not defined.
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@931 d7f09016-e345-0410-b530-edf29a71df78

--- tests/cvextern_test/cvextern_test.cpp
+++ tests/cvextern_test/cvextern_test.cpp
@@ -1,19 +1,19 @@
 #include "sse.h"
 #include "doubleOps.h"
 #include "stdio.h"
-
 #include <iostream>
 using namespace std;
 
-#if EMGU_SSE2
 void Test_cross_product()
 {
+#if EMGU_SSE2
    __m128d v0 = _mm_set_pd(0.01, 0.02);
    __m128d v1 = _mm_set_pd(0.03, 0.04);
 
    double val0 = _cross_product(v0, v1);
    double val1 = v0.m128d_f64[1] * v1.m128d_f64[0] - v0.m128d_f64[0] * v1.m128d_f64[1];
    cout <<"Test cross product: " << (val0 == val1 ? "Passed" : "Failed") << std::endl;
+#endif
 } 
 
 void Test_double_MulS()
@@ -40,6 +40,7 @@ void Test_double_MulS()
 
    cout <<"Test mulS: " << (success ? "Passed" : "Failed") << std::endl;
 }
+
 int main()
 {
    char tmp;
@@ -51,4 +52,4 @@ int main()
 #endif
 }
 
-#endif
+
