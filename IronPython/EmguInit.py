4312515573fc39924315a3115d9470f71a210010
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2010-10-29 03:29:13 +0000

    updated the startup script for iron python
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@946 d7f09016-e345-0410-b530-edf29a71df78

--- IronPython/EmguInit.py
+++ IronPython/EmguInit.py
@@ -1,6 +1,9 @@
 import clr
+clr.AddReferenceToFile("Emgu.Util.dll")
 clr.AddReferenceToFile("Emgu.CV.dll")
+clr.AddReferenceToFile("Emgu.CV.ML.dll")
 clr.AddReferenceToFile("Emgu.CV.UI.dll")
 from Emgu.CV import *
 from Emgu.CV.UI import *
+from Emgu.CV.Structure import *
 from System import *
