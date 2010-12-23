715d273b477a8df8f785047fac139d5c25d68ae7
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2010-12-23 02:43:33 +0000

    Modified Emgu.CV.GPU and Emgu.CV.GPU.Test to use CommonAssemblyInfo.cs
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@1025 d7f09016-e345-0410-b530-edf29a71df78

--- Emgu.CV.GPU/Properties/AssemblyInfo.cs
+++ Emgu.CV.GPU/Properties/AssemblyInfo.cs
@@ -7,12 +7,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("Emgu.CV.GPU")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Emgu.CV.GPU")]
-[assembly: AssemblyCopyright("Copyright ©  2010")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -22,15 +16,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e387390b-6606-4c9b-9a79-0ef7ebefa439")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
