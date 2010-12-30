208dbd284bca7d1e9ba41f95fb6ddc117f230907
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2010-12-30 18:47:18 +0000

    Added the missing GpuDevice.cs file
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@1033 d7f09016-e345-0410-b530-edf29a71df78

---
+++ Emgu.CV.GPU/GpuDevice.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Emgu.CV.GPU
+{
+   /// <summary>
+   /// The Gpu device information
+   /// </summary>
+   public class GpuDevice
+   {
+      /// <summary>
+      /// Query the information of the gpu device with the specific id.
+      /// </summary>
+      /// <param name="deviceId">The device id</param>
+      public GpuDevice(int deviceId)
+      {
+         ID = deviceId;
+         Name = GpuInvoke.GetDeviceName(deviceId);
+         GpuInvoke.GetComputeCapability(deviceId, ref ComputeCapability.Major, ref ComputeCapability.Minor);
+         NumberOfSMs = GpuInvoke.GetNumberOfSMs(deviceId);
+      }
+
+      /// <summary>
+      /// The id of the device
+      /// </summary>
+      public int ID;
+      /// <summary>
+      /// The name of the device
+      /// </summary>
+      public String Name;
+      /// <summary>
+      /// The compute capability
+      /// </summary>
+      public ComputeCapability ComputeCapability;
+
+      /// <summary>
+      /// The number of single multi processors
+      /// </summary>
+      public int NumberOfSMs;
+   }
+
+   /// <summary>
+   /// The compute capability
+   /// </summary>
+   public struct ComputeCapability
+   {
+      /// <summary>
+      /// The major version
+      /// </summary>
+      public int Major;
+      /// <summary>
+      /// The minor version
+      /// </summary>
+      public int Minor;
+   }
+}
