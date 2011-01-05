7c9120f3d03a1fc9e39b9051c09565182b7331b3
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2011-01-05 02:41:37 +0000

    Added GpuStereoBM class
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@1056 d7f09016-e345-0410-b530-edf29a71df78

---
+++ Emgu.CV.Extern/gpuStereo.cpp
@@ -0,0 +1,16 @@
+#include "opencv2/gpu/gpu.hpp"
+
+CVAPI(cv::gpu::StereoBM_GPU*) GpuStereoBMCreate(int preset, int ndisparities, int winSize)
+{
+   return new cv::gpu::StereoBM_GPU(preset, ndisparities, winSize);
+}
+
+CVAPI(void) GpuStereoBMFindStereoCorrespondence(cv::gpu::StereoBM_GPU* stereoBM, const cv::gpu::GpuMat* left, const cv::gpu::GpuMat* right, cv::gpu::GpuMat* disparity)
+{
+   (*stereoBM)(*left, *right, *disparity);
+}
+
+CVAPI(void) GpuStereoBMRelease(cv::gpu::StereoBM_GPU** stereoBM)
+{
+   delete *stereoBM;
+}
---
+++ Emgu.CV.GPU/GpuStereoBM.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using System.Runtime.InteropServices;
+
+namespace Emgu.CV.GPU
+{
+   /// <summary>
+   /// Use Block Matching algorithm to find stereo correspondence
+   /// </summary>
+   public class GpuStereoBM : UnmanagedObject
+   {
+      #region
+      [DllImport(CvInvoke.EXTERN_LIBRARY, CallingConvention = CvInvoke.CvCallingConvention)]
+      private static extern IntPtr GpuStereoBMCreate(PresetType preset, int ndisparities, int winSize);
+
+      [DllImport(CvInvoke.EXTERN_LIBRARY, CallingConvention = CvInvoke.CvCallingConvention)]
+      private static extern void GpuStereoBMFindStereoCorrespondence(IntPtr stereoBM, IntPtr left, IntPtr right, IntPtr disparity);
+
+      [DllImport(CvInvoke.EXTERN_LIBRARY, CallingConvention = CvInvoke.CvCallingConvention)]
+      private static extern void GpuStereoBMRelease(ref IntPtr stereoBM);
+      #endregion
+
+      /// <summary>
+      /// Preset type
+      /// </summary>
+      public enum PresetType 
+      { 
+         /// <summary>
+         /// Basic
+         /// </summary>
+         BasicPreset = 0, 
+         /// <summary>
+         /// prefilter xsobel
+         /// </summary>
+         PrefilterXSobel = 1 
+      };
+
+      /// <summary>
+      /// Create a stereoBM 
+      /// </summary>
+      /// <param name="preset">Preset type</param>
+      /// <param name="numberOfDisparities">The number of disparities. Must be multiple of 8. Use 64 for default </param>
+      /// <param name="winSize">The SAD window size</param>
+      public GpuStereoBM(PresetType preset, int numberOfDisparities, int winSize)
+      {
+         _ptr = GpuStereoBMCreate(preset, numberOfDisparities, winSize);
+      }
+
+      /// <summary>
+      /// Computes disparity map for the input rectified stereo pair.
+      /// </summary>
+      /// <param name="left">The left single-channel, 8-bit image</param>
+      /// <param name="right">The right image of the same size and the same type</param>
+      /// <param name="disparity">The disparity map</param>
+      public void FindStereoCorrespondence(GpuImage<Gray, Byte> left, GpuImage<Gray, Byte> right, GpuImage<Gray, Byte> disparity)
+      {
+         GpuStereoBMFindStereoCorrespondence(_ptr, left, right, disparity);
+      }
+
+      /// <summary>
+      /// Release the stereo state and all the memory associate with it
+      /// </summary>
+      protected override void DisposeObject()
+      {
+         GpuStereoBMRelease(ref _ptr);
+      }
+   }
+}
