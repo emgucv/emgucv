7bd47ab96025480b6d29de3b5ab240cab0fdb868
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2011-01-06 03:29:30 +0000

    Added GpuStereoConstanSpaceBP
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@1065 d7f09016-e345-0410-b530-edf29a71df78

--- Emgu.CV.Extern/gpuStereo.cpp
+++ Emgu.CV.Extern/gpuStereo.cpp
@@ -5,15 +5,33 @@ CVAPI(cv::gpu::StereoBM_GPU*) GpuStereoBMCreate(int preset, int ndisparities, in
    return new cv::gpu::StereoBM_GPU(preset, ndisparities, winSize);
 }
 
-CVAPI(void) GpuStereoBMFindStereoCorrespondence(cv::gpu::StereoBM_GPU* stereoBM, const cv::gpu::GpuMat* left, const cv::gpu::GpuMat* right, cv::gpu::GpuMat* disparity, cv::gpu::Stream* stream)
+CVAPI(void) GpuStereoBMFindStereoCorrespondence(cv::gpu::StereoBM_GPU* stereo, const cv::gpu::GpuMat* left, const cv::gpu::GpuMat* right, cv::gpu::GpuMat* disparity, cv::gpu::Stream* stream)
 {
    if (stream)
-      (*stereoBM)(*left, *right, *disparity, *stream);
+      (*stereo)(*left, *right, *disparity, *stream);
    else
-      (*stereoBM)(*left, *right, *disparity);
+      (*stereo)(*left, *right, *disparity);
 }
 
 CVAPI(void) GpuStereoBMRelease(cv::gpu::StereoBM_GPU** stereoBM)
+{
+   delete *stereoBM;
+}
+
+CVAPI(cv::gpu::StereoConstantSpaceBP*) GpuStereoConstantSpaceBPCreate(int ndisp, int iters, int levels, int nr_plane)
+{
+   return new cv::gpu::StereoConstantSpaceBP(ndisp, iters, levels, nr_plane, CV_32F);
+}
+
+CVAPI(void) GpuStereoConstantSpaceBPFindStereoCorrespondence(cv::gpu::StereoConstantSpaceBP* stereo, const cv::gpu::GpuMat* left, const cv::gpu::GpuMat* right, cv::gpu::GpuMat* disparity, cv::gpu::Stream* stream)
+{
+   if (stream)
+      (*stereo)(*left, *right, *disparity, *stream);
+   else
+      (*stereo)(*left, *right, *disparity);
+}
+
+CVAPI(void) GpuStereoConstantSpaceBPRelease(cv::gpu::StereoConstantSpaceBP** stereoBM)
 {
    delete *stereoBM;
 }
---
+++ Emgu.CV.GPU/GpuStereoConstantSpaceBP.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using System.Runtime.InteropServices;
+
+namespace Emgu.CV.GPU
+{
+   /// <summary>
+   /// A Constant-Space Belief Propagation Algorithm for Stereo Matching.
+   /// Qingxiong Yang, Liang Wang, Narendra Ahuja.
+   /// http://vision.ai.uiuc.edu/~qyang6/
+   /// </summary>
+   public class GpuStereoConstantSpaceBP : UnmanagedObject
+   {
+      #region
+      [DllImport(CvInvoke.EXTERN_LIBRARY, CallingConvention = CvInvoke.CvCallingConvention)]
+      private static extern IntPtr GpuStereoConstantSpaceBPCreate(int ndisp, int iters, int levels, int nr_plane);
+
+      [DllImport(CvInvoke.EXTERN_LIBRARY, CallingConvention = CvInvoke.CvCallingConvention)]
+      private static extern void GpuStereoConstantSpaceBPFindStereoCorrespondence(IntPtr stereoBM, IntPtr left, IntPtr right, IntPtr disparity, IntPtr stream);
+
+      [DllImport(CvInvoke.EXTERN_LIBRARY, CallingConvention = CvInvoke.CvCallingConvention)]
+      private static extern void GpuStereoConstantSpaceBPRelease(ref IntPtr stereoBM);
+      #endregion
+
+      /// <summary>
+      /// A Constant-Space Belief Propagation Algorithm for Stereo Matching
+      /// </summary>
+      /// <param name="ndisp">The number of disparities. Use 128 as default</param>
+      /// <param name="iters">The number of BP iterations on each level. Use 8 as default.</param>
+      /// <param name="levels">The number of levels. Use 4 as default</param>
+      /// <param name="nrPlane">The number of active disparity on the first level. Use 4 as default.</param>
+      public GpuStereoConstantSpaceBP(int ndisp, int iters, int levels, int nrPlane)
+      {
+         _ptr = GpuStereoConstantSpaceBPCreate(ndisp, iters, levels, nrPlane);
+      }
+
+      /// <summary>
+      /// Computes disparity map for the input rectified stereo pair.
+      /// </summary>
+      /// <param name="left">The left single-channel, 8-bit image</param>
+      /// <param name="right">The right image of the same size and the same type</param>
+      /// <param name="disparity">The disparity map</param>
+      /// <param name="stream">Use a Stream to call the function asynchronously (non-blocking) or null to call the function synchronously (blocking).</param>
+      public void FindStereoCorrespondence(GpuImage<Gray, Byte> left, GpuImage<Gray, Byte> right, GpuImage<Gray, Byte> disparity, Stream stream)
+      {
+         GpuStereoConstantSpaceBPFindStereoCorrespondence(_ptr, left, right, disparity, stream);
+      }
+
+      /// <summary>
+      /// Release the unmanaged memory
+      /// </summary>
+      protected override void DisposeObject()
+      {
+         GpuStereoConstantSpaceBPRelease(ref _ptr);
+      }
+   }
+}
