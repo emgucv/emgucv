56e660ea4f39faefab69e527f459c5f6e7d555bf
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2011-01-12 05:35:06 +0000

    minor build script improvements.
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@1099 d7f09016-e345-0410-b530-edf29a71df78

--- miscellaneous/build_packages.bat
+++ miscellaneous/build_packages.bat
@@ -6,14 +6,14 @@ svn revert Emgu.CV.License.txt
 call perl miscellaneous\svn-clean.pl .
 REM build 32 bit package with gpu using visual studio, with document
 call Build_Binary_x86.bat 32 gpu vc doc package
-mv emgucv*.zip ..\release
-mv emgucv*.exe ..\release
+mv libemgucv*.zip ..\release
+mv libemgucv*.exe ..\release
 
 call perl miscellaneous\svn-clean.pl .
 REM build 32 bit package with gpu using visual studio, with document
 call Build_Binary_x86.bat 32 nogpu vc doc package
-mv emgucv*.zip ..\release
-mv emgucv*.exe ..\release
+mv libemgucv*.zip ..\release
+mv libemgucv*.exe ..\release
 
 REM download commercial license
 REM wget http://www.emgu.com/wiki/files/CommercialLicense.txt
