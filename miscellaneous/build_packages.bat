ac8a7c65fc2195bfa5eafe004b7dae094b973a6a
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2011-01-13 03:40:06 +0000

    build script improvements.
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@1102 d7f09016-e345-0410-b530-edf29a71df78

--- miscellaneous/build_packages.bat
+++ miscellaneous/build_packages.bat
@@ -3,6 +3,7 @@ rm -rf ..\release
 mkdir ..\release
 
 svn revert Emgu.CV.License.txt
+
 call perl miscellaneous\svn-clean.pl .
 REM build 32 bit package with gpu using visual studio, with document
 call Build_Binary_x86.bat 32 gpu vc doc package
@@ -10,11 +11,47 @@ mv libemgucv*.zip ..\release
 mv libemgucv*.exe ..\release
 
 call perl miscellaneous\svn-clean.pl .
-REM build 32 bit package with gpu using visual studio, with document
+REM build 32 bit package without gpu using visual studio, with document
 call Build_Binary_x86.bat 32 nogpu vc doc package
 mv libemgucv*.zip ..\release
 mv libemgucv*.exe ..\release
 
+call perl miscellaneous\svn-clean.pl .
+REM build 64 bit package with gpu using visual studio, with document
+call Build_Binary_x86.bat 64 gpu vc doc package
+mv libemgucv*.zip ..\release
+mv libemgucv*.exe ..\release
+
+call perl miscellaneous\svn-clean.pl .
+REM build 64 bit package without gpu using visual studio, with document
+call Build_Binary_x86.bat 64 nogpu vc doc package
+mv libemgucv*.zip ..\release
+mv libemgucv*.exe ..\release
+
 REM download commercial license
-REM wget http://www.emgu.com/wiki/files/CommercialLicense.txt
-REM cp CommercialLicense.txt Emgu.CV.License.txt
+wget http://www.emgu.com/wiki/files/CommercialLicense.txt
+cp CommercialLicense.txt Emgu.CV.License.txt
+
+call perl miscellaneous\svn-clean.pl .
+REM build 32 bit package with gpu using intel compiler, with document
+call Build_Binary_x86.bat 32 gpu intel doc package
+mv libemgucv*.zip ..\release
+mv libemgucv*.exe ..\release
+
+call perl miscellaneous\svn-clean.pl .
+REM build 32 bit package without gpu using intel compiler, with document
+call Build_Binary_x86.bat 32 nogpu intel doc package
+mv libemgucv*.zip ..\release
+mv libemgucv*.exe ..\release
+
+call perl miscellaneous\svn-clean.pl .
+REM build 64 bit package with gpu using intel compiler, with document
+call Build_Binary_x86.bat 64 gpu intel doc package
+mv libemgucv*.zip ..\release
+mv libemgucv*.exe ..\release
+
+call perl miscellaneous\svn-clean.pl .
+REM build 64 bit package without gpu using intel compiler, with document
+call Build_Binary_x86.bat 64 nogpu intel doc package
+mv libemgucv*.zip ..\release
+mv libemgucv*.exe ..\release
