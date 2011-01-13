539a28d367b27571fe7a9e252ce16aaaa72b9687
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2011-01-13 03:41:37 +0000

    Added Pedestrian Detection example.
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@1105 d7f09016-e345-0410-b530-edf29a71df78

---
+++ Emgu.CV.Example/PedestrianDetection/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using Emgu.CV;
+using Emgu.CV.GPU;
+using System.Drawing;
+using System.Diagnostics;
+
+namespace PedestrianDetection
+{
+   static class Program
+   {
+      /// <summary>
+      /// The main entry point for the application.
+      /// </summary>
+      [STAThread]
+      static void Main()
+      {
+         Application.EnableVisualStyles();
+         Application.SetCompatibleTextRenderingDefault(false);
+         Run();
+      }
+
+      static void Run()
+      {
+         Image<Bgr, Byte> image = new Image<Bgr, byte>("pedestrian.png");
+
+         Stopwatch watch = Stopwatch.StartNew();
+         Rectangle[] regions;
+         if (GpuInvoke.HasCuda)
+         {
+            using (GpuHOGDescriptor des = new GpuHOGDescriptor())
+            using (GpuImage<Bgr, Byte> gpuImg = new GpuImage<Bgr,byte>(image))
+            using (GpuImage<Bgra, Byte> gpuBgra = gpuImg.Convert<Bgra, Byte>())
+            {
+               des.SetSVMDetector(GpuHOGDescriptor.GetDefaultPeopleDetector());
+               regions = des.DetectMultiScale(gpuBgra);
+            }
+         }
+         else
+         {
+            using (HOGDescriptor des = new HOGDescriptor())
+            {
+               des.SetSVMDetector(HOGDescriptor.GetDefaultPeopleDetector());
+               regions = des.DetectMultiScale(image);
+            }
+         }
+         watch.Stop();
+
+         foreach (Rectangle pedestrain in regions)
+         {
+            image.Draw(pedestrain, new Bgr(Color.Red), 1);
+         }
+
+         ImageViewer.Show(
+            image,
+            String.Format("Pedestrain detection using {0} in {1} milliseconds.", 
+               GpuInvoke.HasCuda ? "GPU" : "CPU", 
+               watch.ElapsedMilliseconds));
+      }
+   }
+}
