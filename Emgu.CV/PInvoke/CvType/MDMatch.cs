d875cda9393a828c61b6051cb4da55fd5aa5021d
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2011-02-05 05:24:47 +0000

    Added MDMatch type.
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@1181 d7f09016-e345-0410-b530-edf29a71df78

---
+++ Emgu.CV/PInvoke/CvType/MDMatch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace Emgu.CV.PInvoke.CvType
+{
+   /// <summary>
+   /// OpenCV's DMatch structure
+   /// </summary>
+   public struct MDMatch
+   {
+      /// <summary>
+      /// Query descriptor index
+      /// </summary>
+      public int QueryIdx;
+
+      /// <summary>
+      /// Train descriptor index
+      /// </summary>
+      public int TrainIdx;
+
+      /// <summary>
+      /// Train image index
+      /// </summary>
+      public int ImgIdx;
+
+      /// <summary>
+      /// Distance
+      /// </summary>
+      public float distance;
+   }
+}
