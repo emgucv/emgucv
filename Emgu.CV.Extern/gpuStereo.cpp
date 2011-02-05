5949de3736b00b405301f3f3ca4f59ac3ac856e9
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2011-02-05 05:19:49 +0000

    Added GpuDisparityBilateralFilter
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@1173 d7f09016-e345-0410-b530-edf29a71df78

--- Emgu.CV.Extern/gpuStereo.cpp
+++ Emgu.CV.Extern/gpuStereo.cpp
@@ -35,3 +35,21 @@ CVAPI(void) GpuStereoConstantSpaceBPRelease(cv::gpu::StereoConstantSpaceBP** ste
 {
    delete *stereoBM;
 }
+
+CVAPI(cv::gpu::DisparityBilateralFilter*) GpuDisparityBilateralFilterCreate(int ndisp, int radius, int iters, float edge_threshold, float max_disc_threshold, float sigma_range)
+{
+   return new cv::gpu::DisparityBilateralFilter(ndisp, radius, iters, edge_threshold, max_disc_threshold, sigma_range);
+}
+
+CVAPI(void) GpuDisparityBilateralFilterApply(cv::gpu::DisparityBilateralFilter* filter, const cv::gpu::GpuMat* disparity, const cv::gpu::GpuMat* image, cv::gpu::GpuMat* dst, cv::gpu::Stream* stream)
+{
+   if (stream)
+      (*filter)(*disparity, *image, *dst, *stream);
+   else
+      (*filter)(*disparity, *image, *dst);
+}
+
+CVAPI(void) GpuDisparityBilateralFilterRelease(cv::gpu::DisparityBilateralFilter** filter)
+{
+   delete *filter;
+}
