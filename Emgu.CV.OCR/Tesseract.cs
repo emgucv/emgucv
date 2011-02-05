95e9b4929e95502d7eaa044d4b9d9069c5fe3abd
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2011-02-05 05:22:32 +0000

    Use UTF-8 to decode the recognized text
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@1178 d7f09016-e345-0410-b530-edf29a71df78

--- Emgu.CV.OCR/Tesseract.cs
+++ Emgu.CV.OCR/Tesseract.cs
@@ -27,18 +27,25 @@ namespace Emgu.CV.OCR
       [DllImport(CvInvoke.EXTERN_LIBRARY, CallingConvention = CvInvoke.CvCallingConvention)]
       private static extern void TessBaseAPIGetUTF8Text(
          IntPtr ocr,
-         [MarshalAs(UnmanagedType.LPStr)]
-         StringBuilder text, 
+         IntPtr text,
          int maxSizeInBytes);
       #endregion
 
+      private Byte[] _utf8Buffer;
+      private UTF8Encoding _utf8 = new UTF8Encoding();
+
       public Tesseract(String dataPath, String language)
       {
+         _utf8Buffer = new Byte[2048];
+
          int initResult = 0;
          _ptr = TessBaseAPICreate(dataPath, language, ref initResult);
          if (initResult != 0) throw new ArgumentException(String.Format("Unable to create ocr model using Path {0} and language {1}.", dataPath, language));
       }
 
+      /// <summary>
+      /// Release the unmanaged resource associated with this class
+      /// </summary>
       protected override void DisposeObject()
       {
          TessBaseAPIRelease(ref _ptr);
@@ -51,10 +58,11 @@ namespace Emgu.CV.OCR
 
       public string GetText()
       {
-         StringBuilder buffer = new StringBuilder(2048);
-         TessBaseAPIGetUTF8Text(_ptr, buffer, buffer.Capacity);
-         return buffer.ToString();
-      }
+         GCHandle handle = GCHandle.Alloc(_utf8Buffer, GCHandleType.Pinned);
+         TessBaseAPIGetUTF8Text(_ptr, handle.AddrOfPinnedObject(), _utf8Buffer.Length);
+         handle.Free();
 
+         return _utf8.GetString(_utf8Buffer);
+      }
    }
 }
