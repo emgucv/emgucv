e98ec19bcc0bd0f5f80299bcdf28cd3ece528810
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2011-03-15 03:15:55 +0000

    Added comments
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@1228 d7f09016-e345-0410-b530-edf29a71df78

--- Emgu.CV.GPU/GpuBruteForceMatcher.cs
+++ Emgu.CV.GPU/GpuBruteForceMatcher.cs
@@ -6,6 +6,9 @@ using System.Runtime.InteropServices;
 
 namespace Emgu.CV.GPU
 {
+   /// <summary>
+   /// A Brute force matcher using GPU
+   /// </summary>
    public class GpuBruteForceMatcher : UnmanagedObject
    {
       #region PInvoke
@@ -28,20 +31,42 @@ namespace Emgu.CV.GPU
       /// </summary>
       public enum DistanceType
       {
+         /// <summary>
+         /// Sum of absolute difference
+         /// </summary>
          L1 = 0,
+         /// <summary>
+         /// Euclidean distance
+         /// </summary>
          L2
       }
 
+      /// <summary>
+      /// Create a GPUBruteForce Matcher using the specific distance type
+      /// </summary>
+      /// <param name="distType">The distance type</param>
       public GpuBruteForceMatcher(DistanceType distType)
       {
          _ptr = gpuBruteForceMatcherCreate(distType);
       }
 
+      /// <summary>
+      /// Find the k nearest neighbour using the brute force matcher
+      /// </summary>
+      /// <param name="queryDescriptors">The query descriptors</param>
+      /// <param name="modelDescriptors">The model descriptors</param>
+      /// <param name="modelIdx">The model index. A n x <paramref name="k"/> matrix where n = <paramref name="queryDescriptors"/>.Cols</param>
+      /// <param name="distance">The matrix where the distance valus is stored. A n x <paramref name="k"/> matrix where n = <paramref name="queryDescriptors"/>.Size.Height</param>
+      /// <param name="k">The number of nearest neighbours to be searched</param>
+      /// <param name="mask">The mask</param>
       public void KnnMatch(GpuMat<float> queryDescriptors, GpuMat<float> modelDescriptors, GpuMat<int> modelIdx, GpuMat<float> distance, int k, GpuMat<Byte> mask)
       {
          gpuBruteForceMatcherKnnMatch(_ptr, queryDescriptors, modelDescriptors, modelIdx, distance, k, mask);
       }
 
+      /// <summary>
+      /// Release all the unmanaged memory associated with this matcher
+      /// </summary>
       protected override void DisposeObject()
       {
          gpuBruteForceMatcherRelease(ref _ptr);
