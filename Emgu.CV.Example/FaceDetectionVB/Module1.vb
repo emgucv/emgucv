2801868edbb7701f2bd45189fd30fe2f54b9afb3
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2011-03-24 22:54:02 +0000

    Updated VB code to use HaarCascade.Detect function. Use the classified similar to the C# version.
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@1268 d7f09016-e345-0410-b530-edf29a71df78

--- Emgu.CV.Example/FaceDetectionVB/Module1.vb
+++ Emgu.CV.Example/FaceDetectionVB/Module1.vb
@@ -21,17 +21,17 @@ Module Module1
       Dim img As New Image(Of Bgr, Byte)("lena.jpg")
 
       'Load the object detector
-      Dim objectToDetect As New HaarCascade("haarcascade_frontalface_alt2.xml")
+      Dim faceDetector As New HaarCascade("haarcascade_frontalface_alt.xml")
 
       'Convert the image to Grayscale
       Dim imgGray As Image(Of Gray, Byte) = img.Convert(Of Gray, Byte)()
 
-      For Each face As MCvAvgComp In imgGray.DetectHaarCascade( _
-         objectToDetect, _
-         1.1, _
-         50, _
-         CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, _
-         New Size(20, 20))(0)
+      For Each face As MCvAvgComp In faceDetector.Detect( _
+                         imgGray, _
+                         1.1, _
+                         50, _
+                         CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, _
+                         New Size(20, 20))
          img.Draw(face.rect, New Bgr(Color.White), 1)
       Next
 
