afd665a3b45a15a77374ec290d2739b93ccc0bd7
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2011-05-01 20:26:50 +0000

    Move BitmapSourceConverter to the Emgu.CV.WPF folder
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@1318 d7f09016-e345-0410-b530-edf29a71df78

---
+++ Emgu.CV.WPF/BitmapSourceConverter.cs
@@ -0,0 +1,47 @@
+//----------------------------------------------------------------------------
+//  Copyright (C) 2004-2011 by EMGU. All rights reserved.       
+//----------------------------------------------------------------------------
+
+using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Emgu.CV;
+
+namespace Emgu.CV.WPF
+{
+   public static class BitmapSourceConvert
+   {
+      /// <summary>
+      /// Delete a GDI object
+      /// </summary>
+      /// <param name="o">The poniter to the GDI object to be deleted</param>
+      /// <returns></returns>
+      [DllImport("gdi32")]
+      private static extern int DeleteObject(IntPtr o);
+
+      /// <summary>
+      /// Convert an IImage to a WPF BitmapSource. The result can be used in the Set Property of Image.Source
+      /// </summary>
+      /// <param name="image">The Emgu CV Image</param>
+      /// <returns>The equivalent BitmapSource</returns>
+      public static BitmapSource ToBitmapSource(IImage image)
+      {
+         using (System.Drawing.Bitmap source = image.Bitmap)
+         {
+            IntPtr ptr = source.GetHbitmap(); //obtain the Hbitmap
+
+            BitmapSource bs = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                ptr,
+                IntPtr.Zero,
+                Int32Rect.Empty,
+                System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+
+            DeleteObject(ptr); //release the HBitmap
+            return bs;
+         }
+      }
+   }
+}
