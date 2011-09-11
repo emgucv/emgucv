08af9b7fd1be45dde7ca3087534ef3cbbdedf884
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2011-09-11 04:14:19 +0000

    Added Bitmap Visualizer.
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@1433 d7f09016-e345-0410-b530-edf29a71df78

---
+++ Emgu.CV.DebuggerVisualizers/BitmapVisualizer.cs
@@ -0,0 +1,35 @@
+﻿//----------------------------------------------------------------------------
+//  Copyright (C) 2004-2011 by EMGU. All rights reserved.       
+//----------------------------------------------------------------------------
+
+using System;
+using System.Drawing;
+using Microsoft.VisualStudio.DebuggerVisualizers;
+using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.CV.Structure;
+using System.Diagnostics;
+
+[assembly: DebuggerVisualizer(
+typeof(Emgu.CV.DebuggerVisualizers.BitmapVisualizer),
+Target = typeof(Bitmap))]
+
+
+namespace Emgu.CV.DebuggerVisualizers
+{
+   public sealed class BitmapVisualizer : DialogDebuggerVisualizer
+   {
+      protected override void Show(IDialogVisualizerService windowService, IVisualizerObjectProvider objectProvider)
+      {
+         Bitmap image = objectProvider.GetObject() as Bitmap;
+         if (image != null)
+         {
+            using (ImageViewer viewer = new ImageViewer())
+            {
+               viewer.Image = new Image<Bgr, Byte>(image);
+               windowService.ShowDialog(viewer);
+            }
+         }
+      }
+   }
+}
