3a33abfbd8ccd4dfb76437ec2c7ca6eaf523bd3c
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2012-03-20 14:50:48 +0000

    Added Android PInvoke folder.
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@1594 d7f09016-e345-0410-b530-edf29a71df78

---
+++ Emgu.CV/PInvoke/Android/CvInvokeEntryPoints.cs
@@ -0,0 +1,86 @@
+//This file is automatically generate by CMAKE. DO NOT MODIFY.
+using System;
+
+namespace Emgu.CV
+{
+   public partial class CvInvoke
+   {
+      /// <summary>
+      /// The file name of the core library
+      /// </summary>
+      public const string OPENCV_CORE_LIBRARY = "opencv_core";
+      /// <summary>
+      /// The file name of the imgproc library
+      /// </summary>
+      public const string OPENCV_IMGPROC_LIBRARY = "opencv_imgproc";
+      /// <summary>
+      /// The file name of the highgui library
+      /// </summary>
+      public const string OPENCV_HIGHGUI_LIBRARY = "opencv_highgui";
+	  /// <summary>
+      /// The file name of the flann library
+      /// </summary>
+      public const string OPENCV_FLANN_LIBRARY = "opencv_flann";
+      /// <summary>
+      /// The file name of the feature2d library
+      /// </summary>
+      public const string OPENCV_FEATURES2D_LIBRARY = "opencv_features2d";
+      /// <summary>
+      /// The file name of the objdetect library
+      /// </summary>
+      public const string OPENCV_OBJDETECT_LIBRARY = "opencv_objdetect";
+      /// <summary>
+      /// The file name of the calib3d library
+      /// </summary>
+      public const string OPENCV_CALIB3D_LIBRARY = "opencv_calib3d";
+      /// <summary>
+      /// The file name of the legacy library
+      /// </summary>
+      public const string OPENCV_LEGACY_LIBRARY = "opencv_legacy";
+      /// <summary>
+      /// The file name of the video library
+      /// </summary>
+      public const string OPENCV_VIDEO_LIBRARY = "opencv_video";
+      /// <summary>
+      /// The file name of the gpu library
+      /// </summary>
+      public const string OPENCV_GPU_LIBRARY = "";
+      /// <summary>
+      /// The file name of the cvextern library
+      /// </summary>
+      public const string EXTERN_LIBRARY = "cvextern";
+      /// <summary>
+      /// The file name of the cvextern_gpu library
+      /// </summary>
+      public const string EXTERN_GPU_LIBRARY = "";
+      /// <summary>
+      /// The file name of the opencv_ffmpeg library
+      /// </summary>
+      public const string OPENCV_FFMPEG_LIBRARY = "";
+	  /// <summary>
+      /// The file name of the ml library
+      /// </summary>
+      public const string OPENCV_ML_LIBRARY = "opencv_ml";
+	  /// <summary>
+      /// The file name of the contrib library
+      /// </summary>
+      public const string OPENCV_CONTRIB_LIBRARY = "opencv_contrib";
+	  /// <summary>
+      /// The file name of the photo library
+      /// </summary>
+      public const string OPENCV_PHOTO_LIBRARY = "opencv_photo";
+	  /// <summary>
+      /// The file name of the nonfree library
+      /// </summary>
+      public const string OPENCV_NONFREE_LIBRARY = "opencv_nonfree";
+	  /// <summary>
+      /// The file name of the videostab library
+      /// </summary>
+      public const string OPENCV_VIDEOSTAB_LIBRARY = "opencv_videostab";
+	  
+      /// <summary>
+      /// The bit-ness of the unmanaged code (either 32 or 64) 
+      /// </summary>
+      public const uint UnmanagedCodeBitness = 32;
+   }
+}
