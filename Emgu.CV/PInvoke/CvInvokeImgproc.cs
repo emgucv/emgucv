3a644780ac4ce1032f8a6ce69e7bb6f221ff2bd3
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2012-03-20 03:11:03 +0000

    inpaint function belongs to photo module now.
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@1586 d7f09016-e345-0410-b530-edf29a71df78

--- Emgu.CV/PInvoke/CvInvokeImgproc.cs
+++ Emgu.CV/PInvoke/CvInvokeImgproc.cs
@@ -514,7 +514,7 @@ namespace Emgu.CV
       /// <param name="dst">The output image of the same format and the same size as input</param>
       /// <param name="flags">The inpainting method</param>
       /// <param name="inpaintRadius">The radius of circlular neighborhood of each point inpainted that is considered by the algorithm</param>
-      [DllImport(OPENCV_IMGPROC_LIBRARY, CallingConvention = CvInvoke.CvCallingConvention)]
+      [DllImport(OPENCV_PHOTO_LIBRARY, CallingConvention = CvInvoke.CvCallingConvention)]
       public static extern void cvInpaint(IntPtr src, IntPtr mask, IntPtr dst, double inpaintRadius, CvEnum.INPAINT_TYPE flags);
 
       /// <summary>
