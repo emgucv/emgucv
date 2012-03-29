5eef1300d10457ee376aeac5f44df9c536e1925e
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2012-03-29 04:33:14 +0000

    updated build.sh script
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@1613 d7f09016-e345-0410-b530-edf29a71df78

--- ios/build.sh
+++ ios/build.sh
@@ -1,17 +1,24 @@
 cd ..
 ios/configure-device_xcode.sh
 xcodebuild -sdk iphoneos -configuration Release -target ALL_BUILD clean build
+rm -rf ios/armv7
 mkdir -p ios/armv7
 cp -r lib/Release/* ios/armv7/
 cd ios/armv7
 for i in *; do j=`echo $i | cut -d . -f 1`; j=$j"_armv7.a";mv $i $j; done
 cd ../..
+mkdir -p Emgu.CV/PInvoke/MonoTouch/armv7
+mv ios/armv7/* Emgu.CV/PInvoke/MonoTouch/armv7
 rm CMakeCache.txt
 ios/configure-simulator_xcode.sh
 xcodebuild -sdk iphonesimulator -configuration Release -target ALL_BUILD clean build
+rm -rf ios/i386
 mkdir -p ios/i386
 cp -r lib/Release/* ios/i386/
 cd ios/i386
 for i in *; do j=`echo $i | cut -d . -f 1`; j=$j"_i386.a";mv $i $j; done
-cd ..
+cd ../..
+mkdir -p Emgu.CV/PInvoke/MonoTouch/i386
+mv ios/i386/* Emgu.CV/PInvoke/MonoTouch/i386
+cd ios
 
