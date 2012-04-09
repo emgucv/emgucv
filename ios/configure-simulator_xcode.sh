7a90c771b4c4c7d6561af96efe78ac1c1d687ad9
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2012-04-09 03:08:32 +0000

    fixed a bug in building binaries for iOS
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@1633 d7f09016-e345-0410-b530-edf29a71df78

--- ios/configure-simulator_xcode.sh
+++ ios/configure-simulator_xcode.sh
@@ -1,1 +1,1 @@
-cmake -GXcode -DCMAKE_TOOLCHAIN_FILE=../emgucv/ios/cmake/Toolchains/Toolchain-iPhoneSimulator_Xcode.cmake -DCMAKE_INSTALL_PREFIX=../OpenCV_iPhoneSimulator ../emgucv 
+cmake -GXcode -DCMAKE_TOOLCHAIN_FILE=./ios/cmake/Toolchains/Toolchain-iPhoneSimulator_Xcode.cmake -DCMAKE_INSTALL_PREFIX=../OpenCV_iPhoneSimulator ./ 
