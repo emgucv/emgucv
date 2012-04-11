40c8ad19407663006970f55ad55ef3e200536c51
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2012-04-11 03:55:34 +0000

    Added Image<,> constructor that accpts CGImage for MonoTouch
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@1640 d7f09016-e345-0410-b530-edf29a71df78

--- Emgu.CV/PInvoke/MonoTouch/ImageMonoTouch.cs
+++ Emgu.CV/PInvoke/MonoTouch/ImageMonoTouch.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Drawing;
 using Emgu.CV;
 using Emgu.CV.Structure;
 using MonoTouch.CoreGraphics;
@@ -10,7 +11,28 @@ namespace Emgu.CV
       where TColor : struct, IColor
       where TDepth : new()
    {
-      public CGImage ToCGImage ()
+      public Image(CGImage cgImage)
+         : this(cgImage.Width, cgImage.Height)
+      {
+         if (this is Image<Bgra, Byte>)
+         {
+            RectangleF rect = new RectangleF(PointF.Empty, new SizeF(cgImage.Width, cgImage.Height));
+            using (CGBitmapContext context = new CGBitmapContext(
+             MIplImage.imageData,
+             Width, Height,
+             8,
+             Width * 4,
+             CGColorSpace.CreateDeviceRGB(),
+             CGImageAlphaInfo.PremultipliedLast))
+               context.DrawImage(rect, cgImage);
+         } else
+         {
+            using (Image<Bgra, Byte> tmp = new Image<Bgra, Byte>(cgImage))
+               ConvertFrom(tmp);
+         }
+      }
+
+      public CGImage ToCGImage()
       {
          if (this is Image<Bgra, Byte>)
          {
@@ -21,12 +43,12 @@ namespace Emgu.CV
          		Width * 4,
          		CGColorSpace.CreateDeviceRGB(),
          		CGImageAlphaInfo.PremultipliedLast))
-               return context.ToImage ();
+               return context.ToImage();
          } else
          {
             using (Image<Bgra, Byte> tmp = Convert<Bgra, Byte>())
             {
-               return tmp.ToCGImage ();
+               return tmp.ToCGImage();
             }
          }
       }
