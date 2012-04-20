25e6df528eb2848c40168ff9e0c7a359670f379b
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2012-04-20 03:22:41 +0000

    Fixed a compilation error for Stitcher on Android.
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@1647 d7f09016-e345-0410-b530-edf29a71df78

--- Emgu.CV.Stitching/Stitcher.cs
+++ Emgu.CV.Stitching/Stitcher.cs
@@ -6,7 +6,9 @@
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Text;
+#if !ANDROID
 using Emgu.CV.GPU;
+#endif
 using Emgu.CV.Structure;
 using Emgu.CV.Util;
 using Emgu.Util;
@@ -65,8 +67,26 @@ namespace Emgu.CV.Stitching
    {
       static StitchingInvoke()
       {
+#if ANDROID
+         //Dummy code to make sure the static constructor of CvInvoke has been called and the error handler has been registered.
+         CvInvoke.CV_MAKETYPE(0, 0);
+
+         String[] modules = new String[] 
+         {
+            CvInvoke.EXTERN_GPU_LIBRARY
+         };
+
+         String formatString = CvInvoke.GetModuleFormatString();
+         for (int i = 0; i < modules.Length; ++i)
+         {
+            modules[i] = String.Format(formatString, modules[i]);
+         }
+
+         CvInvoke.LoadUnmanagedModules(null, modules);
+#else
          //Dummy code to make sure the static constructor of GpuInvoke has been called
          bool hasCuda = GpuInvoke.HasCuda;
+#endif
       }
 
       [DllImport(CvInvoke.EXTERN_GPU_LIBRARY, CallingConvention = CvInvoke.CvCallingConvention)]
