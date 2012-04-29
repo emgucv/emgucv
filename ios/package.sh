3a175b777bf4c8ade1f41ad545f9253d6501e9de
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2012-04-29 01:58:16 +0000

    Added missing Emgu.CV.GPU.MonoTouch.csproj. Updated ios package script
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@1664 d7f09016-e345-0410-b530-edf29a71df78

--- ios/package.sh
+++ ios/package.sh
@@ -10,15 +10,31 @@ svn export ../Emgu.CV.Stitching ios-package/Emgu.CV.Stitching
 svn export ../Emgu.Util ios-package/Emgu.Util
 svn export ../Emgu.CV.GPU ios-package/Emgu.CV.GPU
 
-mkdir ios-package/Emgu.CV.Example
-svn export ../Emgu.CV.Example/MonoTouch ios-package/Emgu.CV.Example/MonoTouch
-svn export ../Emgu.CV.Example/SURFFeature/box.png ios-package/Emgu.CV.Example/SURFFeature
-svn export ../Emgu.CV.Example/SURFFeature/box_in_scene.png ios-package/Emgu.CV.Example/SURFFeature
-svn export ../Emgu.CV.Example/SURFFeature/DrawMatches.cs ios-package/Emgu.CV.Example/SURFFeature
-svn export ../Emgu.CV.Example/PlanarSubdivision/DrawSubdivision.cs ios-package/Emgu.CV.Example/PlanarSubdivision
+mkdir -p ios-package/Solution/MonoTouch
+svn export ../Solution/MonoTouch/Emgu.CV.MonoTouch.sln ios-package/Solution/MonoTouch/Emgu.CV.MonoTouch.sln
 
-mkdir -p ios-package/Solution/VS2010/
-cp ../Solution/VS2010/Emgu.CV.MonoTouch.sln ios-package/Solution/VS2010
+mkdir -p ios-package/Emgu.CV.Example/SURFFeature
+mkdir -p ios-package/Emgu.CV.Example/PlanarSubdivision
+mkdir -p ios-package/Emgu.CV.Example/LicensePlateRecognition
+mkdir -p ios-package/Emgu.CV.Example/PedestrianDetection
+mkdir -p ios-package/Emgu.CV.Example/TrafficSignRecognition
+svn export ../Emgu.CV.Example/MonoTouch ios-package/Emgu.CV.Example/MonoTouch
+svn export ../Emgu.CV.Example/SURFFeature/box.png ios-package/Emgu.CV.Example/SURFFeature/box.png
+svn export ../Emgu.CV.Example/SURFFeature/box_in_scene.png ios-package/Emgu.CV.Example/SURFFeature/box_in_scene.png
+svn export ../Emgu.CV.Example/SURFFeature/DrawMatches.cs ios-package/Emgu.CV.Example/SURFFeature/DrawMatches.cs
+svn export ../Emgu.CV.Example/PlanarSubdivision/DrawSubdivision.cs ios-package/Emgu.CV.Example/PlanarSubdivision/DrawSubdivision.cs
+svn export ../Emgu.CV.Example/LicensePlateRecognition/license-plate.jpg ios-package/Emgu.CV.Example/LicensePlateRecognition/license-plate.jpg
+svn export ../Emgu.CV.Example/LicensePlateRecognition/LicensePlateDetector.cs ios-package/Emgu.CV.Example/LicensePlateRecognition/LicensePlateDetector.cs
+svn export ../Emgu.CV.Example/PedestrianDetection/pedestrian.png ios-package/Emgu.CV.Example/PedestrianDetection/pedestrian.png
+svn export ../Emgu.CV.Example/PedestrianDetection/FindPedestrian.cs ios-package/Emgu.CV.Example/PedestrianDetection/FindPedestrian.cs
+svn export ../Emgu.CV.Example/TrafficSignRecognition/stop-sign.jpg ios-package/Emgu.CV.Example/TrafficSignRecognition/stop-sign.jpg
+svn export ../Emgu.CV.Example/TrafficSignRecognition/stop-sign-model.png ios-package/Emgu.CV.Example/TrafficSignRecognition/stop-sign-model.png
+svn export ../Emgu.CV.Example/TrafficSignRecognition/StopSignDetector.cs ios-package/Emgu.CV.Example/TrafficSignRecognition/StopSignDetector.cs
+
 cp ../CommonAssemblyInfo.cs ios-package
 find ./ios-package -type f -name CMakeList* -exec rm '{}' \;
 find ./ios-package -type f -name *Android* -exec rm '{}' \;
+cd ios-package
+rm Emgu.CV/Emgu.CV.csproj Emgu.CV.ML/Emgu.CV.ML.csproj Emgu.CV.GPU/Emgu.CV.GPU.csproj Emgu.CV.OCR/Emgu.CV.OCR.csproj Emgu.Util/Emgu.Util.csproj Emgu.CV.Stitching/Emgu.CV.Stitching.csproj
+cd ..
+
