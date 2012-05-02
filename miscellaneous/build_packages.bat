b03e99af5df8dbeed05f556cbc6eb743c73301c3
canming <canming@d7f09016-e345-0410-b530-edf29a71df78>
2012-05-02 01:35:29 +0000

    updated build_packages.bat script.
    git-svn-id: https://emgucv.svn.sourceforge.net/svnroot/emgucv/trunk@1678 d7f09016-e345-0410-b530-edf29a71df78

--- miscellaneous/build_packages.bat
+++ miscellaneous/build_packages.bat
@@ -4,25 +4,25 @@ mkdir ..\release
 
 svn revert Emgu.CV.License.txt
 
-call perl miscellaneous\svn-clean.pl .
+call perl miscellaneous\svn-clean.pl --force
 REM build 32 bit package with openni using visual studio, with document
 call Build_Binary_x86.bat 32 gpu vc openni doc package
 mv libemgucv*.zip ..\release
 mv libemgucv*.exe ..\release
 
-call perl miscellaneous\svn-clean.pl .
+call perl miscellaneous\svn-clean.pl --force
 REM build 32 bit package without openni using visual studio, with document
 call Build_Binary_x86.bat 32 gpu vc noopenni doc package
 mv libemgucv*.zip ..\release
 mv libemgucv*.exe ..\release
 
-call perl miscellaneous\svn-clean.pl .
+call perl miscellaneous\svn-clean.pl --force
 REM build 64 bit package with openni using visual studio, with document
 call Build_Binary_x86.bat 64 gpu vc openni doc package
 mv libemgucv*.zip ..\release
 mv libemgucv*.exe ..\release
 
-call perl miscellaneous\svn-clean.pl .
+call perl miscellaneous\svn-clean.pl --force
 REM build 64 bit package without openni using visual studio, with document
 call Build_Binary_x86.bat 64 gpu vc noopenni doc package
 mv libemgucv*.zip ..\release
@@ -32,25 +32,25 @@ REM download commercial license
 wget http://www.emgu.com/wiki/files/CommercialLicense.txt
 cp CommercialLicense.txt Emgu.CV.License.txt
 
-call perl miscellaneous\svn-clean.pl .
+call perl miscellaneous\svn-clean.pl --force
 REM build 32 bit package with openni using intel compiler, with document
 call Build_Binary_x86.bat 32 gpu intel openni doc package
 mv libemgucv*.zip ..\release
 mv libemgucv*.exe ..\release
 
-call perl miscellaneous\svn-clean.pl .
+call perl miscellaneous\svn-clean.pl --force
 REM build 32 bit package without openni using intel compiler, with document
 call Build_Binary_x86.bat 32 gpu intel noopenni doc package
 mv libemgucv*.zip ..\release
 mv libemgucv*.exe ..\release
 
-call perl miscellaneous\svn-clean.pl .
+call perl miscellaneous\svn-clean.pl --force
 REM build 64 bit package with openni using intel compiler, with document
 call Build_Binary_x86.bat 64 gpu intel openni doc package
 mv libemgucv*.zip ..\release
 mv libemgucv*.exe ..\release
 
-call perl miscellaneous\svn-clean.pl .
+call perl miscellaneous\svn-clean.pl --force
 REM build 64 bit package without openni using intel compiler, with document
 call Build_Binary_x86.bat 64 gpu intel nopenni doc package
 mv libemgucv*.zip ..\release
