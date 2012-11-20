5f6294efdb1e31f495144dc8866a837aa894df7e
Canming Huang <support@emgu.com>
2012-11-20 15:07:54 -0500

    iOS performance improvements

--- Emgu.CV.Extern/ios/divmodsi4.c
+++ Emgu.CV.Extern/ios/divmodsi4.c
@@ -7,13 +7,37 @@
 #include "opencv2/core/core.hpp"
 
 CVAPI(int) __divmodsi4(int a, int b, int* rem);
-
+CVAPI(unsigned long) __udivmodsi4(unsigned long num, unsigned long den, int modwanted);
 /* Returns: a / b, *rem = a % b  */
 
 int __divmodsi4(int a, int b, int* rem)
 {
-  *rem = a%b;
-  return a/b; 
+  int d = a / b;
+  *rem = a - (d * b); 
+  return d; 
 }
 
+unsigned long __udivmodsi4(unsigned long num, unsigned long den, int modwanted)
+{
+  unsigned long bit = 1;
+  unsigned long res = 0;
+
+  while (den < num && bit && !(den & (1L<<31)))
+    {
+      den <<=1;
+      bit <<=1;
+    }
+  while (bit)
+    {
+      if (num >= den)
+	{
+	  num -= den;
+	  res |= bit;
+	}
+      bit >>=1;
+      den >>=1;
+    }
+  if (modwanted) return num;
+  return res;
+}
 
