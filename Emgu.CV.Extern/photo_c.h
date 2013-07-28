23e17dd513e93d7a73d5bd0ae716b37769327ce0
Canming Huang <support@emgu.com>
2013-07-28 09:52:57 -0400

    Added missing file.

---
+++ Emgu.CV.Extern/photo_c.h
@@ -0,0 +1,18 @@
+//----------------------------------------------------------------------------
+//
+//  Copyright (C) 2004-2013 by EMGU. All rights reserved.
+//
+//----------------------------------------------------------------------------
+
+#pragma once
+#ifndef EMGU_PHOTO_C_H
+#define EMGU_PHOTO_C_H
+
+#include "opencv2/core/core_c.h"
+#include "opencv2/photo/photo.hpp"
+
+CVAPI(void) cvFastNlMeansDenoising(CvArr* src, CvArr* dst, float h, int templateWindowSize, int searchWindowSize);
+
+CVAPI(void) cvFastNlMeansDenoisingColored(CvArr* src, CvArr* dst, float h, float hColor, int templateWindowSize, int searchWindowSize);
+
+#endif
