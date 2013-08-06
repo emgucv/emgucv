13ac4beee929067d82b9e27ca7342e681872acdb
Canming Huang <support@emgu.com>
2013-08-06 16:59:48 -0400

    Use Xamarin Android unit test projects.

--- Emgu.CV.Test/Emgu.CV.Test.Android/MainActivity.cs
+++ Emgu.CV.Test/Emgu.CV.Test.Android/MainActivity.cs
@@ -1,47 +1,32 @@
 using System.Reflection;
-
 using Android.App;
 using Android.OS;
-using Android.NUnitLite;
-using Android.NUnitLite.UI;
-
+using Xamarin.Android.NUnitLite;
 using Android.Content.Res;
-using Emgu.CV;
 using Emgu.CV.Structure;
-using System.IO;
-using Android.Graphics;
 
 namespace Emgu.CV.Test
 {
-   [Activity(Label = "Emgu.CV.Test", MainLauncher = true)]
-   public class MainActivity : RunnerActivity
-   {
-      protected override void OnCreate(Bundle bundle)
-      {
-         // tests can be inside the main assembly
-         Add(Assembly.GetExecutingAssembly());
-         // or in any reference assemblies			
-         //Add (typeof (m4a.tests.RunnerTest).Assembly);
-         // or in any assembly that you load (since JIT is available)
-
-         //dummy code to load emgucv;
+	[Activity (Label = "Emgu.CV.Test.Android", MainLauncher = true)]
+	public class MainActivity : TestSuiteActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+//dummy code to load emgucv;
          Image<Bgr, byte> dummy = new Image<Bgr, byte>(2, 2);
          dummy._Not();
          
-         AssetsUtil.Assets = this.Assets;
-#if false
-			// you can use the default or set your own custom writer (e.g. save to web site and tweet it ;-)
-			Runner.Writer = new TcpTextWriter ("10.0.1.2", 16384);
-			// start running the test suites as soon as the application is loaded
-			Runner.AutoStart = true;
-			// crash the application (to ensure it's ended) and return to springboard
-			Runner.TerminateAfterExecution = true;
-#endif
-         // you cannot add more assemblies once calling base
-         base.OnCreate(bundle);
-      }
-   }
 
+         AssetsUtil.Assets = this.Assets;
+			// tests can be inside the main assembly
+			AddTest (Assembly.GetExecutingAssembly ());
+			// or in any reference assemblies
+			// AddTest (typeof (Your.Library.TestClass).Assembly);
+
+			// Once you called base.OnCreate(), you cannot add more assemblies.
+			base.OnCreate (bundle);
+		}
+	}
    public static class AssetsUtil
    {
       public static AssetManager Assets { get; set; }
@@ -54,3 +39,4 @@ namespace Emgu.CV.Test
       }
    }
 }
+
