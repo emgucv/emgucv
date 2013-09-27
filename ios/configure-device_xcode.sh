0bdcd3761a7cb5ac24dc11f5bd86e4026a20bc78
Canming Huang <support@emgu.com>
2013-09-27 13:40:26 -0400

    move ios folder to platform/ios

--- ios/configure-device_xcode.sh
+++
@@ -1,1 +0,0 @@
-cmake -GXcode -DCMAKE_TOOLCHAIN_FILE=./ios/cmake/Toolchains/Toolchain-iPhoneOS_Xcode.cmake -DCMAKE_INSTALL_PREFIX=../OpenCV_iPhoneOS ./ 
--- ios/configure-simulator_xcode.sh
+++
@@ -1,1 +0,0 @@
-cmake -GXcode -DCMAKE_TOOLCHAIN_FILE=./ios/cmake/Toolchains/Toolchain-iPhoneSimulator_Xcode.cmake -DCMAKE_INSTALL_PREFIX=../OpenCV_iPhoneSimulator ./ 
