aa95a949f493e71bb4cfbb751c968549cda5693c
Canming Huang <support@emgu.com>
2016-02-28 20:49:50 -0500

    line_descriptor updates.

--- Emgu.CV.Extern/line_descriptor/line_descriptor_c.cpp
+++ Emgu.CV.Extern/line_descriptor/line_descriptor_c.cpp
@@ -12,6 +12,10 @@ cv::line_descriptor::BinaryDescriptor* cveLineDescriptorBinaryDescriptorCreate()
    ptr.addref();
    return ptr.get();
 }
+void cveBinaryDescriptorDetect(cv::line_descriptor::BinaryDescriptor* descriptor, cv::Mat* image, std::vector<cv::line_descriptor::KeyLine>* keypoints, cv::Mat* mask)
+{
+   descriptor->detect(*image, *keypoints, mask ? *mask : cv::Mat());
+}
 void cveLineDescriptorBinaryDescriptoyRelease(cv::line_descriptor::BinaryDescriptor** descriptor)
 {
    delete * descriptor;
