786e913c726234115171737c09f9241e52c11ef8
Canming Huang <support@emgu.com>
2016-04-05 17:22:13 -0400

    include the tess_data files with unity demo.

--- Emgu.CV.Unity/copy_demo_assets.bat
+++ Emgu.CV.Unity/copy_demo_assets.bat
@@ -6,6 +6,7 @@ cp ../opencv/samples/data/lena.jpg Assets/Emgu.CV/Emgu.CV.Demo/Resources
 cp ../Emgu.CV.Test/stitch*.jpg Assets/Emgu.CV/Emgu.CV.Demo/Resources/
 
 cp ../opencv/data/haarcascades/haarcascade_frontalface_alt2.xml Assets/Emgu.CV/Emgu.CV.Demo/Resources
+cp -rf ../Emgu.CV.World/tessdata Assets/Emgu.CV/Emgu.CV.Demo/Resources/
 cp haarcascade_frontalface_alt2.xml.meta Assets/Emgu.CV/Emgu.CV.Demo/Resources
 cp -rf demo/* Assets/Emgu.CV/Emgu.CV.Demo
 cp unityStoreIcons/EmguCVLogo_128x128.png Assets/Emgu.CV/Emgu.CV.Demo/EmguCVLogo.png
