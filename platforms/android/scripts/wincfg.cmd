d956e7aecc20db5094ed6a0f9096c13551c3cd81
Canming Huang <support@emgu.com>
2016-09-12 09:33:48 -0400

    Search for 64bit cmake on Windows.

--- platforms/android/scripts/wincfg.cmd
+++ platforms/android/scripts/wincfg.cmd
@@ -4,7 +4,12 @@ IF EXIST "C:\android-ndk-r10e" SET ANDROID_NDK=C:\android-ndk-r10e
 IF EXIST "C:\android-ndk-r11" SET ANDROID_NDK=C:\android-ndk-r11
 IF EXIST "C:\android-ndk-r11c" SET ANDROID_NDK=C:\android-ndk-r11c
 IF EXIST "C:\android-ndk-r12b" SET ANDROID_NDK=C:\android-ndk-r12b
-SET CMAKE_EXE=C:\Program Files (x86)\CMake\bin\cmake.exe
+
+
+IF EXIST "%PROGRAMFILES_DIR_X86%\CMake 2.8\bin\cmake.exe" SET CMAKE_EXE="%PROGRAMFILES_DIR_X86%\CMake 2.8\bin\cmake.exe"
+IF EXIST "%PROGRAMFILES_DIR_X86%\CMake\bin\cmake.exe" SET CMAKE_EXE="%PROGRAMFILES_DIR_X86%\CMake\bin\cmake.exe"
+IF EXIST "%PROGRAMFILES_DIR%\CMake\bin\cmake.exe" SET CMAKE_EXE="%PROGRAMFILES_DIR%\CMake\bin\cmake.exe"
+
 SET MAKE_EXE=%ANDROID_NDK%\prebuilt\windows-x86_64\bin\make.exe
 
 :: variables required for android-opencv build ::
@@ -15,9 +20,11 @@ SET ANT_DIR="%VS140COMNTOOLS%..\..\Apps\apache-ant-1.9.3"
 
 @ECHO OFF &SETLOCAL 
 FOR /F "tokens=2*" %%a IN ('REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\JavaSoft\Java Development Kit\1.7" /v JavaHome') DO set "JavaHome17=%%b"
+FOR /F "tokens=2*" %%a IN ('REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\JavaSoft\Java Development Kit\1.8" /v JavaHome') DO set "JavaHome18=%%b"
 ::ECHO Java Home 17: %JavaHome17%
 
-SET JAVA_HOME=%JavaHome17%
+IF NOT "" == "%JavaHome17%" SET JAVA_HOME=%JavaHome17%
+IF NOT "" == "%JavaHome18%" set JAVA_HOME=%JavaHome18%
 
 :: configuration options ::
 :::: general ARM-V7 settings
