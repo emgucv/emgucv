21079373c26e81ec98c9b61450041a2f7ce31b86
Canming Huang <support@emgu.com>
2017-01-23 09:46:14 -0500

    Show both original and processed image for UWP camera demo.

--- Emgu.CV.Example/RealtimeCamera.UWP/MainPage.xaml.cs
+++ Emgu.CV.Example/RealtimeCamera.UWP/MainPage.xaml.cs
@@ -4,8 +4,10 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -21,77 +23,97 @@ using Emgu.Util;
 
 namespace RealtimeCamera
 {
-   /// <summary>
-   /// An empty page that can be used on its own or navigated to within a Frame.
-   /// </summary>
-   public sealed partial class MainPage : Page
-   {
-      public MainPage()
-      {
-         this.InitializeComponent();
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            textBlock.Text = String.Empty;
+            Window.Current.VisibilityChanged += (sender, args) =>
+            {
+                CvInvoke.WinrtOnVisibilityChanged(args.Visible);
+            };
+            CvInvoke.WinrtSetFrameContainer(this.image2);
+            CvInvoke.WinrtStartMessageLoop(Process);
 
-         Window.Current.VisibilityChanged += (sender, args) =>
-         {
-            CvInvoke.WinrtOnVisibilityChanged(args.Visible);
-         };
-         CvInvoke.WinrtSetFrameContainer(this.image);
-         CvInvoke.WinrtStartMessageLoop(Process);
+        }
 
-      }
+        private VideoCapture _capture;
+        public void Process()
+        {
+            Mat m = new Mat();
+            Mat mProcessed = new Mat();
+            while (true)
+            {
 
-      private VideoCapture _capture;
-      public void Process()
-      {
-         Mat m = new Mat();
-         while (true)
-         {
+                if (_captureEnabled)
+                {
+                    try
+                    {
+                        if (_capture == null)
+                            _capture = new VideoCapture();
+
+                        //Read the camera data to the mat
+                        //Must use VideoCapture.Read function for UWP to read image from capture.
+                        _capture.Read(m);
+                        if (!m.IsEmpty)
+                        {
+                            //Can apply simple image processing to the captured image, let just invert the pixels
+                            CvInvoke.BitwiseNot(m, mProcessed);
+                            CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                                async () =>
+                                {
+                                    var wb = mProcessed.ToWritableBitmap();
+
+                                    image1.Source = wb;
+                                });
+
+                            //The data in the mat that is read from the camera will 
+                            //be drawn to the Image control
+                            CvInvoke.WinrtImshow();
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                            async () =>
+                            {
+                                textBlock.Text = e.Message;
+                            });
+                    }
+                }
+                else
+                {
+                    if (_capture != null)
+                    {
+                        _capture.Dispose();
+                        _capture = null;
+                    }
+
+                    Task t = Task.Delay(100);
+                    t.Wait();
+                }
+            }
+        }
+
+        private bool _captureEnabled = false;
+
+        private void captureButton_Click(object sender, RoutedEventArgs e)
+        {
+            _captureEnabled = !_captureEnabled;
+
             if (_captureEnabled)
             {
-               if (_capture == null)
-                  _capture = new VideoCapture();
-
-               //Read the camera data to the mat
-               //Must use VideoCapture.Read function for UWP to read image from capture.
-               _capture.Read(m);
-               if (!m.IsEmpty)
-               {
-                  //some simple image processing, let just invert the pixels
-                  CvInvoke.BitwiseNot(m, m);
-
-                  //The data in the mat that is read from the camera will 
-                  //be drawn to the Image control
-                  CvInvoke.WinrtImshow();
-               }
+                captureButton.Content = "Stop";
             }
             else
             {
-               if (_capture != null)
-               {
-                  _capture.Dispose();
-                  _capture = null;
-               }
-
-               Task t = Task.Delay(100);
-               t.Wait();
+                captureButton.Content = "Start Capture";
             }
-         }
-      }
 
-      private bool _captureEnabled = false;
-
-      private void captureButton_Click(object sender, RoutedEventArgs e)
-      {
-         _captureEnabled = !_captureEnabled;
-
-         if (_captureEnabled)
-         {
-            captureButton.Content = "Stop";
-         }
-         else
-         {
-            captureButton.Content = "Start Capture";
-         }
-
-      }
-   }
+        }
+    }
 }
