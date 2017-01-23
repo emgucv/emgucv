74ac1180763c89c8109e6f96153a5bcd5f6197ff
Canming Huang <support@emgu.com>
2017-01-23 10:57:12 -0500

    Added camera undistort code for UWP demo.

--- Emgu.CV.Example/RealtimeCamera.UWP/MainPage.xaml.cs
+++ Emgu.CV.Example/RealtimeCamera.UWP/MainPage.xaml.cs
@@ -19,6 +19,8 @@ using Windows.UI.Xaml.Navigation;
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
 using Emgu.Util;
 
 namespace RealtimeCamera
@@ -36,10 +38,15 @@ namespace RealtimeCamera
             {
                 CvInvoke.WinrtOnVisibilityChanged(args.Visible);
             };
+            //render the captured image in the bottom image view
             CvInvoke.WinrtSetFrameContainer(this.image2);
+
             CvInvoke.WinrtStartMessageLoop(Process);
 
         }
+
+        private IntrinsicCameraParameters p;
+        private Matrix<float> mapx, mapy;
 
         private VideoCapture _capture;
         public void Process()
@@ -58,16 +65,44 @@ namespace RealtimeCamera
 
                         //Read the camera data to the mat
                         //Must use VideoCapture.Read function for UWP to read image from capture.
+                        //Note that m is in 3 channel RGB color space, 
+                        //our default color space is BGR for 3 channel Mat
                         _capture.Read(m);
+                        
                         if (!m.IsEmpty)
                         {
+                            if (p == null)
+                            {
+                                //Create a dummy camera calibration matrix for testing
+                                //Use your own if you have calibrated your camera
+                                p = new IntrinsicCameraParameters(5);
+                                int centerY = m.Width >> 1;
+                                int centerX = m.Height >> 1;
+                                CvInvoke.SetIdentity(p.IntrinsicMatrix, new MCvScalar(1.0));
+                                p.IntrinsicMatrix.Data[0, 2] = centerY;
+                                p.IntrinsicMatrix.Data[1, 2] = centerX;
+                                p.IntrinsicMatrix.Data[2, 2] = 1;
+                                p.DistortionCoeffs.Data[0, 0] = -0.000003;
+                                p.InitUndistortMap(m.Width, m.Height, out mapx, out mapy);
+
+                            }
+
+                            m.CopyTo(mProcessed);
+                            CvInvoke.Undistort(m, mProcessed, p.IntrinsicMatrix, p.DistortionCoeffs );
+
+                            //mProcess is in the same color space as m, which is RGB, 
+                            //needed to change to BGR
+                            CvInvoke.CvtColor(mProcessed, mProcessed, ColorConversion.Rgb2Bgr);
+                            
+                            
                             //Can apply simple image processing to the captured image, let just invert the pixels
-                            CvInvoke.BitwiseNot(m, mProcessed);
+                            //CvInvoke.BitwiseNot(m, m);
+
+                            //render the processed image on the top imageview
                             CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
                                 async () =>
                                 {
                                     var wb = mProcessed.ToWritableBitmap();
-
                                     image1.Source = wb;
                                 });
 
