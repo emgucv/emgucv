e85fbe1865a1c4e117ae805857db270801ae6fca
Canming Huang <support@emgu.com>
2017-03-22 09:53:01 -0400

    Quick fix to get around the UWP C# compiler issue under release mode compilation.

--- Emgu.CV/CameraCalibration/RotationVector3D.cs
+++ Emgu.CV/CameraCalibration/RotationVector3D.cs
@@ -9,14 +9,14 @@ using System.Xml.Serialization;
 
 namespace Emgu.CV
 {
-   /// <summary>
-   /// A (3x1) Rodrigues rotation vector. Rotation vector is a compact representation of rotation matrix. Direction of the rotation vector is the rotation axis and the length of the vector is the rotation angle around the axis. 
-   /// </summary>
+    /// <summary>
+    /// A (3x1) Rodrigues rotation vector. Rotation vector is a compact representation of rotation matrix. Direction of the rotation vector is the rotation axis and the length of the vector is the rotation angle around the axis. 
+    /// </summary>
 #if !NETFX_CORE
    [Serializable]
 #endif
-   public class RotationVector3D : Matrix<double>
-   {
+    public class RotationVector3D : Matrix<double>
+    {
 
 #if !NETFX_CORE
       /// <summary>
@@ -30,43 +30,44 @@ namespace Emgu.CV
       }
 #endif
 
-      /// <summary>
-      /// Create a 3D rotation vector (3x1 Matrix).
-      /// </summary>
-      public RotationVector3D()
-         : base(3, 1)
-      {
-      }
+        /// <summary>
+        /// Create a 3D rotation vector (3x1 Matrix).
+        /// </summary>
+        public RotationVector3D()
+           : base(3, 1)
+        {
+        }
 
-      /// <summary>
-      /// Create a rotation vector using the specific values
-      /// </summary>
-      /// <param name="value">The values of the (3 x 1) Rodrigues rotation vector</param>
-      public RotationVector3D(double[] value)
-         : base(value)
-      {
-         Debug.Assert(value.Length == 3, "Rodrigues rotation Vector must have size == 3");
-      }
+        /// <summary>
+        /// Create a rotation vector using the specific values
+        /// </summary>
+        /// <param name="value">The values of the (3 x 1) Rodrigues rotation vector</param>
+        public RotationVector3D(double[] value)
+           : this()
+        {
+            Debug.Assert(value.Length == 3, "Rodrigues rotation Vector must have size == 3");
+            Buffer.BlockCopy(value, 0, this.Data, 0, 3*sizeof(double));
+        }
 
-      /// <summary>
-      /// Get or Set the (3x3) rotation matrix represented by this rotation vector.
-      /// </summary>
-      [XmlIgnore]
-      public Mat RotationMatrix
-      {
-         get
-         {
-            Mat mat = new Mat();
-            CvInvoke.Rodrigues(this, mat, null);
-            return mat;
-         }
-         set
-         {
-            Debug.Assert(value.Rows == 3 && value.Cols == 3, "The rotation matrix should be a 3x3 matrix");
+        /// <summary>
+        /// Get or Set the (3x3) rotation matrix represented by this rotation vector.
+        /// </summary>
+        [XmlIgnore]
+        public Mat RotationMatrix
+        {
+            get
+            {
+                Mat mat = new Mat();
+                CvInvoke.Rodrigues(this, mat, null);
+                return mat;
+            }
+            set
+            {
+                Debug.Assert(value.Rows == 3 && value.Cols == 3, "The rotation matrix should be a 3x3 matrix");
 
-            CvInvoke.Rodrigues(value, this, null);
-            
-         }
-      }
-   }
+                CvInvoke.Rodrigues(value, this, null);
+
+            }
+        }
+    }
 }
