daf4000dc05843b0a2e3fdd169cad918108aeeab
Justin <robojoos@gmail.com>
2017-05-01 17:20:27 -0700

    forgot to add cs files
    adding them.

---
+++ Emgu.CV.Contrib/Dpm/DpmInvoke.cs
@@ -0,0 +1,13 @@
+﻿namespace Emgu.CV.Dpm
+{
+    /// <summary>
+    /// Provide interfaces to the Open CV DPM functions
+    /// </summary>
+    public static partial class DpmInvoke
+    {
+        static DpmInvoke()
+        {
+            CvInvoke.CheckLibraryLoaded();
+        }
+    }
+}
---
+++ Emgu.CV.Contrib/Dpm/ObjectDetection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Emgu.CV.Dpm
+{
+    /// <summary>
+    /// A DPM detection
+    /// </summary>
+    public struct ObjectDetection
+    {
+        /// <summary>
+        /// rectangle
+        /// </summary>
+        public readonly Rectangle Rect;
+        /// <summary>
+        /// detection score
+        /// </summary>
+        public readonly float Score;
+        /// <summary>
+        /// class of the detection
+        /// </summary>
+        public readonly int ClassId;
+
+        /// <summary>
+        /// create a detection
+        /// </summary>
+        /// <param name="rect"></param>
+        /// <param name="score"></param>
+        /// <param name="classId"></param>
+        public ObjectDetection(Rectangle rect, float score, int classId)
+        {
+            Rect = rect;
+            Score = score;
+            ClassId = classId;
+        }
+    }
+}
