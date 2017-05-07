ac9f673aec7b11181cb5fe7ecfb41c8dddf07b4d
Canming Huang <support@emgu.com>
2017-05-07 19:00:44 -0400

    Remove c++x11 syntax for raspberry pi.

--- Emgu.CV.Extern/dpm/dpm_c.cpp
+++ Emgu.CV.Extern/dpm/dpm_c.cpp
@@ -8,11 +8,10 @@
 
 using cv::dpm::DPMDetector;
 
-
 DPMDetector* cveDPMDetectorCreate(std::vector<cv::String>* filenames, std::vector<cv::String>* classNames)
 {
-	std::vector<std::basic_string<char>> files = std::vector<std::string>(filenames->size());
-	std::vector<std::basic_string<char>> classes = std::vector<std::string>(classNames->size());
+	std::vector< std::basic_string<char> > files = std::vector<std::string>(filenames->size());
+	std::vector< std::basic_string<char> > classes = std::vector<std::string>(classNames->size());
 
 	for (std::vector<cv::String>::iterator it = filenames->begin(); it != filenames->end(); ++it)
 		files.push_back(std::string(it->c_str(), it->size()));
@@ -30,7 +29,7 @@ void cveDPMDetectorDetect(DPMDetector* dpm, cv::Mat* image, std::vector<CvRect>*
 	std::vector<DPMDetector::ObjectDetection> dobjects = std::vector<DPMDetector::ObjectDetection>();
 	dpm->detect(*image, dobjects);
 
-	for (auto it = dobjects.begin(); it != dobjects.end(); ++it)
+	for (std::vector<DPMDetector::ObjectDetection>::iterator it = dobjects.begin(); it != dobjects.end(); ++it)
 	{
 		rects->push_back(it->rect);
 		scores->push_back(it->score);
