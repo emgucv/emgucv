e48b99ce521887de3e423e2c1c3ea0797887c96b
Canming Huang <support@emgu.com>
2017-06-21 09:42:07 -0400

    Added OCR sample for Xamarin forms.

--- Emgu.CV.Example/XamarinForms/Core/App.cs
+++ Emgu.CV.Example/XamarinForms/Core/App.cs
@@ -11,113 +11,121 @@ using Xamarin.Forms;
 
 namespace Emgu.CV.XamarinForms
 {
-	public class App : Application
-	{
-		public App ()
-		{
-         Button helloWorldButton = new Button();
-		   helloWorldButton.Text = "Hello world";
+    public class App : Application
+    {
+        public App()
+        {
+            Button helloWorldButton = new Button();
+            helloWorldButton.Text = "Hello world";
 
-         Button planarSubdivisionButton = new Button();
-		   planarSubdivisionButton.Text = "Planar Subdivision";
+            Button planarSubdivisionButton = new Button();
+            planarSubdivisionButton.Text = "Planar Subdivision";
 
-         Button faceDetectionButton = new Button();
-		   faceDetectionButton.Text = "Face Detection";
+            Button faceDetectionButton = new Button();
+            faceDetectionButton.Text = "Face Detection";
 
-         Button featureDetectionButton = new Button();
-		   featureDetectionButton.Text = "Feature Matching";
+            Button featureDetectionButton = new Button();
+            featureDetectionButton.Text = "Feature Matching";
 
-         Button pedestrianDetectionButton = new Button();
-		   pedestrianDetectionButton.Text = "Pedestrian Detection";
+            Button pedestrianDetectionButton = new Button();
+            pedestrianDetectionButton.Text = "Pedestrian Detection";
 
-			// The root page of your application
-		   ContentPage page =
-		      new ContentPage
-		      {
-		         Content = new StackLayout
-		         {
-		            VerticalOptions = LayoutOptions.Start,
-		            Children =
-		            {
-		               helloWorldButton,
-		               planarSubdivisionButton,
-		               faceDetectionButton,
+            Button ocrButton = new Button();
+            ocrButton.Text = "OCR";
+
+            // The root page of your application
+            ContentPage page =
+              new ContentPage
+              {
+                  Content = new StackLayout
+                  {
+                      VerticalOptions = LayoutOptions.Start,
+                      Children =
+                    {
+                       helloWorldButton,
+                       planarSubdivisionButton,
+                       faceDetectionButton,
                      featureDetectionButton,
-                     pedestrianDetectionButton
-		            }
-		         }
-		      };
+                     pedestrianDetectionButton,
+                     ocrButton
+                    }
+                  }
+              };
 
 #if NETFX_CORE
 		   String aboutIcon = "questionmark.png";
 #else
-		   String aboutIcon = null;
+            String aboutIcon = null;
 #endif
 
-		   MainPage = 
-            new NavigationPage(
-               page
-            );
+            MainPage =
+             new NavigationPage(
+                page
+             );
 
-         ToolbarItem aboutItem = new ToolbarItem("About", aboutIcon,
-            () =>
-            {
-               MainPage.Navigation.PushAsync(new AboutPage());
-               //page.DisplayAlert("Emgu CV Examples", "App version: ...", "Ok");
+            ToolbarItem aboutItem = new ToolbarItem("About", aboutIcon,
+               () =>
+               {
+                   MainPage.Navigation.PushAsync(new AboutPage());
+                //page.DisplayAlert("Emgu CV Examples", "App version: ...", "Ok");
             }
-         );
-         page.ToolbarItems.Add(aboutItem);
+            );
+            page.ToolbarItems.Add(aboutItem);
 
-         helloWorldButton.Clicked += (sender, args) =>
-         {
-            MainPage.Navigation.PushAsync(new HelloWorldPage());
-         };
+            helloWorldButton.Clicked += (sender, args) =>
+            {
+                MainPage.Navigation.PushAsync(new HelloWorldPage());
+            };
 
-		   planarSubdivisionButton.Clicked += (sender, args) =>
-		   {
-		      MainPage.Navigation.PushAsync(new PlanarSubdivisionPage());
-		   };
+            planarSubdivisionButton.Clicked += (sender, args) =>
+            {
+                MainPage.Navigation.PushAsync(new PlanarSubdivisionPage());
+            };
 
-		   faceDetectionButton.Clicked += (sender, args) =>
-		   {
-		      MainPage.Navigation.PushAsync(new FaceDetectionPage());
-		   };
+            faceDetectionButton.Clicked += (sender, args) =>
+            {
+                MainPage.Navigation.PushAsync(new FaceDetectionPage());
+            };
 
-		   pedestrianDetectionButton.Clicked += (sender, args) =>
-		   {
-		      MainPage.Navigation.PushAsync(new PedestrianDetectionPage());
-		   };
+            pedestrianDetectionButton.Clicked += (sender, args) =>
+            {
+                MainPage.Navigation.PushAsync(new PedestrianDetectionPage());
+            };
 
-         featureDetectionButton.Clicked += (sender, args) =>
-         {
-            MainPage.Navigation.PushAsync(new FeatureMatchingPage());
-         };
-        
-		}
+            featureDetectionButton.Clicked += (sender, args) =>
+            {
+                MainPage.Navigation.PushAsync(new FeatureMatchingPage());
+            };
 
-	   public Page CurrentPage
-	   {
-	      get
-	      {
-	         NavigationPage np = MainPage as NavigationPage;
-	         return np.CurrentPage;
-	      }
-	   }
+            ocrButton.Clicked += (sender, args) =>
+            {
+                MainPage.Navigation.PushAsync(new OcrPage());
+            };
+        }
+
+        public Page CurrentPage
+        {
+            get
+            {
+                NavigationPage np = MainPage as NavigationPage;
+                return np.CurrentPage;
+            }
+        }
 
 
-      protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
 
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
 
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
-		}
-	}
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
 }
---
+++ Emgu.CV.Example/XamarinForms/Core/OcrPage.cs
@@ -0,0 +1,109 @@
+﻿//----------------------------------------------------------------------------
+//  Copyright (C) 2004-2017 by EMGU Corporation. All rights reserved.       
+//----------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Drawing;
+using System.Threading.Tasks;
+#if __ANDROID__
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Graphics;
+using Android.Preferences;
+#endif
+
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.OCR;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using FaceDetection;
+
+namespace Emgu.CV.XamarinForms
+{
+    public class OcrPage : ButtonTextImagePage
+    {
+        private Tesseract _ocr;
+        private static void TesseractDownloadLangFile(String folder, String lang)
+        {
+            String subfolderName = "tessdata";
+            String folderName = System.IO.Path.Combine(folder, subfolderName);
+            if (!System.IO.Directory.Exists(folderName))
+            {
+                System.IO.Directory.CreateDirectory(folderName);
+            }
+            String dest = System.IO.Path.Combine(folderName, String.Format("{0}.traineddata", lang));
+            if (!System.IO.File.Exists(dest))
+                using (System.Net.WebClient webclient = new System.Net.WebClient())
+                {
+                    String source =
+                        String.Format("https://github.com/tesseract-ocr/tessdata/blob/4592b8d453889181e01982d22328b5846765eaad/{0}.traineddata?raw=true", lang);
+
+                    Console.WriteLine(String.Format("Downloading file from '{0}' to '{1}'", source, dest));
+                    webclient.DownloadFile(source, dest);
+                    Console.WriteLine(String.Format("Download completed"));
+                }
+        }
+
+
+        public OcrPage()
+         : base()
+        {
+
+            var button = this.GetButton();
+            button.Text = "Perform Text Detection";
+            button.Clicked += OnButtonClicked;
+
+            OnImagesLoaded += async (sender, image) =>
+            {
+                GetLabel().Text = "Please wait...";
+                SetImage(null);
+
+                Task<Tuple<Mat, String, long>> t = new Task<Tuple<Mat, String, long>>(
+                  () =>
+                  {
+                      String lang = "eng";
+#if __ANDROID__
+                      String path = System.IO.Path.Combine(Android.OS.Environment.ExternalStorageDirectory.AbsolutePath,
+                             Android.OS.Environment.DirectoryDownloads);
+#else
+                      String path = ".";
+#endif
+
+                      TesseractDownloadLangFile(path, lang);
+                      TesseractDownloadLangFile(path, "osd"); //script orientation detection
+                      String pathFinal = path.Length == 0 || path.Substring(path.Length - 1, 1).Equals(System.IO.Path.DirectorySeparatorChar.ToString())
+                             ? path
+                             : String.Format("{0}{1}", path, System.IO.Path.DirectorySeparatorChar);
+                      if (_ocr == null)
+                          _ocr = new Tesseract(pathFinal, lang, OcrEngineMode.TesseractOnly);
+                      _ocr.SetImage(image[0]);
+                      _ocr.Recognize();
+                      //GetLabel().Text = _ocr.GetUTF8Text();
+                      long time = 0;
+
+                      return new Tuple<Mat, String, long>(image[0], _ocr.GetUTF8Text(), time);
+                  });
+                t.Start();
+
+                var result = await t;
+                SetImage(t.Result.Item1);
+                String computeDevice = CvInvoke.UseOpenCL ? "OpenCL: " + Ocl.Device.Default.Name : "CPU";
+                GetLabel().Text = t.Result.Item2;
+            };
+        }
+
+        private void OnButtonClicked(Object sender, EventArgs args)
+        {
+            LoadImages(new string[] { "lena.jpg" });
+        }
+
+    }
+}
