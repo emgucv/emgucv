6e6937473757c752d9f090f461e2e462971d584d
Canming Huang <support@emgu.com>
2017-06-20 21:44:35 -0400

    rename libemgucv.a to libcvextern.a

--- Emgu.CV/PInvoke/iOS/libcvextern.linkwith.cs
+++ Emgu.CV/PInvoke/iOS/libcvextern.linkwith.cs
@@ -8,7 +8,7 @@ using System;
 using ObjCRuntime;
 
 [assembly: LinkWith (
-   "libemgucv.a", 
+   "libcvextern.a", 
    LinkTarget.ArmV7s | LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.Arm64 | LinkTarget.Simulator64, 
    ForceLoad = true, 
    Frameworks="Foundation Accelerate CoreFoundation CoreGraphics AssetsLibrary AVFoundation CoreImage CoreMedia CoreVideo QuartzCore ImageIO", 
