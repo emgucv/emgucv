363380237e78ec79cf838c34bf0adb116db8bbfb
Canming Huang <support@emgu.com>
2017-06-27 15:52:52 -0400

    Fix iOS build

--- platforms/ios/build.sh
+++ platforms/ios/build.sh
@@ -18,7 +18,7 @@ if [ "$1" != "simulator" ]; then
     xcodebuild IPHONEOS_DEPLOYMENT_TARGET=6.0 -parallelizeTargets -jobs 8 -sdk iphoneos -configuration Release ARCHS="armv7s" -target ALL_BUILD clean build
     mkdir -p platforms/ios/armv7s 
     #cp -r lib/Release/* ios/armv7s/
-    cp -r bin/Release/* platforms/ios/armv7s/
+    cp -r libs/Release/* platforms/ios/armv7s/
     cp -r opencv/3rdparty/lib/Release/* platforms/ios/armv7s/
     #cp -r opencv/lib/Release/* ios/armv7/
     cd platforms/ios/armv7s
@@ -32,7 +32,7 @@ if [ "$1" != "simulator" ]; then
     xcodebuild IPHONEOS_DEPLOYMENT_TARGET=6.0 -parallelizeTargets -jobs 8 -sdk iphoneos -configuration Release ARCHS="armv7" -target ALL_BUILD clean build
     mkdir -p platforms/ios/armv7 
     #cp -r lib/Release/* ios/armv7/
-    cp -r bin/Release/* platforms/ios/armv7/
+    cp -r libs/Release/* platforms/ios/armv7/
     cp -r opencv/3rdparty/lib/Release/* platforms/ios/armv7/
     #cp -r opencv/lib/Release/* ios/armv7/
     cd platforms/ios/armv7
@@ -46,7 +46,7 @@ if [ "$1" != "simulator" ]; then
     xcodebuild IPHONEOS_DEPLOYMENT_TARGET=6.0 -parallelizeTargets -jobs 8 -sdk iphoneos -configuration Release ARCHS="arm64" -target ALL_BUILD clean build
     mkdir -p platforms/ios/arm64 
     #cp -r lib/Release/* ios/arm64/
-    cp -r bin/Release/* platforms/ios/arm64/
+    cp -r libs/Release/* platforms/ios/arm64/
     cp -r opencv/3rdparty/lib/Release/* platforms/ios/arm64/
     #cp -r opencv/lib/Release/* ios/arm64/
     cd platforms/ios/arm64
@@ -66,7 +66,7 @@ rm -rf platforms/ios/i386 bin/Release opencv/3rdparty/lib/Release
 xcodebuild IPHONEOS_DEPLOYMENT_TARGET=6.0 -parallelizeTargets -jobs 8 -sdk iphonesimulator -configuration Release ARCHS="i386" -target ALL_BUILD clean build
 mkdir -p platforms/ios/i386
 #cp -r lib/Release/* ios/i386/
-cp -r bin/Release/* platforms/ios/i386/
+cp -r libs/Release/* platforms/ios/i386/
 #cp -r opencv/lib/Release/* ios/i386/
 cp -r opencv/3rdparty/lib/Release/* platforms/ios/i386/
 cd platforms/ios/i386
@@ -84,7 +84,7 @@ rm -rf platforms/ios/x86_64 bin/Release opencv/3rdparty/lib/Release
 xcodebuild IPHONEOS_DEPLOYMENT_TARGET=6.0 WARNING_CFLAGS=-Wno-implicit-function-declaration -parallelizeTargets -jobs 8 -sdk iphonesimulator -configuration Release ARCHS="x86_64" -target ALL_BUILD clean build
 mkdir -p platforms/ios/x86_64
 #cp -r lib/Release/* ios/x86_64/
-cp -r bin/Release/* platforms/ios/x86_64/
+cp -r libs/Release/* platforms/ios/x86_64/
 #cp -r opencv/lib/Release/* ios/x86_64/
 cp -r opencv/3rdparty/lib/Release/* platforms/ios/x86_64/
 cd platforms/ios/x86_64
