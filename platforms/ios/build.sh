100f3d1d77b145166153c6a81703f84708e7f72a
Canming Huang <support@emgu.com>
2017-07-18 18:28:54 -0400

    update ios build script

--- platforms/ios/build.sh
+++ platforms/ios/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+rm cmake
 if [ ! -f cmake ]
 then 
   ln -s ../../opencv/platforms/ios/cmake cmake
@@ -8,98 +9,72 @@ fi
 
 cd ../..
 
-
 if [ "$1" != "simulator" ]; then    
 
-    rm -f CMakeCache.txt
-    rm -rf CMakeFiles CMakeScripts
-    platforms/ios/configure-device_xcode.sh -DIOS_ARCH="armv7s" $*
-    rm -rf platforms/ios/armv7s bin/Release opencv/3rdparty/lib/Release 
-    xcodebuild IPHONEOS_DEPLOYMENT_TARGET=6.0 -parallelizeTargets -jobs 8 -sdk iphoneos -configuration Release ARCHS="armv7s" -target ALL_BUILD clean build
-    mkdir -p platforms/ios/armv7s 
-    #cp -r lib/Release/* ios/armv7s/
-    cp -r bin/Release/* platforms/ios/armv7s/
-    cp -r libs/Release/* platforms/ios/armv7s/
-    cp -r opencv/3rdparty/lib/Release/* platforms/ios/armv7s/
-    #cp -r opencv/lib/Release/* ios/armv7/
+    mkdir -p platforms/ios/armv7s     
     cd platforms/ios/armv7s
-    libtool -static -o libemgucv_armv7s.a *.a
+    ../configure-device_xcode.sh -DIOS_ARCH="armv7s" $*
+    xcodebuild IPHONEOS_DEPLOYMENT_TARGET=6.0 -parallelizeTargets -jobs 8 -sdk iphoneos -configuration Release ARCHS="armv7s" -target ALL_BUILD clean build
+    cp -r ../../../libs/Release/* bin/Release
+    cp -r opencv/3rdparty/lib/Release/* bin/Release  
+    libtool -static -o libemgucv_armv7s.a bin/Release/*.a
     cd ../../..
     
-    rm -f CMakeCache.txt
-    rm -rf CMakeFiles CMakeScripts
-    platforms/ios/configure-device_xcode.sh -DIOS_ARCH="armv7" $*
-    rm -rf platforms/ios/armv7 bin/Release opencv/3rdparty/lib/Release
-    xcodebuild IPHONEOS_DEPLOYMENT_TARGET=6.0 -parallelizeTargets -jobs 8 -sdk iphoneos -configuration Release ARCHS="armv7" -target ALL_BUILD clean build
-    mkdir -p platforms/ios/armv7 
-    #cp -r lib/Release/* ios/armv7/
-    cp -r bin/Release/* platforms/ios/armv7/
-    cp -r libs/Release/* platforms/ios/armv7/
-    cp -r opencv/3rdparty/lib/Release/* platforms/ios/armv7/
-    #cp -r opencv/lib/Release/* ios/armv7/
+    mkdir -p platforms/ios/armv7
     cd platforms/ios/armv7
-    libtool -static -o libemgucv_armv7.a *.a
+    ../configure-device_xcode.sh -DIOS_ARCH="armv7" $*
+    xcodebuild IPHONEOS_DEPLOYMENT_TARGET=6.0 -parallelizeTargets -jobs 8 -sdk iphoneos -configuration Release ARCHS="armv7" -target ALL_BUILD clean build
+    cp -r ../../../libs/Release/* bin/Release
+    cp -r opencv/3rdparty/lib/Release/* bin/Release  
+    libtool -static -o libemgucv_armv7.a bin/Release/*.a
     cd ../../..
 
-    rm -f CMakeCache.txt
-    rm -rf CMakeFiles CMakeScripts
-    platforms/ios/configure-device_xcode.sh -DIOS_ARCH="arm64" $*
-    rm -rf platforms/ios/arm64 bin/Release opencv/3rdparty/lib/Release
-    xcodebuild IPHONEOS_DEPLOYMENT_TARGET=6.0 -parallelizeTargets -jobs 8 -sdk iphoneos -configuration Release ARCHS="arm64" -target ALL_BUILD clean build
-    mkdir -p platforms/ios/arm64 
-    #cp -r lib/Release/* ios/arm64/
-    cp -r bin/Release/* platforms/ios/arm64/
-    cp -r libs/Release/* platforms/ios/arm64/
-    cp -r opencv/3rdparty/lib/Release/* platforms/ios/arm64/
-    #cp -r opencv/lib/Release/* ios/arm64/
+    mkdir -p platforms/ios/arm64
     cd platforms/ios/arm64
-    libtool -static -o libemgucv_arm64.a *.a
+    ../configure-device_xcode.sh -DIOS_ARCH="arm64" $*
+    xcodebuild IPHONEOS_DEPLOYMENT_TARGET=6.0 -parallelizeTargets -jobs 8 -sdk iphoneos -configuration Release ARCHS="arm64" -target ALL_BUILD clean build
+    cp -r ../../../libs/Release/* bin/Release
+    cp -r opencv/3rdparty/lib/Release/* bin/Release  
+    libtool -static -o libemgucv_arm64.a bin/Release/*.a
     cd ../../..
-
 fi
 
-rm -f CMakeCache.txt
-rm -rf CMakeFiles CMakeScripts
-if [ "$1" == "simulator" ]; then    
-  platforms/ios/configure-simulator_xcode.sh -DIOS_ARCH="i386" ${@:2}
-else
-  platforms/ios/configure-simulator_xcode.sh -DIOS_ARCH="i386" $*
-fi
-rm -rf platforms/ios/i386 bin/Release opencv/3rdparty/lib/Release
-xcodebuild IPHONEOS_DEPLOYMENT_TARGET=6.0 -parallelizeTargets -jobs 8 -sdk iphonesimulator -configuration Release ARCHS="i386" -target ALL_BUILD clean build
+
 mkdir -p platforms/ios/i386
-#cp -r lib/Release/* ios/i386/
-cp -r bin/Release/* platforms/ios/i386/
-cp -r libs/Release/* platforms/ios/i386/
-#cp -r opencv/lib/Release/* ios/i386/
-cp -r opencv/3rdparty/lib/Release/* platforms/ios/i386/
 cd platforms/ios/i386
-libtool -static -o libemgucv_i386.a *.a
+if [ "$1" == "simulator" ]; then    
+  ../configure-simulator_xcode.sh -DIOS_ARCH="i386" ${@:2}
+else
+  ../configure-simulator_xcode.sh -DIOS_ARCH="i386" $*
+fi
+xcodebuild IPHONEOS_DEPLOYMENT_TARGET=6.0 -parallelizeTargets -jobs 8 -sdk iphonesimulator -configuration Release ARCHS="i386" -target ALL_BUILD clean build
+cp -r ../../../libs/Release/* bin/Release
+cp -r opencv/3rdparty/lib/Release/* bin/Release  
+cp -r opencv/3rdparty/ippicv/ippiw_mac/lib/ia32/* bin/Release
+cp -r opencv/3rdparty/ippicv/ippicv_mac/lib/ia32/* bin/Release
+libtool -static -o libemgucv_i386.a bin/Release/*.a
 cd ../../..
 
-rm -f CMakeCache.txt
-rm -rf CMakeFiles CMakeScripts
-if [ "$1" == "simulator" ]; then    
-  platforms/ios/configure-simulator_xcode.sh -DIOS_ARCH="x86_64" ${@:2}
-else
-  platforms/ios/configure-simulator_xcode.sh -DIOS_ARCH="x86_64" $*
-fi
-rm -rf platforms/ios/x86_64 bin/Release opencv/3rdparty/lib/Release
-xcodebuild IPHONEOS_DEPLOYMENT_TARGET=6.0 WARNING_CFLAGS=-Wno-implicit-function-declaration -parallelizeTargets -jobs 8 -sdk iphonesimulator -configuration Release ARCHS="x86_64" -target ALL_BUILD clean build
 mkdir -p platforms/ios/x86_64
-#cp -r lib/Release/* ios/x86_64/
-cp -r bin/Release/* platforms/ios/x86_64/
-cp -r libs/Release/* platforms/ios/x86_64/
-#cp -r opencv/lib/Release/* ios/x86_64/
-cp -r opencv/3rdparty/lib/Release/* platforms/ios/x86_64/
 cd platforms/ios/x86_64
-libtool -static -o libemgucv_x86_64.a *.a
+if [ "$1" == "simulator" ]; then    
+  ../configure-simulator_xcode.sh -DIOS_ARCH="x86_64" ${@:2}
+else
+  ../configure-simulator_xcode.sh -DIOS_ARCH="x86_64" $*
+fi
+xcodebuild IPHONEOS_DEPLOYMENT_TARGET=6.0 WARNING_CFLAGS=-Wno-implicit-function-declaration -parallelizeTargets -jobs 8 -sdk iphonesimulator -configuration Release ARCHS="x86_64" -target ALL_BUILD clean build
+cp -r ../../../libs/Release/* bin/Release
+cp -r opencv/3rdparty/lib/Release/* bin/Release
+cp -r opencv/3rdparty/ippicv/ippiw_mac/lib/intel64/* bin/Release
+cp -r opencv/3rdparty/ippicv/ippicv_mac/lib/intel64/* bin/Release
+
+libtool -static -o libemgucv_x86_64.a bin/Release/*.a
 cd ../../..
 
 rm -rf platforms/ios/universal
 mkdir -p platforms/ios/universal
 if [ "$1" == "simulator" ]; then
-    lipo -create -output platforms/ios/universal/libemgucv.a  platforms/ios/i386/libemgucv_i386.a  platforms/ios/x86_64/libemgucv_x86_64.a
+    lipo -create -output platforms/ios/universal/libemgucv.a platforms/ios/i386/libemgucv_i386.a platforms/ios/x86_64/libemgucv_x86_64.a
 else
     lipo -create -output platforms/ios/universal/libemgucv.a platforms/ios/armv7/libemgucv_armv7.a platforms/ios/armv7s/libemgucv_armv7s.a platforms/ios/arm64/libemgucv_arm64.a platforms/ios/i386/libemgucv_i386.a  platforms/ios/x86_64/libemgucv_x86_64.a
 fi
--- platforms/ios/configure-simulator_xcode.sh
+++ platforms/ios/configure-simulator_xcode.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -e
-cmake -GXcode -DCMAKE_TOOLCHAIN_FILE=./platforms/ios/cmake/Toolchains/Toolchain-iPhoneSimulator_Xcode.cmake -DCMAKE_INSTALL_PREFIX=../OpenCV_iPhoneSimulator -DOPENCV_EXTRA_MODULES_PATH=opencv_contrib/modules -DBUILD_opencv_hdf:BOOL=FALSE -DBUILD_SHARED_LIBS:BOOL=FALSE -DBUILD_PERF_TESTS:BOOL=FALSE -DBUILD_TESTS:BOOL=FALSE -DBUILD_opencv_apps:BOOL=FALSE $* ./ 
+cmake -GXcode -DCMAKE_TOOLCHAIN_FILE=`dirname $0`/cmake/Toolchains/Toolchain-iPhoneSimulator_Xcode.cmake -DCMAKE_INSTALL_PREFIX=../OpenCV_iPhoneSimulator -DOPENCV_EXTRA_MODULES_PATH=`dirname $0`/../../opencv_contrib/modules -DBUILD_opencv_hdf:BOOL=FALSE -DBUILD_SHARED_LIBS:BOOL=FALSE -DBUILD_PERF_TESTS:BOOL=FALSE -DBUILD_TESTS:BOOL=FALSE -DBUILD_opencv_apps:BOOL=FALSE $* `dirname $0`/../.. 
