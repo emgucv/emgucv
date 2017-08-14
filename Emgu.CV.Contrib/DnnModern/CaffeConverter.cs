ce4473fabae670487c93907e202c1d3ae2b907e1
Canming Huang <support@emgu.com>
2017-08-14 10:05:22 -0400

    commented out DnnModern code.

--- Emgu.CV.Contrib/DnnModern/CaffeConverter.cs
+++ Emgu.CV.Contrib/DnnModern/CaffeConverter.cs
@@ -2,12 +2,12 @@
 //  Copyright (C) 2004-2017 by EMGU Corporation. All rights reserved.       
 //----------------------------------------------------------------------------
 
+/*
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Runtime.InteropServices;
 using Emgu.CV.Structure;
-using Emgu.CV.Text;
 using Emgu.CV.Util;
 using Emgu.Util;
 using System.Diagnostics;
@@ -27,3 +27,4 @@ namespace Emgu.CV.Dnn2
 
    }
 }
+*/
