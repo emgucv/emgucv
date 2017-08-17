9d9af57d0dd5a4cef1dd579838116cbbc7856c4e
Canming Huang <canming@brightappsllc.com>
2017-08-17 15:34:58 -0400

    unity updates.

---
+++ Emgu.CV.Unity/demo/MenuManagerScript.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManagerScript : MonoBehaviour {
+
+    public void LoadScene(String name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
