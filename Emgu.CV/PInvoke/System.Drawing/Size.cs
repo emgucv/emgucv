c8bb24fe2b07da66847df10ab4775a7f0136d527
Bernd Baumanns <familiebaumanns@googlemail.com>
2017-08-20 15:35:22 +0200

    removed may conflicting System.Drawing types (these can be found in nuget package System.Drawing.Primitives)

--- Emgu.CV/PInvoke/System.Drawing/Size.cs
+++ Emgu.CV/PInvoke/System.Drawing/Size.cs
@@ -2,7 +2,7 @@
 //  Copyright (C) 2004-2017 by EMGU Corporation. All rights reserved.       
 //----------------------------------------------------------------------------
 
-#if (NETFX_CORE || NETSTANDARD1_4 || UNITY_EDITOR || UNITY_IOS || UNITY_ANDROID || UNITY_STANDALONE)
+#if (NETFX_CORE || UNITY_EDITOR || UNITY_IOS || UNITY_ANDROID || UNITY_STANDALONE)
 using System;
 using System.Runtime.InteropServices;
 
