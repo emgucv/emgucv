e3301aba31faed00853c9b507c89f63650d7fd31
Canming Huang <support@emgu.com>
2018-08-11 06:04:50 +0900

    Include dnn_objdetect.

---
+++ Emgu.CV.Extern/dnn_objdetect/dnn_objdetect_c.h
@@ -0,0 +1,18 @@
+//----------------------------------------------------------------------------
+//
+//  Copyright (C) 2004-2018 by EMGU Corporation. All rights reserved.
+//
+//----------------------------------------------------------------------------
+
+#pragma once
+#ifndef EMGU_DNN_OBJDETECT_C_H
+#define EMGU_DNN_OBJDETECT_C_H
+
+#include "opencv2/core/core_c.h"
+#include "opencv2/core_detect.hpp"
+
+CVAPI(cv::dnn_objdetect::InferBbox*) cveInferBboxCreate(cv::Mat* deltaBbox, cv::Mat* classScores, cv::Mat* confScores);
+CVAPI(void) cveInferBboxFilter(cv::dnn_objdetect::InferBbox* inferBbox, double thresh);
+CVAPI(void) cveInferBboxRelease(cv::dnn_objdetect::InferBbox** inferBbox);
+
+#endif
