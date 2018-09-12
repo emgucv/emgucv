0ee48dc330471157f926741c635d120bebaf882e
Canming Huang <support@emgu.com>
2018-09-11 23:35:14 -0400

    Fix the Android permission problems, update to the latest nuget packages.

--- Emgu.CV.Example/Android/AndroidExamplesActivity.cs
+++ Emgu.CV.Example/Android/AndroidExamplesActivity.cs
@@ -3,160 +3,191 @@
 //----------------------------------------------------------------------------
 
 using System;
-
+using Android;
 using Android.App;
 using Android.Content;
+using Android.Content.PM;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
 using Emgu.CV;
 using Emgu.CV.Cuda;
+using Plugin.CurrentActivity;
 
 namespace AndroidExamples
 {
-   [Activity(Label = "Emgu CV Examples", MainLauncher = true)]
-   public class AndroidExamplesActivity : Activity
-   {
-      private IMenuItem _settingsMenu;
-      private IMenuItem _aboutUsMenu;
+    [Activity(Label = "Emgu CV Examples", MainLauncher = true)]
+    public class AndroidExamplesActivity : Activity
+    {
+        private IMenuItem _settingsMenu;
+        private IMenuItem _aboutUsMenu;
 
-      protected override void OnCreate(Bundle bundle)
-      {
-         base.OnCreate(bundle);
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
 
-         // Set our view from the "main" layout resource
-         SetContentView(Resource.Layout.AndroidExamples);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.AndroidExamples);
 
-         /*
-         // Get our buttons from the layout resource,
-         // and attach events to it
-         Button helloWorldButton = FindViewById<Button>(Resource.Id.GotoHelloWorldButton);
-         helloWorldButton.Click += delegate
-         {
-            StartActivity(typeof(HelloWorldActivity));
-         };*/
+            CrossCurrentActivity.Current.Init(this, bundle);
 
-         Button plannarSubdivisionButton = FindViewById<Button>(Resource.Id.GotoPlanarSubdivisionButton);
-         plannarSubdivisionButton.Click += delegate
-         {
-            StartActivity(typeof(PlanarSubdivisionActivity));
-         };
+            CheckAppPermissions();
 
-         Button featureMatchingButton = FindViewById<Button>(Resource.Id.GotoFeatureMatchingButton);
-         featureMatchingButton.Click += delegate 
-         {
-            StartActivity(typeof(FeatureMatchingActivity));
-         };
 
-         Button pedestrianDetectionButton = FindViewById<Button>(Resource.Id.GotoPedestrianDetectionButton);
-         pedestrianDetectionButton.Click += delegate
-         {
-            StartActivity(typeof(PedestrianDetectionActivity));
-         };
-
-         Button faceDetectionButton = FindViewById<Button>(Resource.Id.GotoFaceDetectionButton);
-         faceDetectionButton.Click += delegate
-         {
-            StartActivity(typeof(FaceDetectionActivity));
-         };
-
-         Button trafficSignRecognitionButton = FindViewById<Button>(Resource.Id.GotoTrafficSignRecognitionButton);
-         trafficSignRecognitionButton.Click += delegate
-         {
-            StartActivity(typeof(TrafficSignRecognitionActivity));
-         };
-
-         Button cameraButton = FindViewById<Button>(Resource.Id.GotoCameraButton);
-         if (Android.Hardware.Camera.NumberOfCameras > 0)
-         {
-            cameraButton.Click += delegate
+            /*
+            // Get our buttons from the layout resource,
+            // and attach events to it
+            Button helloWorldButton = FindViewById<Button>(Resource.Id.GotoHelloWorldButton);
+            helloWorldButton.Click += delegate
             {
-               StartActivity(typeof(CameraPreviewActivity));
-            };
-         }
-         else
-         {
-            cameraButton.Visibility = ViewStates.Gone;
-         }
+               StartActivity(typeof(HelloWorldActivity));
+            };*/
 
-          Button camera2Button = FindViewById<Button>(Resource.Id.GotoCamera2Button);
-          if (Android.Hardware.Camera.NumberOfCameras > 0)
-          {
-              camera2Button.Click += delegate
-              {
-                  StartActivity(typeof(Camera2Activity));
-              };
-          }
-          else
-          {
-              camera2Button.Visibility = ViewStates.Gone;
-          }
+            Button plannarSubdivisionButton = FindViewById<Button>(Resource.Id.GotoPlanarSubdivisionButton);
+            plannarSubdivisionButton.Click += delegate
+            {
+                StartActivity(typeof(PlanarSubdivisionActivity));
+            };
+
+            Button featureMatchingButton = FindViewById<Button>(Resource.Id.GotoFeatureMatchingButton);
+            featureMatchingButton.Click += delegate
+            {
+                StartActivity(typeof(FeatureMatchingActivity));
+            };
+
+            Button pedestrianDetectionButton = FindViewById<Button>(Resource.Id.GotoPedestrianDetectionButton);
+            pedestrianDetectionButton.Click += delegate
+            {
+                StartActivity(typeof(PedestrianDetectionActivity));
+            };
+
+            Button faceDetectionButton = FindViewById<Button>(Resource.Id.GotoFaceDetectionButton);
+            faceDetectionButton.Click += delegate
+            {
+                StartActivity(typeof(FaceDetectionActivity));
+            };
+
+            Button trafficSignRecognitionButton = FindViewById<Button>(Resource.Id.GotoTrafficSignRecognitionButton);
+            trafficSignRecognitionButton.Click += delegate
+            {
+                StartActivity(typeof(TrafficSignRecognitionActivity));
+            };
+
+            Button cameraButton = FindViewById<Button>(Resource.Id.GotoCameraButton);
+            if (Android.Hardware.Camera.NumberOfCameras > 0)
+            {
+                cameraButton.Click += delegate
+                {
+                    StartActivity(typeof(CameraPreviewActivity));
+                };
+            }
+            else
+            {
+                cameraButton.Visibility = ViewStates.Gone;
+            }
+
+            Button camera2Button = FindViewById<Button>(Resource.Id.GotoCamera2Button);
+            if (Android.Hardware.Camera.NumberOfCameras > 0)
+            {
+                camera2Button.Click += delegate
+                {
+                    StartActivity(typeof(Camera2Activity));
+                };
+            }
+            else
+            {
+                camera2Button.Visibility = ViewStates.Gone;
+            }
 
             Button licensePlateRecognitionButton = FindViewById<Button>(Resource.Id.GotoLicensePlateRecognitionButton);
-         licensePlateRecognitionButton.Click += delegate
-         {
-            StartActivity(typeof(LicensePlateRecognitionActivity));
-         };
-      }
-
-      public override bool OnCreateOptionsMenu(IMenu menu)
-      {
-         _aboutUsMenu = menu.Add(Resources.GetString(Resource.String.menu_option_about_us));
-         _settingsMenu = menu.Add(Resources.GetString(Resource.String.menu_option_settings));
-         return base.OnCreateOptionsMenu(menu);
-      }
-
-      public override bool OnOptionsItemSelected(IMenuItem item)
-      {
-         if (item == _aboutUsMenu)
-         {
-            Display display = WindowManager.DefaultDisplay;
-            Android.Util.DisplayMetrics metrics = new Android.Util.DisplayMetrics();
-            display.GetMetrics(metrics);
-            int width = (int)Math.Min(display.Width * 0.8, 360 * metrics.Density);
-            int height = (int)Math.Min(display.Height * 0.8, 480 * metrics.Density);
-
-            PopupWindow aboutWindow = new PopupWindow(LayoutInflater.Inflate(Resource.Layout.about_us, null, false), width, height);
-            TextView appVersionText = aboutWindow.ContentView.FindViewById<TextView>(Resource.Id.AboutUsVersionTextView);
-            appVersionText.Text = String.Format("{0}: {1}",
-               Resources.GetString(Resource.String.application_version),
-               this.PackageManager.GetPackageInfo(PackageName, Android.Content.PM.PackageInfoFlags.Activities).VersionName);
-
-            Button closeButton = aboutWindow.ContentView.FindViewById<Button>(Resource.Id.AboutUsCloseButton);
-            closeButton.Click += delegate
+            licensePlateRecognitionButton.Click += delegate
             {
-               aboutWindow.Dismiss();
+                StartActivity(typeof(LicensePlateRecognitionActivity));
             };
+        }
 
-            TextView aboutUsNoteTextView =
-               aboutWindow.ContentView.FindViewById<TextView>(Resource.Id.AboutUsModuleInfoTextView);
-            //Emgu.CV.Util.VectorOfOclPlatformInfo oclInfo = Emgu.CV.OclInvoke.GetPlatformInfo();
-            
-            String txt = String.Format("Has OpenCL: {0}", CvInvoke.HaveOpenCL);
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            _aboutUsMenu = menu.Add(Resources.GetString(Resource.String.menu_option_about_us));
+            _settingsMenu = menu.Add(Resources.GetString(Resource.String.menu_option_settings));
+            return base.OnCreateOptionsMenu(menu);
+        }
 
-            if (CvInvoke.HaveOpenCL)
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            if (item == _aboutUsMenu)
             {
-               txt = String.Format("{0}{1}Use OpenCL: {2}{3}{4}{5}",
-                  txt, System.Environment.NewLine,
-                  CvInvoke.UseOpenCL, System.Environment.NewLine,
-                  CvInvoke.OclGetPlatformsSummary(), System.Environment.NewLine);
+                Display display = WindowManager.DefaultDisplay;
+                Android.Util.DisplayMetrics metrics = new Android.Util.DisplayMetrics();
+                display.GetMetrics(metrics);
+                int width = (int)Math.Min(display.Width * 0.8, 360 * metrics.Density);
+                int height = (int)Math.Min(display.Height * 0.8, 480 * metrics.Density);
+
+                PopupWindow aboutWindow = new PopupWindow(LayoutInflater.Inflate(Resource.Layout.about_us, null, false), width, height);
+                TextView appVersionText = aboutWindow.ContentView.FindViewById<TextView>(Resource.Id.AboutUsVersionTextView);
+                appVersionText.Text = String.Format("{0}: {1}",
+                   Resources.GetString(Resource.String.application_version),
+                   this.PackageManager.GetPackageInfo(PackageName, Android.Content.PM.PackageInfoFlags.Activities).VersionName);
+
+                Button closeButton = aboutWindow.ContentView.FindViewById<Button>(Resource.Id.AboutUsCloseButton);
+                closeButton.Click += delegate
+                {
+                    aboutWindow.Dismiss();
+                };
+
+                TextView aboutUsNoteTextView =
+                   aboutWindow.ContentView.FindViewById<TextView>(Resource.Id.AboutUsModuleInfoTextView);
+                //Emgu.CV.Util.VectorOfOclPlatformInfo oclInfo = Emgu.CV.OclInvoke.GetPlatformInfo();
+
+                String txt = String.Format("Has OpenCL: {0}", CvInvoke.HaveOpenCL);
+
+                if (CvInvoke.HaveOpenCL)
+                {
+                    txt = String.Format("{0}{1}Use OpenCL: {2}{3}{4}{5}",
+                       txt, System.Environment.NewLine,
+                       CvInvoke.UseOpenCL, System.Environment.NewLine,
+                       CvInvoke.OclGetPlatformsSummary(), System.Environment.NewLine);
+                }
+                txt = String.Format("{0}{1}Has Cuda: {2}", txt, System.Environment.NewLine, CudaInvoke.HasCuda);
+
+
+                aboutUsNoteTextView.Text = txt;
+
+                aboutWindow.ShowAtLocation(this.Window.DecorView, GravityFlags.Center, 0, 0);
             }
-            txt = String.Format("{0}{1}Has Cuda: {2}", txt, System.Environment.NewLine, CudaInvoke.HasCuda);
-            
+            else if (item == _settingsMenu)
+            {
+                Intent settingsIntent = new Intent(this, typeof(SettingActivity));
+                StartActivity(settingsIntent);
+            }
 
-            aboutUsNoteTextView.Text = txt;
+            return base.OnOptionsItemSelected(item);
+        }
 
-            aboutWindow.ShowAtLocation(this.Window.DecorView, GravityFlags.Center, 0, 0);
-         }
-         else if (item == _settingsMenu)
-         {
-            Intent settingsIntent = new Intent(this, typeof(SettingActivity));
-            StartActivity(settingsIntent);
-         }
+        private void CheckAppPermissions()
+        {
+            if ((int)Build.VERSION.SdkInt < 23)
+            {
+                return;
+            }
+            else
+            {
+                if (PackageManager.CheckPermission(Manifest.Permission.ReadExternalStorage, PackageName) != Permission.Granted
+                    && PackageManager.CheckPermission(Manifest.Permission.WriteExternalStorage, PackageName) != Permission.Granted
+                    && PackageManager.CheckPermission(Manifest.Permission.Camera, PackageName) != Permission.Granted)
+                {
+                    var permissions = new string[] { Manifest.Permission.ReadExternalStorage, Manifest.Permission.WriteExternalStorage, Manifest.Permission.Camera };
+                    RequestPermissions(permissions, 1);
+                }
+            }
+        }
 
-         return base.OnOptionsItemSelected(item);
-      }
-   }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+    }
 }
 
--- Emgu.CV.Example/XamarinForms/Android/Properties/AssemblyInfo.cs
+++ Emgu.CV.Example/XamarinForms/Android/Properties/AssemblyInfo.cs
@@ -8,26 +8,8 @@ using Android.App;
 // associated with an assembly.
 [assembly: AssemblyTitle("Emgu.CV.XamarinForms.Droid")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Emgu.CV.XamarinForms.Droid")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
