f159a4bb6fa8523424da6dba070f7ad61767f238
Canming Huang <support@emgu.com>
2018-09-14 10:25:55 -0400

    Fix the OCR and Pedestrian detection demo for iOS

--- Emgu.CV.Example/XamarinForms/Core/OcrPage.cs
+++ Emgu.CV.Example/XamarinForms/Core/OcrPage.cs
@@ -35,8 +35,7 @@ namespace Emgu.CV.XamarinForms
         private Tesseract _ocr;
         private static void TesseractDownloadLangFile(String folder, String lang)
         {
-            String subfolderName = "tessdata";
-            String folderName = System.IO.Path.Combine(folder, subfolderName);
+         String folderName = folder;
             if (!System.IO.Directory.Exists(folderName))
             {
                 System.IO.Directory.CreateDirectory(folderName);
@@ -45,9 +44,8 @@ namespace Emgu.CV.XamarinForms
             if (!System.IO.File.Exists(dest))
                 using (System.Net.WebClient webclient = new System.Net.WebClient())
                 {
-                    String source =
-                        String.Format("https://github.com/tesseract-ocr/tessdata/blob/4592b8d453889181e01982d22328b5846765eaad/{0}.traineddata?raw=true", lang);
-
+                    String source = Emgu.CV.OCR.Tesseract.GetLangFileUrl (lang);
+                     
                     Console.WriteLine(String.Format("Downloading file from '{0}' to '{1}'", source, dest));
                     webclient.DownloadFile(source, dest);
                     Console.WriteLine(String.Format("Download completed"));
@@ -65,27 +63,28 @@ namespace Emgu.CV.XamarinForms
 
             OnImagesLoaded += async (sender, image) =>
             {
+               if (image == null || image [0] == null)
+                  return;
                 GetLabel().Text = "Please wait...";
                 SetImage(null);
 
                 Task<Tuple<Mat, String, long>> t = new Task<Tuple<Mat, String, long>>(
                   () =>
                   {
+
                       String lang = "eng";
 #if __ANDROID__
                       String path = System.IO.Path.Combine(Android.OS.Environment.ExternalStorageDirectory.AbsolutePath,
-                             Android.OS.Environment.DirectoryDownloads);
+                             Android.OS.Environment.DirectoryDownloads, "tessdata");
 #else
-                      String path = ".";
+                      String path = "./tessdata/";
 #endif
 
                       TesseractDownloadLangFile(path, lang);
                       TesseractDownloadLangFile(path, "osd"); //script orientation detection
-                      String pathFinal = path.Length == 0 || path.Substring(path.Length - 1, 1).Equals(System.IO.Path.DirectorySeparatorChar.ToString())
-                             ? path
-                             : String.Format("{0}{1}", path, System.IO.Path.DirectorySeparatorChar);
+                      
                       if (_ocr == null)
-                          _ocr = new Tesseract(pathFinal, lang, OcrEngineMode.TesseractOnly);
+                          _ocr = new Tesseract(path, lang, OcrEngineMode.TesseractOnly);
                       _ocr.SetImage(image[0]);
                       _ocr.Recognize();
                       //GetLabel().Text = _ocr.GetUTF8Text();
