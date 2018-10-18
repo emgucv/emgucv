6440be61273eb0cacf9258bb9eb08108710be1db
Canming Huang <support@emgu.com>
2018-10-18 08:59:54 -0400

    iOS build fixes.

--- platforms/ios/package.sh
+++ platforms/ios/package.sh
@@ -11,7 +11,7 @@ rm -rf ios-package
 mkdir -p ios-package/libs/iOS
 cp -f ../../libs/iOS/libcvextern.a ios-package/libs/iOS
 
-cp -r ../../tmp/Emgu.CV ios-package/Emgu.CV
+cp -r ../../Emgu.CV ios-package/Emgu.CV
 #cp -f ../../Emgu.CV/*.cs ios-package/Emgu.CV/
 #cp -f ../../Emgu.CV/Capture/*.cs ios-package/Emgu.CV/Capture
 #cp -f ../../Emgu.CV/Features2D/*.cs ios-package/Emgu.CV/Features2D
@@ -28,15 +28,19 @@ rm -rf ios-package/Emgu.CV/PInvoke/System.Drawing
 rm -rf ios-package/Emgu.CV/PInvoke/Windows.Store
 rm -rf ios-package/Emgu.CV/PInvoke/Unity
 
-cp -r ../../tmp/Emgu.CV.OCR ios-package/Emgu.CV.OCR
-cp -f ../../Emgu.CV.OCR/*.cs ios-package/Emgu.CV.OCR
+cp -r ../../Emgu.CV.OCR ios-package/Emgu.CV.OCR
+#cp -r ../../tmp/Emgu.CV.OCR ios-package/Emgu.CV.OCR
+#cp -f ../../Emgu.CV.OCR/*.cs ios-package/Emgu.CV.OCR
 
-cp -r ../../tmp/Emgu.Util ios-package/Emgu.Util
+cp -r ../../Emgu.Util ios-package/Emgu.Util
+#cp -r ../../tmp/Emgu.Util ios-package/Emgu.Util
 
-cp -r ../../tmp/Emgu.CV.Cuda ios-package/Emgu.CV.Cuda
-cp -f ../../Emgu.CV.Cuda/*.cs ios-package/Emgu.CV.Cuda/
+cp -r ../../Emgu.CV.Cuda ios-package/Emgu.CV.Cuda
+#cp -r ../../tmp/Emgu.CV.Cuda ios-package/Emgu.CV.Cuda
+#cp -f ../../Emgu.CV.Cuda/*.cs ios-package/Emgu.CV.Cuda/
 
-cp -r ../../tmp/Emgu.CV.Contrib ios-package/Emgu.CV.Contrib
+cp -r ../../Emgu.CV.Contrib ios-package/Emgu.CV.Contrib
+#cp -r ../../tmp/Emgu.CV.Contrib ios-package/Emgu.CV.Contrib
 #cp -f ../../Emgu.CV.Contrib/Plot/*.cs ios-package/Emgu.CV.Contrib/Plot
 #cp -f ../../Emgu.CV.Contrib/LineDescriptor/VectorOf*.cs ios-package/Emgu.CV.Contrib/LineDescriptor
 #cp -f ../../Emgu.CV.Contrib/Text/VectorOf*.cs ios-package/Emgu.CV.Contrib/Text
