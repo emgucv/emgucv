39620ffdde27e92e5daf933a0f282d2435a5e8bc
Canming Huang <support@emgu.com>
2018-10-23 12:21:49 -0400

    Enable DOPENCV_SKIP_VISIBILITY_HIDDEN for iOS build and disable the java & python binding projects.

--- platforms/ios/configure-device_xcode.sh
+++ platforms/ios/configure-device_xcode.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -e
-cmake -GXcode -DCMAKE_TOOLCHAIN_FILE=`dirname $0`/cmake/Toolchains/Toolchain-iPhoneOS_Xcode.cmake -DCMAKE_INSTALL_PREFIX=../OpenCV_iPhoneOS -DOPENCV_EXTRA_MODULES_PATH=`dirname $0`/../../opencv_contrib/modules -DBUILD_opencv_hdf:BOOL=FALSE -DBUILD_SHARED_LIBS:BOOL=FALSE -DBUILD_PERF_TESTS:BOOL=FALSE -DBUILD_TESTS:BOOL=FALSE -DBUILD_opencv_apps:BOOL=FALSE $* `dirname $0`/../.. 
+cmake -GXcode -DCMAKE_TOOLCHAIN_FILE=`dirname $0`/cmake/Toolchains/Toolchain-iPhoneOS_Xcode.cmake -DCMAKE_INSTALL_PREFIX=../OpenCV_iPhoneOS -DOPENCV_EXTRA_MODULES_PATH=`dirname $0`/../../opencv_contrib/modules -DBUILD_opencv_hdf:BOOL=FALSE -DBUILD_SHARED_LIBS:BOOL=FALSE -DBUILD_PERF_TESTS:BOOL=FALSE -DBUILD_TESTS:BOOL=FALSE -DBUILD_opencv_apps:BOOL=FALSE -DBUILD_opencv_java_bindings_generator:BOOL=FALSE -DBUILD_opencv_python_bindings_generator:BOOL=FALSE -DOPENCV_SKIP_VISIBILITY_HIDDEN:BOOL=TRUE $* `dirname $0`/../.. 
