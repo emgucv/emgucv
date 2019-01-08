93e263f9962f7aa46a4a323e6c407decd3c790dd
Canming Huang <support@emgu.com>
2019-01-07 22:32:26 -0500

    include Emgu.CV/videoio folder for unity package

--- Emgu.CV.Unity/sync_unity_assets.bat
+++ Emgu.CV.Unity/sync_unity_assets.bat
@@ -27,6 +27,7 @@ cp Assets/Emgu.CV/Emgu.CV/Superres/*.cs ../Emgu.CV/Superres
 #cp Assets/Emgu.CV/Emgu.CV/Tiff/*.cs ../Emgu.CV/Tiff 
 cp Assets/Emgu.CV/Emgu.CV/Util/*.cs ../Emgu.CV/Util 
 cp Assets/Emgu.CV/Emgu.CV/Video/*.cs ../Emgu.CV/Video
+cp Assets/Emgu.CV/Emgu.CV/Videoio/*.cs ../Emgu.CV/Videoio
 cp Assets/Emgu.CV/Emgu.CV/Videostab/*.cs ../Emgu.CV/Videostab
 #cp Assets/Emgu.CV/Emgu.CV/Viz/*.cs ../Emgu.CV/Viz 
 
