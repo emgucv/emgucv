4f92c31cc55d3ce4744bc26939a7827edb914b06
Canming Huang <support@emgu.com>
2019-03-04 18:05:58 -0500

    Exclude icrosoft.ApplicationInsights nuget packages.

--- Emgu.CV.Example/XamarinForms/UWP/App.xaml.cs
+++ Emgu.CV.Example/XamarinForms/UWP/App.xaml.cs
@@ -28,9 +28,6 @@ namespace WindowsUniversal
       /// </summary>
       public App()
       {
-         Microsoft.ApplicationInsights.WindowsAppInitializer.InitializeAsync(
-            Microsoft.ApplicationInsights.WindowsCollectors.Metadata |
-            Microsoft.ApplicationInsights.WindowsCollectors.Session);
          this.InitializeComponent();
          this.Suspending += OnSuspending;
       }
