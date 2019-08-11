f6442291c03f452593f8de79cc3c6ac376075630
Canming Huang <support@emgu.com>
2019-08-10 22:59:45 -0400

    Added AP3P, IPPE and IPPESquare to SolvePnpMethod

--- Emgu.CV/PInvoke/CvEnum/SolvePnpMethod.cs
+++ Emgu.CV/PInvoke/CvEnum/SolvePnpMethod.cs
@@ -16,10 +16,12 @@ namespace Emgu.CV.CvEnum
         /// </summary>
         Iterative = 0,
         /// <summary>
+        /// EPnP: Efficient Perspective-n-Point Camera Pose Estimation
         /// F.Moreno-Noguer, V.Lepetit and P.Fua "EPnP: Efficient Perspective-n-Point Camera Pose Estimation"
         /// </summary>
         EPnP = 1,
         /// <summary>
+        /// Complete Solution Classification for the Perspective-Three-Point Problem
         /// X.S. Gao, X.-R. Hou, J. Tang, H.-F. Chang; "Complete Solution Classification for the Perspective-Three-Point Problem"
         /// </summary>
         P3P = 2,
@@ -30,6 +32,24 @@ namespace Emgu.CV.CvEnum
         /// <summary>
         /// Exhaustive Linearization for Robust Camera Pose and Focal Length Estimation
         /// </summary>
-        UPnP = 4
+        UPnP = 4,
+        /// <summary>
+        /// An Efficient Algebraic Solution to the Perspective-Three-Point Problem
+        /// </summary>
+        AP3P = 5,
+        /// <summary>
+        /// Infinitesimal Plane-Based Pose Estimation. Object points must be coplanar.
+        /// </summary>
+        IPPE = 6,
+        /// <summary>
+        /// Infinitesimal Plane-Based Pose Estimation. This is a special case suitable for marker pose estimation.
+        ///  4 coplanar object points must be defined in the following order:
+        ///   - point 0: [-squareLength / 2,  squareLength / 2, 0]
+        ///   - point 1: [ squareLength / 2,  squareLength / 2, 0]
+        ///   - point 2: [ squareLength / 2, -squareLength / 2, 0]
+        ///   - point 3: [-squareLength / 2, -squareLength / 2, 0]
+        /// </summary>
+        IPPESquare = 7
+
     }
 }
