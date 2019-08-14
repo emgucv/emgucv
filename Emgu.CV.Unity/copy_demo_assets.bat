8b754cd344199ff2c9c4f2995f2a67db7f20c1a1
Canming Huang <support@emgu.com>
2019-08-14 11:37:12 -0400

    Improved unity copy / sync scripts.

--- Emgu.CV.Unity/copy_demo_assets.bat
+++ Emgu.CV.Unity/copy_demo_assets.bat
@@ -1,10 +1,4 @@
-
-cp ../opencv/samples/data/lena.jpg Assets/Emgu.CV/Demo/Resources/
-cp ../Emgu.CV.Test/stitch*.jpg Assets/Emgu.CV/Demo/Resources/
-cp ../Emgu.CV.Example/FeatureMatching/box.png Assets/Emgu.CV/Demo/Resources/
-cp ../Emgu.CV.Example/FeatureMatching/box_in_scene.png Assets/Emgu.CV/Demo/Resources/
-cp ../Emgu.CV.Example/FeatureMatching/DrawMatches.cs Assets/Emgu.CV/Demo/
-
-cp ../opencv/data/haarcascades/haarcascade_frontalface_alt2.xml Assets/Emgu.CV/Demo/Resources/
-
-cp unityStoreIcons/EmguCVLogo_128x128.png Assets/Emgu.CV/Demo/EmguCVLogo.png
+pushd %~p0
+call wsl dos2unix copy_demo_assets
+call wsl ./copy_demo_assets
+popd
