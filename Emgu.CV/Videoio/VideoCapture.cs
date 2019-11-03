1fec34ab8b611ff2abf4e2147f3f11bd849c2890
Victor Irzak <victor.irzak@zomp.com>
2019-11-03 13:54:47 -0500

    WIP

--- Emgu.CV/Videoio/VideoCapture.cs
+++ Emgu.CV/Videoio/VideoCapture.cs
@@ -2,7 +2,7 @@
 //  Copyright (C) 2004-2019 by EMGU Corporation. All rights reserved.       
 //----------------------------------------------------------------------------
 
-#if !(__ANDROID__ || __UNIFIED__ || NETFX_CORE || UNITY_WSA || NETSTANDARD1_4 || UNITY_ANDROID || UNITY_IOS || UNITY_EDITOR || UNITY_STANDALONE)
+#if !(__ANDROID__ || __UNIFIED__ || NETFX_CORE || UNITY_WSA || NETSTANDARD1_4 || NETCOREAPP || UNITY_ANDROID || UNITY_IOS || UNITY_EDITOR || UNITY_STANDALONE)
 #define WITH_SERVICE_MODEL
 #endif
 
