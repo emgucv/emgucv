80e8a4a7f162606f96b2afbdcef9c7fcaa7323f9
Canming Huang <support@emgu.com>
2019-11-22 15:24:25 -0500

    Build VTK out of source folder.

--- platforms/ubuntu/16.04/apt_install_dependency.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-sudo apt-get install build-essential monodevelop mono-mcs libtiff5-dev libgeotiff-dev libgtk-3-dev libgstreamer1.0-dev libavcodec-dev libswscale-dev libavformat-dev libopenexr-dev libjasper-dev libdc1394-22-dev libv4l-dev libeigen3-dev libtbb-dev libtesseract-dev cmake-curses-gui ocl-icd-dev freeglut3-dev 
