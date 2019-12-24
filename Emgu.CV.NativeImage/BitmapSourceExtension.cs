54b8b022675833cd0046d879843b18a6f8ecbe59
Canming Huang <support@emgu.com>
2019-12-24 11:22:43 -0500

    Fixed typo.

--- Emgu.CV.NativeImage/BitmapSourceExtension.cs
+++ Emgu.CV.NativeImage/BitmapSourceExtension.cs
@@ -19,7 +19,7 @@ namespace Emgu.CV
         /// <summary>
         /// Delete a GDI object
         /// </summary>
-        /// <param name="o">The poniter to the GDI object to be deleted</param>
+        /// <param name="o">The pointer to the GDI object to be deleted</param>
         /// <returns></returns>
         [DllImport("gdi32")]
         private static extern int DeleteObject(IntPtr o);
