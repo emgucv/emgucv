05695ac8f9c9bc876c201d6e7bfa28e45e23e310
Canming Huang <support@emgu.com>
2020-01-07 09:56:32 -0500

    Move the Viz3dPage to main app.

--- Emgu.CV.Example/XamarinForms/Core/App.cs
+++ Emgu.CV.Example/XamarinForms/Core/App.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 
 using Xamarin.Forms;
+using Emgu.CV.Structure;
 
 namespace Emgu.CV.XamarinForms
 {
@@ -52,13 +53,39 @@ namespace Emgu.CV.XamarinForms
             };
 
 #if !(NETFX_CORE || __ANDROID__ || __IOS__ || __MACOS__)
-            //Button viz3dButton = new Button();
-            //viz3dButton.Text = "Viz3D";
-            //buttonList.Add(viz3dButton);
-            //viz3dButton.Clicked += (sender, args) =>
-            //{
-            //    MainPage.Navigation.PushAsync(new Viz3dPage());
-            //};
+            Button viz3dButton = new Button();
+            viz3dButton.Text = "Viz3D";
+            buttonList.Add(viz3dButton);
+            
+            viz3dButton.Clicked += (sender, args) =>
+            {
+                using (Viz3d viz = new Viz3d("show_simple_widgets"))
+                {
+                    viz.SetBackgroundMeshLab();
+                    using (WCoordinateSystem coor = new WCoordinateSystem())
+                    {
+                        viz.ShowWidget("coor", coor);
+                        using (WCube cube = new WCube(
+                            new MCvPoint3D64f(-.5, -.5, -.5),
+                            new MCvPoint3D64f(.5, .5, .5),
+                            true,
+                            new MCvScalar(255, 255, 255)))
+                        {
+                            viz.ShowWidget("cube", cube);
+                            using (WCube cube0 = new WCube(
+                                new MCvPoint3D64f(-1, -1, -1),
+                                new MCvPoint3D64f(-.5, -.5, -.5),
+                                false,
+                                new MCvScalar(123, 45, 200)))
+                            {
+                                viz.ShowWidget("cub0", cube0);
+                                viz.Spin();
+                            }
+                        }
+                    }
+                }
+            };
+            
 #endif
 
             StackLayout buttonsLayout = new StackLayout
