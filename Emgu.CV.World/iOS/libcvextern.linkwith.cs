ef8e8231b711afa3679d414d506c6f8ccb28937b
Canming Huang <support@emgu.com>
2020-02-11 14:03:51 -0500

    Remove LinkTarget.ArmV7

--- Emgu.CV.World/iOS/libcvextern.linkwith.cs
+++ Emgu.CV.World/iOS/libcvextern.linkwith.cs
@@ -9,7 +9,7 @@ using ObjCRuntime;
 
 [assembly: LinkWith (
    "libcvextern.a", 
-   LinkTarget.ArmV7s | LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.Arm64 | LinkTarget.Simulator64, 
+   LinkTarget.ArmV7s | LinkTarget.Simulator | LinkTarget.Arm64 | LinkTarget.Simulator64, 
    ForceLoad = true, 
    Frameworks="Foundation Accelerate CoreFoundation CoreGraphics AssetsLibrary AVFoundation CoreImage CoreMedia CoreVideo QuartzCore ImageIO UIKit CoreText", 
    LinkerFlags = "-stdlib=libc++ -ObjC -lc++", 
