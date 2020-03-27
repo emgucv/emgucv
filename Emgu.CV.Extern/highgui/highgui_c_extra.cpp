71a6d6134b785259cc4e14debc5351ee05f731a8
Canming Huang <support@emgu.com>
2020-03-27 14:05:28 -0400

    Make it possible to build cvextern without the highgui module.

--- Emgu.CV.Extern/highgui/highgui_c_extra.cpp
+++ Emgu.CV.Extern/highgui/highgui_c_extra.cpp
@@ -8,44 +8,80 @@
 
 void cveImshow(cv::String* winname, cv::_InputArray* mat)
 {
+#if HAVE_OPENCV_HIGHGUI
 	cv::imshow(*winname, *mat);
+#else
+	throw_no_highgui();
+#endif
 }
 void cveNamedWindow(cv::String* winname, int flags)
 {
+#if HAVE_OPENCV_HIGHGUI
 	cv::namedWindow(*winname, flags);
+#else
+	throw_no_highgui();
+#endif
 }
 void cveSetWindowProperty(cv::String* winname, int propId, double propValue)
 {
+#if HAVE_OPENCV_HIGHGUI
 	cv::setWindowProperty(*winname, propId, propValue);
+#else
+	throw_no_highgui();
+#endif
 }
 void cveSetWindowTitle(cv::String* winname, cv::String* title)
 {
+#if HAVE_OPENCV_HIGHGUI
 	cv::setWindowTitle(*winname, *title);
+#else
+	throw_no_highgui();
+#endif
 }
 double cveGetWindowProperty(cv::String* winname, int propId)
 {
+#if HAVE_OPENCV_HIGHGUI
 	return cv::getWindowProperty(*winname, propId);
+#else
+	throw_no_highgui();
+#endif
 }
 void cveDestroyWindow(cv::String* winname)
 {
+#if HAVE_OPENCV_HIGHGUI
 	cv::destroyWindow(*winname);
+#else
+	throw_no_highgui();
+#endif
 }
 void cveDestroyAllWindows()
 {
+#if HAVE_OPENCV_HIGHGUI
 	cv::destroyAllWindows();
+#else
+	throw_no_highgui();
+#endif
 }
 int cveWaitKey(int delay)
 {
+#if HAVE_OPENCV_HIGHGUI
 	return cv::waitKey(delay);
+#else
+	throw_no_highgui();
+#endif
 }
 
 void cveSelectROI(cv::String* windowName, cv::_InputArray* img, bool showCrosshair, bool fromCenter, CvRect* roi)
 {
+#if HAVE_OPENCV_HIGHGUI
 	cv::Rect r = cv::selectROI(*windowName, *img, showCrosshair, fromCenter);
 	roi->x = r.x;
 	roi->y = r.y;
 	roi->width = r.width;
 	roi->height = r.height;
+#else
+	throw_no_highgui();
+#endif
 }
 
 void cveSelectROIs(
@@ -55,5 +91,9 @@ void cveSelectROIs(
 	bool showCrosshair,
 	bool fromCenter)
 {
+#if HAVE_OPENCV_HIGHGUI
 	cv::selectROIs(*windowName, *img, *boundingBoxs, showCrosshair, fromCenter);
+#else
+	throw_no_highgui();
+#endif
 }
--- Emgu.CV.Extern/highgui/highgui_c_extra.h
+++ Emgu.CV.Extern/highgui/highgui_c_extra.h
@@ -7,9 +7,16 @@
 #pragma once
 #ifndef EMGU_HIGHGUI_C_H
 #define EMGU_HIGHGUI_C_H
+#include "opencv2/core/cvdef.h"
 
+#ifdef HAVE_OPENCV_HIGHGUI
 #include "opencv2/highgui/highgui_c.h"
 #include "opencv2/highgui/highgui.hpp"
+#else
+#include "opencv2/core/core_c.h"
+static inline CV_NORETURN void throw_no_highgui() { CV_Error(cv::Error::StsBadFunc, "The library is compiled without highgui support"); }
+#endif
+
 
 CVAPI(void) cveImshow(cv::String* winname, cv::_InputArray* mat);
 CVAPI(void) cveNamedWindow(cv::String* winname, int flags);
