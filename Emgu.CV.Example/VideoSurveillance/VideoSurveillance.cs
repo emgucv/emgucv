d69d181d69302acba50a351804161f652df96b4f
Canming Huang <support@emgu.com>
2020-03-27 18:10:47 -0400

    Updated VideoSurveillance demo to use Tracking module instead of CvBlob.

--- Emgu.CV.Example/VideoSurveillance/VideoSurveillance.cs
+++ Emgu.CV.Example/VideoSurveillance/VideoSurveillance.cs
@@ -7,79 +7,155 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.ServiceModel.Configuration;
 using System.Text;
 using System.Windows.Forms;
 
 using Emgu.CV;
-using Emgu.CV.Cvb;
 using Emgu.CV.UI;
 using Emgu.CV.CvEnum;
+using Emgu.CV.Features2D;
 using Emgu.CV.Structure;
+using Emgu.CV.Tracking;
+using Emgu.CV.Util;
 
 namespace VideoSurveillance
 {
-   public partial class VideoSurveillance : Form
-   {
-      
-      private static VideoCapture _cameraCapture;
-      
-      private static IBackgroundSubtractor _fgDetector;
-      private static Emgu.CV.Cvb.CvBlobDetector _blobDetector;
-      private static Emgu.CV.Cvb.CvTracks _tracker;
+    public class TrackedObject : TrackerKCF
+    {
+        public TrackedObject(int id) :
+            base()
+        {
+            Id = id;
+        }
 
-      public VideoSurveillance()
-      {
-         InitializeComponent();
-         Run();
-      }
+        public int Id { get; set; }
+    }
 
-      void Run()
-      {
-         try
-         {
-            _cameraCapture = new VideoCapture();
-         }
-         catch (Exception e)
-         {
-            MessageBox.Show(e.Message);
-            return;
-         }
+    public partial class VideoSurveillance : Form
+    {
+        private int _idCounter = 0;
 
-         _fgDetector = new BackgroundSubtractorMOG2();
-         _blobDetector = new CvBlobDetector();
-         _tracker = new CvTracks();
+        List<TrackedObject> _trackers = new List<TrackedObject>();
 
-         Application.Idle += ProcessFrame;
-      }
+        private static VideoCapture _cameraCapture;
 
-      void ProcessFrame(object sender, EventArgs e)
-      {
-         Mat frame = _cameraCapture.QueryFrame();
-         Mat smoothedFrame = new Mat();
-         CvInvoke.GaussianBlur(frame, smoothedFrame, new Size(3, 3), 1); //filter out noises
-         //frame._SmoothGaussian(3); 
+        private static IBackgroundSubtractor _fgDetector;
+        private static SimpleBlobDetector _blobDetector;
 
-         #region use the BG/FG detector to find the forground mask
-         Mat forgroundMask = new Mat();
-         _fgDetector.Apply(smoothedFrame, forgroundMask);
-         #endregion
+        public VideoSurveillance()
+        {
+            InitializeComponent();
+            Run();
+        }
 
-         CvBlobs blobs = new CvBlobs();
-         _blobDetector.Detect(forgroundMask.ToImage<Gray, byte>(), blobs);
-         blobs.FilterByArea(100, int.MaxValue);
+        void Run()
+        {
+            try
+            {
+                _cameraCapture = new VideoCapture();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return;
+            }
 
-         float scale = (frame.Width + frame.Width)/2.0f;
-         _tracker.Update(blobs, 0.01 * scale, 5, 5);
-        
-         foreach (var pair in _tracker)
-         {
-            CvTrack b = pair.Value;
-            CvInvoke.Rectangle(frame, b.BoundingBox, new MCvScalar(255.0, 255.0, 255.0), 2);
-            CvInvoke.PutText(frame,  b.Id.ToString(), new Point((int)Math.Round(b.Centroid.X), (int)Math.Round(b.Centroid.Y)), FontFace.HersheyPlain, 1.0, new MCvScalar(255.0, 255.0, 255.0));
-         }
+            _fgDetector = new BackgroundSubtractorMOG2();
+            _blobDetector = new SimpleBlobDetector();
+            
 
-         imageBox1.Image = frame;
-         imageBox2.Image = forgroundMask;
-      }
-   }
+            Application.Idle += ProcessFrame;
+        }
+
+        void ProcessFrame(object sender, EventArgs e)
+        {
+            Mat frame = _cameraCapture.QueryFrame();
+            Mat smoothedFrame = new Mat();
+            CvInvoke.GaussianBlur(frame, smoothedFrame, new Size(3, 3), 1); //filter out noises
+            
+            #region use the BG/FG detector to find the forground mask
+            Mat forgroundMask = new Mat();
+            _fgDetector.Apply(smoothedFrame, forgroundMask);
+            #endregion
+
+            List<TrackedObject> toBeRemovedList = new List<TrackedObject>();
+            List<Rectangle> boundingBoxesTracked = new List<Rectangle>();
+            List<int> idsTracked = new List<int>();
+
+            foreach (TrackedObject t in _trackers)
+            {
+                Rectangle boundingBox;
+                bool success = t.Update(frame, out boundingBox);
+                if (success)
+                {
+                    boundingBoxesTracked.Add(boundingBox);
+                    idsTracked.Add(t.Id);
+                    
+                }
+                else 
+                    toBeRemovedList.Add(t);
+            }
+
+            foreach (TrackedObject t in toBeRemovedList)
+            {
+                _trackers.Remove(t);
+                t.Dispose();
+            }
+
+            List<Rectangle> newObjects = new List<Rectangle>();
+            int minAreaThreshold = 100;
+            using (Mat canny = new Mat())
+            using (VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint())
+            {
+                CvInvoke.Canny(forgroundMask, canny, 180, 100);
+                CvInvoke.FindContours(canny, contours, null, RetrType.External, ChainApproxMethod.ChainApproxSimple);
+                int count = contours.Size;
+                for (int i = 0; i < count; i++)
+                {
+                    using (VectorOfPoint contour = contours[i])
+                    using (VectorOfPoint approxContour = new VectorOfPoint())
+                    {
+                        CvInvoke.ApproxPolyDP(contour, approxContour, CvInvoke.ArcLength(contour, true) * 0.05, true);
+                        if (CvInvoke.ContourArea(approxContour, false) > minAreaThreshold) //only consider contours with area greater than 250
+                        {
+                            Rectangle r = CvInvoke.BoundingRectangle(approxContour);
+                            bool overlapped = false;
+                            foreach (var trackedObj in boundingBoxesTracked)
+                            {
+                                if (r.IntersectsWith(trackedObj))
+                                {
+                                    overlapped = true;
+                                    break;
+                                }
+                            }
+                            if (!overlapped)
+                                newObjects.Add(r);
+                        }
+                    }
+                }
+            }
+            
+            foreach (Rectangle r in newObjects)
+            {
+                TrackedObject t = new TrackedObject(_idCounter);
+                boundingBoxesTracked.Add(r);
+                idsTracked.Add(_idCounter);
+                t.Init(frame, r);
+                _trackers.Add(t);
+                _idCounter++;
+            }
+
+            for (int i = 0; i < boundingBoxesTracked.Count; i++)
+            {
+                Rectangle boundingBox = boundingBoxesTracked[i];
+                CvInvoke.Rectangle(frame, boundingBox, new MCvScalar(255.0, 255.0, 255.0), 2);
+                CvInvoke.PutText(frame, idsTracked[i].ToString(), new Point(boundingBox.X, boundingBox.Y), FontFace.HersheyPlain,
+                    1.0, new MCvScalar(255.0, 255.0, 255.0));
+            }
+
+            imageBox1.Image = frame;
+            imageBox2.Image = forgroundMask;
+        }
+    }
 }
