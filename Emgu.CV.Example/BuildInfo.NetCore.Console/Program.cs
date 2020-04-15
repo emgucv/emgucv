42f737c859594fbfef4a42bc5d46bd4133437097
Canming Huang <support@emgu.com>
2020-04-15 17:54:56 -0400

    Added BuildInfo.NetCore.Console demo project.

---
+++ Emgu.CV.Example/BuildInfo.NetCore.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿//----------------------------------------------------------------------------
+//  Copyright (C) 2004-2020 by EMGU Corporation. All rights reserved.       
+//----------------------------------------------------------------------------
+
+
+using System;
+using Emgu.CV;
+
+
+namespace BuildInfo.NetCore.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(CvInvoke.BuildInformation);
+        }
+    }
+}
