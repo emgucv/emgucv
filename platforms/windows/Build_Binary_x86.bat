43c34f59ac4e6b105297a44d0647adbf151eb647
Canming Huang <support@emgu.com>
2020-04-15 11:02:37 -0400

    Automatically detect CUDA_HOST_COMPILER folder (instead of hardcoding the path).

--- platforms/windows/Build_Binary_x86.bat
+++ platforms/windows/Build_Binary_x86.bat
@@ -238,8 +238,12 @@ IF %DEVENV%==%VS2010% SET CUDA_HOST_COMPILER=%VS100COMNTOOLS%..\..\VC\bin\cl.exe
 IF %DEVENV%==%VS2012% SET CUDA_HOST_COMPILER=%VS110COMNTOOLS%..\..\VC\bin\cl.exe
 IF %DEVENV%==%VS2013% SET CUDA_HOST_COMPILER=%VS120COMNTOOLS%..\..\VC\bin\cl.exe
 IF %DEVENV%==%VS2015% SET CUDA_HOST_COMPILER=%VS140COMNTOOLS%..\..\VC\bin\cl.exe
-IF %DEVENV%==%VS2017% SET CUDA_HOST_COMPILER=%VS2017_DIR%\VC\Tools\MSVC\14.16.27023\bin\Hostx64\x64\cl.exe
-IF %DEVENV%==%VS2019% SET CUDA_HOST_COMPILER=%VS2019_DIR%\VC\Tools\MSVC\14.25.28610\bin\Hostx64\x64\cl.exe
+
+for /d %%i in ( "%VS2017_DIR%\VC\Tools\MSVC\*" ) do SET VS2017_CUDA_HOST_COMPILER=%%i\bin\Hostx64\x64\cl.exe
+IF %DEVENV%==%VS2017% SET CUDA_HOST_COMPILER=%VS2017_CUDA_HOST_COMPILER%
+
+for /d %%i in ( "%VS2019_DIR%\VC\Tools\MSVC\*" ) do SET VS2019_CUDA_HOST_COMPILER=%%i\bin\Hostx64\x64\cl.exe
+IF %DEVENV%==%VS2019% SET CUDA_HOST_COMPILER=%VS2019_CUDA_HOST_COMPILER%
 
 REM Find cuda. Use latest Cuda release for 64 bit and Cuda 6.5 for 32bit
 REM We cannot use latest Cuda release for 32 bit because the 32bit version of npp has been depreciated from Cuda 7
@@ -442,8 +446,8 @@ SET CMAKE_CONF_FLAGS=%CMAKE_CONF_FLAGS% ^
 
 :BUILD
 IF NOT "%7%"=="build" GOTO END
-
-call %DEVENV% %BUILD_TYPE% emgucv.sln %BUILD_PROJECT% 
+%CMAKE% --build . --config Release
+REM call %DEVENV% %BUILD_TYPE% emgucv.sln %BUILD_PROJECT% 
 
 IF "%5%"=="htmldoc" ^
 call %DEVENV% %BUILD_TYPE% emgucv.sln /project Emgu.CV.Document 
