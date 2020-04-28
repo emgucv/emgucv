996a44950d6de046433c1f613ef60663370222dd
Canming Huang <support@emgu.com>
2020-04-28 11:54:29 -0400

    Enable using camera on Android for the PedestrianDetection demo.

--- Emgu.CV.Example/XamarinForms/Core/FindPedestrian.cs
+++ Emgu.CV.Example/XamarinForms/Core/FindPedestrian.cs
@@ -14,60 +14,57 @@ using Emgu.CV.Cuda;
 
 namespace PedestrianDetection
 {
-   public static class FindPedestrian
-   {
-      /// <summary>
-      /// Find the pedestrian in the image
-      /// </summary>
-      /// <param name="image">The image</param>
-      /// <returns>The region where pedestrians are detected</returns>
-      public static Rectangle[] Find(IInputArray image)
-      {
-         //Stopwatch watch;
-         Rectangle[] regions;
+    public static class FindPedestrian
+    {
+        /// <summary>
+        /// Find the pedestrian in the image
+        /// </summary>
+        /// <param name="image">The image</param>
+        /// <returns>The region where pedestrians are detected</returns>
+        public static Rectangle[] Find(IInputArray image, HOGDescriptor hog, CudaHOG hogCuda = null)
+        {
+            //Stopwatch watch;
+            Rectangle[] regions;
 
-         using (InputArray iaImage = image.GetInputArray())
-         {
-
-            //if the input array is a GpuMat
-            //check if there is a compatible Cuda device to run pedestrian detection
-            if (iaImage.Kind == InputArray.Type.CudaGpuMat)
+            using (InputArray iaImage = image.GetInputArray())
             {
-               //this is the Cuda version
-               using (CudaHOG des = new CudaHOG(new Size(64, 128), new Size(16, 16), new Size(8, 8), new Size(8, 8)))
-               {
-                  des.SetSVMDetector(des.GetDefaultPeopleDetector());
+                //if the input array is a GpuMat
+                //check if there is a compatible Cuda device to run pedestrian detection
+                if (iaImage.Kind == InputArray.Type.CudaGpuMat && hogCuda != null)
+                {
+                    //this is the Cuda version
 
-                  //watch = Stopwatch.StartNew();
-                  using (GpuMat cudaBgra = new GpuMat())
-                  using (VectorOfRect vr = new VectorOfRect())
-                  {
-                     CudaInvoke.CvtColor(image, cudaBgra, ColorConversion.Bgr2Bgra);
-                     des.DetectMultiScale(cudaBgra, vr);
-                     regions = vr.ToArray();
-                  }
-               }
+                    
+
+                    //watch = Stopwatch.StartNew();
+                    using (GpuMat cudaBgra = new GpuMat())
+                    using (VectorOfRect vr = new VectorOfRect())
+                    {
+                        CudaInvoke.CvtColor(image, cudaBgra, ColorConversion.Bgr2Bgra);
+                        hogCuda.DetectMultiScale(cudaBgra, vr);
+                        regions = vr.ToArray();
+                    }
+
+                }
+                else
+                {
+                    //this is the CPU/OpenCL version
+
+                    
+                    //watch = Stopwatch.StartNew();
+
+                    MCvObjectDetection[] results = hog.DetectMultiScale(image);
+                    regions = new Rectangle[results.Length];
+                    for (int i = 0; i < results.Length; i++)
+                        regions[i] = results[i].Rect;
+                    //watch.Stop();
+
+                }
+
+                //processingTime = watch.ElapsedMilliseconds;
+
+                return regions;
             }
-            else
-            {
-               //this is the CPU/OpenCL version
-               using (HOGDescriptor des = new HOGDescriptor())
-               {
-                  des.SetSVMDetector(HOGDescriptor.GetDefaultPeopleDetector());
-                  //watch = Stopwatch.StartNew();
-
-                  MCvObjectDetection[] results = des.DetectMultiScale(image);
-                  regions = new Rectangle[results.Length];
-                  for (int i = 0; i < results.Length; i++)
-                     regions[i] = results[i].Rect;
-                  //watch.Stop();
-               }
-            }
-
-            //processingTime = watch.ElapsedMilliseconds;
-
-            return regions;
-         }
-      }
-   }
+        }
+    }
 }
--- Emgu.CV.Example/XamarinForms/Core/PedestrianDetectionPage.cs
+++ Emgu.CV.Example/XamarinForms/Core/PedestrianDetectionPage.cs
@@ -14,20 +14,51 @@ using Emgu.CV;
 using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
 using Emgu.Util;
-
+using Emgu.CV.Cuda;
 using PedestrianDetection;
 
 namespace Emgu.CV.XamarinForms
 {
-    public class PedestrianDetectionPage : ButtonTextImagePage
+    public class PedestrianDetectionPage
+#if __ANDROID__
+        : AndroidCameraPage
+#else
+        : ButtonTextImagePage
+#endif
     {
+        private VideoCapture _capture = null;
+        private Mat _mat = null;
+        private String _defaultButtonText = "Perform Pedestrian Detection";
+        private CudaHOG _hogCuda = null;
+        private HOGDescriptor _hog;
+
+#if __ANDROID__
+        private String _StopCameraButtonText = "Stop Camera";
+        private bool _isBusy = false;
+#endif
         public PedestrianDetectionPage()
            : base()
         {
+
+#if __ANDROID__
+            HasCameraOption = true;
+#endif
             var button = this.GetButton();
-            button.Text = "Perform Pedestrian Detection";
+            button.Text = _defaultButtonText;
 
             button.Clicked += OnButtonClicked;
+
+            _hog = new HOGDescriptor();
+            _hog.SetSVMDetector(HOGDescriptor.GetDefaultPeopleDetector());
+            if (CudaInvoke.HasCuda)
+            {
+                _hogCuda = new CudaHOG(
+                    new Size(64, 128),
+                    new Size(16, 16),
+                    new Size(8, 8),
+                    new Size(8, 8));
+                _hogCuda.SetSVMDetector(_hogCuda.GetDefaultPeopleDetector());
+            }
         }
 
 
@@ -40,11 +71,11 @@ namespace Emgu.CV.XamarinForms
                 using (Mat bgr = new Mat())
                 {
                     CvInvoke.CvtColor(m, bgr, ColorConversion.Bgra2Bgr);
-                    pedestrians = FindPedestrian.Find(bgr);
+                    pedestrians = FindPedestrian.Find(bgr, _hog, _hogCuda);
                 }
             }
             else
-                pedestrians = FindPedestrian.Find(m);
+                pedestrians = FindPedestrian.Find(m, _hog, _hogCuda);
 
             foreach (Rectangle rect in pedestrians)
             {
@@ -55,20 +86,75 @@ namespace Emgu.CV.XamarinForms
 
         private async void OnButtonClicked(object sender, EventArgs e)
         {
-            Mat[] images = await LoadImages(new String[] { "pedestrian.png" });
-            if (images == null || images[0] == null)
+#if __ANDROID__
+            var button = GetButton();
+            if (button.Text.Equals(_StopCameraButtonText))
+            {
+                StopCapture();
+                button.Text = _defaultButtonText;
+                //AndroidImageView.Visibility = ViewStates.Invisible;
                 return;
-            SetMessage("please wait...");
-            SetImage(null);
+            }
+#endif
+            Mat[] images = await LoadImages(new String[] { "pedestrian.png" });
+            if (images == null)
+            {
+                //User cancel
+                return;
+            } else if (images.Length == 0)
+            {
+#if __ANDROID__
+                button.Text = _StopCameraButtonText;
+                StartCapture(async delegate (Object sender, Mat m)
+                {
+                    //Skip the frame if busy, 
+                    //Otherwise too many frames arriving and will eventually saturated the memory.
+                    if (!_isBusy)
+                    {
+                        _isBusy = true;
+                        try
+                        {
+                            Stopwatch watch = Stopwatch.StartNew();
+                            await Task.Run(() => { ProcessImage(m); });
+                            watch.Stop();
+                            SetImage(m);
+                            SetMessage(String.Format("Detected in {0} milliseconds.", watch.ElapsedMilliseconds));
+                        }
+                        finally
+                        {
+                            _isBusy = false;
+                        }
+                    }
+                });
+#else
+                    //Handle video
+                    if (_capture == null)
+                    {
+                        _capture = new VideoCapture();
+                        _capture.ImageGrabbed += _capture_ImageGrabbed;
+                    }
+                    _capture.Start();
+#endif
+            }
+            else if (images[0] == null)
+            {
+                return; //user cancel
+            } else
+            {
+                SetMessage("please wait...");
+                SetImage(null);
 
-            Stopwatch watch = Stopwatch.StartNew();
-            await Task.Run(() => ProcessImage(images[0]));
-            watch.Stop();
+                Stopwatch watch = Stopwatch.StartNew();
+                await Task.Run(() => ProcessImage(images[0]));
+                watch.Stop();
 
-            String computeDevice = CvInvoke.UseOpenCL ? "OpenCL: " + Ocl.Device.Default.Name : "CPU";
-            SetMessage(String.Format("Detection completed with {1} in {0} milliseconds.", computeDevice, watch.ElapsedMilliseconds));
-            SetImage(images[0]);
+                String computeDevice = CvInvoke.UseOpenCL ? "OpenCL: " + Ocl.Device.Default.Name : "CPU";
+                SetMessage(String.Format("Detection completed with {1} in {0} milliseconds.", computeDevice, watch.ElapsedMilliseconds));
+                SetImage(images[0]);
+            }
         }
+
+        
     }
 }
 
