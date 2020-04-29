c6ef737e1b787e9b6f3e7d902754de1bc08f802e
Canming Huang <support@emgu.com>
2020-04-29 15:34:15 -0400

    Updated assembly info.

--- Emgu.CV.Platform/iOS/ApiDefinition.cs
+++ Emgu.CV.Platform/iOS/ApiDefinition.cs
@@ -5,7 +5,7 @@ using ObjCRuntime;
 using Foundation;
 //using UIKit;
 
-namespace Emgu.CV.World
+namespace Emgu.CV
 {
    // The first step to creating a binding is to add your native library ("libNativeLibrary.a")
    // to the project by right-clicking (or Control-clicking) the folder containing this source
--- Emgu.CV.Platform/iOS/Properties/AssemblyInfo.cs
+++ Emgu.CV.Platform/iOS/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Emgu.CV.World")]
+[assembly: AssemblyTitle("Emgu.CV.Platform.IOS")]
 [assembly: AssemblyDescription("")]
 
 
