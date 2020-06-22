6bcd335c6e1790c34e0bfa2f911c88d5cf540f31
Canming Huang <support@emgu.com>
2020-06-22 14:48:44 -0700

    Added build script for Ubuntu 20.04

---
+++ platforms/ubuntu/20.04/apt_install_dependency.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo apt-get install build-essential libtiff5-dev libgeotiff-dev libgtk-3-dev libgstreamer1.0-dev libavcodec-dev libswscale-dev libavformat-dev libopenexr-dev libdc1394-22-dev libv4l-dev libeigen3-dev libtbb-dev cmake-curses-gui ocl-icd-dev freeglut3-dev 
