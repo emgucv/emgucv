793c441150d5c7672ca910ad34988a98c1382b7a
Canming Huang <support@emgu.com>
2020-09-16 18:36:53 -0400

    Added build-unity-doc script for linux.

--- Emgu.CV.Unity/build-unity-doc.bat
+++ Emgu.CV.Unity/build-unity-doc.bat
@@ -1,10 +1,4 @@
-REM go to the folder of the current script
 pushd %~p0
-cp NamespaceDoc.cs Assets/Emgu.CV/Assets/Scripts
-wsl doxygen Emgu.CV.Doxyfile
-cd latex
-wsl make
-cd ..
-cp latex/refman.pdf ../Emgu.CV.Unity/Assets/Emgu.CV/Documentation/Emgu.CV.Documentation.pdf
-rm Assets/Emgu.CV/Assets/Scripts/NamespaceDoc.cs
+call wsl dos2unix build-unity-doc
+call wsl ./build-unity-doc
 popd
