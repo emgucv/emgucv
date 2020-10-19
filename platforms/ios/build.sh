79373785dddd205eb2bea723ea35e2bd40a7ed95
Canming Huang <support@emgu.com>
2020-10-19 17:34:16 -0400

    Added option to build ios binary without the contrib module.

--- platforms/ios/build.sh
+++ platforms/ios/build.sh
@@ -15,22 +15,28 @@ cd ../..
 
 mkdir -p libs/iOS
 
+if [[ "$2" == "core" ]]; then
+    CV_CONTRIB_OPTION=core
+else
+    CV_CONTRIB_OPTION=full
+fi
+
 if [ \( "$1" != "simulator" \) -a \( "$1" != "simulator_x86_64" \) ]; then    
     mkdir -p platforms/ios/armv7s     
     cd platforms/ios/armv7s
-    ../configure-device_xcode.sh -DIOS_ARCH="armv7s" $*
+    ../configure-device_xcode.sh $CV_CONTRIB_OPTION -DIOS_ARCH="armv7s" ${@:3}
     ./xcodebuild_wrapper BITCODE_GENERATION_MODE=bitcode -parallelizeTargets -jobs 8 -configuration Release -target ALL_BUILD clean build
     cd ../../..
     
     mkdir -p platforms/ios/armv7
     cd platforms/ios/armv7
-    ../configure-device_xcode.sh -DIOS_ARCH="armv7" $*
+    ../configure-device_xcode.sh $CV_CONTRIB_OPTION -DIOS_ARCH="armv7" ${@:3}
     ./xcodebuild_wrapper BITCODE_GENERATION_MODE=bitcode -parallelizeTargets -jobs 8 -configuration Release -target ALL_BUILD clean build
     cd ../../..
 
     mkdir -p platforms/ios/arm64
     cd platforms/ios/arm64
-    ../configure-device_xcode.sh -DIOS_ARCH="arm64" $*
+    ../configure-device_xcode.sh $CV_CONTRIB_OPTION -DIOS_ARCH="arm64" ${@:3}
     ./xcodebuild_wrapper BITCODE_GENERATION_MODE=bitcode -parallelizeTargets -jobs 8 -configuration Release -target ALL_BUILD clean build
     cd ../../..
 fi
@@ -38,24 +44,17 @@ fi
 if [ "$1" != "simulator_x86_64" ]; then
     mkdir -p platforms/ios/i386
     cd platforms/ios/i386
-    if [ "$1" = "simulator" ]; then    
-  #skip the first parameter
-    ../configure-simulator_xcode.sh -DIOS_ARCH="i386" -DBUILD_IPP_IW:BOOL=FALSE -DWITH_IPP:BOOL=FALSE ${@:2}
-    else
-    ../configure-simulator_xcode.sh -DIOS_ARCH="i386" -DBUILD_IPP_IW:BOOL=FALSE -DWITH_IPP:BOOL=FALSE $*
-    fi
+    #skip the first two parameter
+    ../configure-simulator_xcode.sh $CV_CONTRIB_OPTION -DIOS_ARCH="i386" -DBUILD_IPP_IW:BOOL=FALSE -DWITH_IPP:BOOL=FALSE ${@:3}
     ./xcodebuild_wrapper -parallelizeTargets -jobs 8 -configuration Release -target ALL_BUILD clean build
     cd ../../..
 fi
 
 mkdir -p platforms/ios/x86_64
 cd platforms/ios/x86_64
-if [ \( "$1" = "simulator" \) -o \( "$1" = "simulator_x86_64" \) ]; then
-  #skip the first parameter    
-  ../configure-simulator_xcode.sh -DIOS_ARCH="x86_64" -DBUILD_IPP_IW:BOOL=FALSE -DWITH_IPP:BOOL=FALSE ${@:2}
-else
-  ../configure-simulator_xcode.sh -DIOS_ARCH="x86_64" -DBUILD_IPP_IW:BOOL=FALSE -DWITH_IPP:BOOL=FALSE $*
-fi
+#skip the first two parameter    
+../configure-simulator_xcode.sh $CV_CONTRIB_OPTION -DIOS_ARCH="x86_64" -DBUILD_IPP_IW:BOOL=FALSE -DWITH_IPP:BOOL=FALSE ${@:3}
+
 ./xcodebuild_wrapper WARNING_CFLAGS=-Wno-implicit-function-declaration -parallelizeTargets -jobs 8 -configuration Release -target ALL_BUILD clean build
 
 cd ../../..
--- platforms/ios/configure-simulator_xcode.sh
+++ platforms/ios/configure-simulator_xcode.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 set -e
-cmake -GXcode -DHB_BUILD_TESTS:BOOL=FALSE -DCMAKE_TOOLCHAIN_FILE=`dirname $0`/cmake/Toolchains/Toolchain-iPhoneSimulator_Xcode.cmake -DCMAKE_INSTALL_PREFIX=../OpenCV_iPhoneSimulator -DOPENCV_EXTRA_MODULES_PATH=`dirname $0`/../../opencv_contrib/modules -DBUILD_opencv_hdf:BOOL=FALSE -DBUILD_SHARED_LIBS:BOOL=FALSE -DBUILD_PERF_TESTS:BOOL=FALSE -DBUILD_TESTS:BOOL=FALSE -DBUILD_opencv_apps:BOOL=FALSE -DBUILD_opencv_java_bindings_generator:BOOL=FALSE -DBUILD_opencv_python_bindings_generator:BOOL=FALSE -DOPENCV_SKIP_VISIBILITY_HIDDEN:BOOL=TRUE -DIPHONEOS_DEPLOYMENT_TARGET:STRING=8.0 -DHB_HAVE_FREETYPE:BOOL=TRUE $* `dirname $0`/../.. 
+
+if [ "$1" == "core" ]; then
+    CV_CONTRIB_OPTION=
+else
+    CV_CONTRIB_OPTION=-DOPENCV_EXTRA_MODULES_PATH=`dirname $0`/../../opencv_contrib/modulde
+fi
+
+cmake -GXcode -DHB_BUILD_TESTS:BOOL=FALSE -DCMAKE_TOOLCHAIN_FILE=`dirname $0`/cmake/Toolchains/Toolchain-iPhoneSimulator_Xcode.cmake -DCMAKE_INSTALL_PREFIX=../OpenCV_iPhoneSimulator $CV_CONTRIB_OPTION -DBUILD_opencv_hdf:BOOL=FALSE -DBUILD_SHARED_LIBS:BOOL=FALSE -DBUILD_PERF_TESTS:BOOL=FALSE -DBUILD_TESTS:BOOL=FALSE -DBUILD_opencv_apps:BOOL=FALSE -DBUILD_opencv_java_bindings_generator:BOOL=FALSE -DBUILD_opencv_python_bindings_generator:BOOL=FALSE -DOPENCV_SKIP_VISIBILITY_HIDDEN:BOOL=TRUE -DIPHONEOS_DEPLOYMENT_TARGET:STRING=8.0 -DHB_HAVE_FREETYPE:BOOL=TRUE ${@:2} `dirname $0`/../.. 
