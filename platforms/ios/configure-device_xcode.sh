65d6ae069fd763db9b65296b653a401a86de1813
Canming Huang <support@emgu.com>
2020-10-21 10:09:03 -0400

    Fixed the path to the contrib modules for ios build.

--- platforms/ios/configure-device_xcode.sh
+++ platforms/ios/configure-device_xcode.sh
@@ -4,7 +4,7 @@ set -e
 if [ "$1" == "core" ]; then
     CV_CONTRIB_OPTION=
 else
-    CV_CONTRIB_OPTION=-DOPENCV_EXTRA_MODULES_PATH=`dirname $0`/../../opencv_contrib/modulde
+    CV_CONTRIB_OPTION=-DOPENCV_EXTRA_MODULES_PATH=`dirname $0`/../../opencv_contrib/modules
 fi
 
 cmake -GXcode -DHB_BUILD_TESTS:BOOL=FALSE -DCMAKE_TOOLCHAIN_FILE=`dirname $0`/cmake/Toolchains/Toolchain-iPhoneOS_Xcode.cmake -DCMAKE_INSTALL_PREFIX=../OpenCV_iPhoneOS $CV_CONTRIB_OPTION -DBUILD_opencv_hdf:BOOL=FALSE -DBUILD_SHARED_LIBS:BOOL=FALSE -DBUILD_PERF_TESTS:BOOL=FALSE -DBUILD_TESTS:BOOL=FALSE -DBUILD_opencv_apps:BOOL=FALSE -DBUILD_opencv_java_bindings_generator:BOOL=FALSE -DBUILD_opencv_python_bindings_generator:BOOL=FALSE -DCMAKE_C_FLAGS=-fembed-bitcode -DCMAKE_CXX_FLAGS=-fembed-bitcode -DOPENCV_SKIP_VISIBILITY_HIDDEN:BOOL=TRUE -DIPHONEOS_DEPLOYMENT_TARGET:STRING=8.0 -DHB_HAVE_FREETYPE:BOOL=TRUE ${@:2} `dirname $0`/../.. 
