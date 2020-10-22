21b6217b81d149373e897c90dbe1dbed64db105d
Canming Huang <support@emgu.com>
2020-10-22 16:33:31 -0400

    Documentation improvements.

--- Emgu.CV.Contrib/Text/TextDetectorCNN.cs
+++ Emgu.CV.Contrib/Text/TextDetectorCNN.cs
@@ -3,22 +3,26 @@
 //----------------------------------------------------------------------------
 
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Text;
 using System.Runtime.InteropServices;
 using Emgu.CV.Structure;
-using Emgu.CV.Text;
 using Emgu.CV.Util;
 using System.Diagnostics;
 using System.Drawing;
-using Emgu.CV.XFeatures2D;
 
 namespace Emgu.CV.Text
 {
+    /// <summary>
+    /// This class provides the functionality of text bounding box detection. This class find bounding boxes of text words given an input image.
+    /// </summary>
     public class TextDetectorCNN : SharedPtrObject
     {
 
+        /// <summary>
+        /// Creates an instance of the TextDetectorCNN class using the provided parameters.
+        /// </summary>
+        /// <param name="modelArchFilename">The relative or absolute path to the prototxt file describing the classifiers architecture.</param>
+        /// <param name="modelWeightsFilename">The relative or absolute path to the file containing the pretrained weights of the model in caffe-binary form.</param>
+        /// <param name="detectionSizes">A list of sizes for multi-scale detection. The values[(300,300),(700,500),(700,300),(700,700),(1600,1600)] are recommended to achieve the best quality.</param>
         public TextDetectorCNN(String modelArchFilename, String modelWeightsFilename, Size[] detectionSizes = null)
         {
             using (CvString csModelArchFilename = new CvString(modelArchFilename))
@@ -54,6 +58,11 @@ namespace Emgu.CV.Text
             public float Confident;
         }
 
+        /// <summary>
+        /// Find bounding boxes of text words given an input image.
+        /// </summary>
+        /// <param name="inputImage">An image expected to be a CV_U8C3 of any size</param>
+        /// <returns>The text regions found.</returns>
         public TextRegion[] Detect(IInputArray inputImage)
         {
             using (InputArray iaImage = inputImage.GetInputArray())
@@ -75,6 +84,9 @@ namespace Emgu.CV.Text
             }
         }
 
+        /// <summary>
+        /// Release all the unmanaged memory associate with this TextDetector
+        /// </summary>
         protected override void DisposeObject()
         {
             if (_sharedPtr != IntPtr.Zero)
