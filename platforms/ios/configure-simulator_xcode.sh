bf5ad681462b6f8aab6a88d690b55c664b1298ab
Canming Huang <support@emgu.com>
2020-10-25 19:38:05 -0400

    For ios, if building without contrib, do not compile freetype

--- platforms/ios/configure-simulator_xcode.sh
+++ platforms/ios/configure-simulator_xcode.sh
@@ -2,14 +2,13 @@
 set -e
 
 if [ "$1" == "core" ]; then
-    CV_CONTRIB_OPTION=
+    CV_CONTRIB_OPTION=-DEMGU_CV_WITH_FREETYPE:BOOL=FALSE
 else
-    CV_CONTRIB_OPTION=-DOPENCV_EXTRA_MODULES_PATH=`dirname $0`/../../opencv_contrib/modules
+    CV_CONTRIB_OPTION=-DOPENCV_EXTRA_MODULES_PATH=`dirname $0`/../../opencv_contrib/modules -DHB_HAVE_FREETYPE:BOOL=TRUE -DHB_BUILD_TESTS:BOOL=FALSE 
 fi
 
 cmake \
 -GXcode \
--DHB_BUILD_TESTS:BOOL=FALSE \
 -DCMAKE_TOOLCHAIN_FILE=`dirname $0`/cmake/Toolchains/Toolchain-iPhoneSimulator_Xcode.cmake \
 -DCMAKE_INSTALL_PREFIX=../OpenCV_iPhoneSimulator \
 $CV_CONTRIB_OPTION \
@@ -23,5 +22,4 @@ $CV_CONTRIB_OPTION \
 -DCMAKE_CXX_FLAGS_RELEASE:STRING=-g0 \
 -DCMAKE_C_FLAGS_RELEASE:STRING=-g0 \
 -DIPHONEOS_DEPLOYMENT_TARGET:STRING=8.0 \
--DHB_HAVE_FREETYPE:BOOL=TRUE \
 ${@:2} `dirname $0`/../.. 
