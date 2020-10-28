6def084d8006f81b806a10f8e06382bc883369d7
Canming Huang <support@emgu.com>
2020-10-28 16:12:51 -0400

    added option to build only core components

--- platforms/ubuntu/20.04/cmake_configure.sh
+++ platforms/ubuntu/20.04/cmake_configure.sh
@@ -1,16 +1,34 @@
-#!/bin/sh
+#!/bin/bash
 
 cd "$(dirname "$0")"
 
+BUILD_TYPE=full
+if [[ $# -gt 0 ]]; then
+    if [ "$1" == "core" ]; then
+	BUILD_TYPE=core
+    fi
+fi
+
 cd ../../..
 
-cd vtk
-mkdir -p build
-cd build
-CFLAGS=-fPIC CXXFLAGS=-fPIC cmake -DBUILD_TESTING:BOOL=FALSE -DBUILD_SHARED_LIBS:BOOL=FALSE -DCMAKE_BUILD_TYPE:STRING="Release" ..
-make
+if [ "$BUILD_TYPE" == "core" ]; then
+    echo "Performing a core build"
+    TESSERACT_OPTION=-DEMGU_CV_WITH_TESSERACT:BOOL=FALSE
+    VTK_OPTION=
+    CONTRIB_OPTION=
+else
+    echo "Performing a full build"
+    TESSERACT_OPTION=-DEMGU_CV_WITH_TESSERACT:BOOL=TRUE
+    cd vtk
+    mkdir -p build
+    cd build
+    CFLAGS=-fPIC CXXFLAGS=-fPIC cmake -DBUILD_TESTING:BOOL=FALSE -DBUILD_SHARED_LIBS:BOOL=FALSE -DCMAKE_BUILD_TYPE:STRING="Release" ..
+    make
+    VTK_OPTION=-DVTK_DIR:String="$PWD"
+    cd ../..
+    CONTRIB_OPTION=-DOPENCV_EXTRA_MODULES_PATH=../../../../opencv_contrib/modules 
+fi
 
-cd ../..
 cd eigen
 mkdir -p build
 cd build
@@ -20,6 +38,28 @@ cd ../../platforms/ubuntu/20.04
 
 mkdir -p build
 cd build
-CFLAGS=-fPIC CXXFLAGS=-fPIC cmake -DBUILD_TESTS:BOOL=FALSE -DBUILD_PERF_TESTS:BOOL=FALSE -DBUILD_opencv_apps:BOOL=FALSE -DBUILD_DOCS:BOOL=FALSE -DWITH_TBB:BOOL=TRUE -DWITH_CUDA:BOOL=FALSE -DWITH_IPP:BOOL=FALSE -DWITH_EIGEN:BOOL=FALSE -DOPENCV_EXTRA_MODULES_PATH=../../../../opencv_contrib/modules -DBUILD_opencv_ts:BOOL=FALSE -DBUILD_opencv_java:BOOL=FALSE -DBUILD_opencv_python2:BOOL=FALSE -DBUILD_opencv_python3:BOOL=FALSE -DBUILD_SHARED_LIBS:BOOL=FALSE -DVTK_DIR:String="$PWD/../../../../vtk/build" -DWITH_EIGEN:BOOL=TRUE -DEigen3_DIR:String="$PWD/../../../../eigen/build" -DCMAKE_BUILD_TYPE:String="Release" -DCMAKE_CXX_STANDARD:String="11" $PWD/../../../..
+CFLAGS=-fPIC CXXFLAGS=-fPIC cmake \
+      $TESSERACT_OPTION \
+      -DBUILD_TESTS:BOOL=FALSE \
+      -DBUILD_PERF_TESTS:BOOL=FALSE \
+      -DBUILD_opencv_apps:BOOL=FALSE \
+      -DBUILD_DOCS:BOOL=FALSE \
+      $CONTRIB_OPTION \
+      -DBUILD_opencv_ts:BOOL=FALSE \
+      -DBUILD_opencv_java:BOOL=FALSE \
+      -DBUILD_opencv_python2:BOOL=FALSE \
+      -DBUILD_opencv_python3:BOOL=FALSE \
+      -DBUILD_SHARED_LIBS:BOOL=FALSE \
+      $VTK_OPTION \
+      -DWITH_EIGEN:BOOL=TRUE \
+      -DEigen3_DIR:String="$PWD/../../../../eigen/build" \
+      -DCMAKE_BUILD_TYPE:String="Release" \
+      -DCMAKE_CXX_STANDARD:String="11" \
+      $PWD/../../../..
+
+#      -DWITH_TBB:BOOL=TRUE \
+#      -DWITH_CUDA:BOOL=FALSE \
+    
 C_INCLUDE_PATH=$PWD/../../../../eigen/ CPLUS_INCLUDE_PATH=$PWD/../../../../eigen/ make
+
 cd ..
