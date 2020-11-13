ba24d03d08fd29faf8da1c1fa16279f4ef3c0c49
Canming Huang <support@emgu.com>
2020-11-13 14:06:11 -0500

    Updated docker build script.

--- platforms/windows/docker/Docker_Build_Binary_x86-64_cuda_dldt_doc.bat
+++ platforms/windows/docker/Docker_Build_Binary_x86-64_cuda_dldt_doc.bat
@@ -1,3 +1,4 @@
 REM go to the folder of the current script
 pushd %~p0
 call Docker_Build_binary_x86.bat 64 gpu inf no-openni doc package build
+popd
--- platforms/windows/docker/Docker_Build_Helper.bat
+++ platforms/windows/docker/Docker_Build_Helper.bat
@@ -4,3 +4,4 @@ cd c:\TEMP\platforms\windows
 call Build_Binary_x86.bat %*
 IF NOT EXIST c:\src\libs mkdir c:\src\libs
 xcopy c:\TEMP\libs c:\src\libs /s /e /h
+xcopy c:\TEMP\build_* c:\src /s /e /h
