50c20565d4798042664512339c04445dc0db9bbe
Canming Huang <support@emgu.com>
2020-11-19 10:20:00 -0500

    Documentation update for HDF module.

--- Emgu.CV.Contrib/Hdf/HDF5.cs
+++ Emgu.CV.Contrib/Hdf/HDF5.cs
@@ -11,24 +11,37 @@ using Emgu.CV.Text;
 using Emgu.CV.Util;
 using Emgu.Util;
 using System.Diagnostics;
+using Emgu.CV.CvEnum;
 
 namespace Emgu.CV.Hdf
 {
-
+    /// <summary>
+    /// Hierarchical Data Format version 5 interface.
+    /// </summary>
     public partial class HDF5 : SharedPtrObject
     {
-
+        /// <summary>
+        /// Open or create hdf5 file.
+        /// </summary>
+        /// <param name="fileName">Specify the HDF5 filename.</param>
         public HDF5(String fileName)
         {
             using (CvString csFileName = new CvString(fileName))
                 _ptr = HdfInvoke.cveHDF5Create(csFileName, ref _sharedPtr);
         }
 
+        /// <summary>
+        /// Close and release hdf5 object.
+        /// </summary>
         public void Close()
         {
             HdfInvoke.cveHDF5Close(_ptr);
         }
 
+        /// <summary>
+        /// Create a hdf5 group with default properties. The group is closed automatically after creation.
+        /// </summary>
+        /// <param name="grLabel">Specify the hdf5 group label.</param>
         public void GrCreate(String grLabel)
         {
             using (CvString csGrLabel = new CvString(grLabel))
@@ -37,6 +50,11 @@ namespace Emgu.CV.Hdf
             }
         }
 
+        /// <summary>
+        /// Check if label exists or not.
+        /// </summary>
+        /// <param name="label">Specify the hdf5 dataset label.</param>
+        /// <returns>Returns true if dataset exists, and false otherwise.</returns>
         public bool HlExist(String label)
         {
             using (CvString csLabel = new CvString(label))
@@ -45,121 +63,204 @@ namespace Emgu.CV.Hdf
             }
         }
 
-        public void DsRead(IOutputArray array, String dslabel)
+        /// <summary>
+        /// Read specific dataset from hdf5 file into Mat object.
+        /// </summary>
+        /// <param name="array">Mat container where data reads will be returned.</param>
+        /// <param name="dsLabel">Specify the source hdf5 dataset label.</param>
+        public void DsRead(IOutputArray array, String dsLabel)
         {
-            using (CvString csDsLabel = new CvString(dslabel))
+            using (CvString csDsLabel = new CvString(dsLabel))
             using (OutputArray oaArray = array.GetOutputArray())
             {
                 HdfInvoke.cveHDF5DsRead(_ptr, oaArray, csDsLabel);
             }
         }
 
-        public void DsWrite(IInputArray array, String dslabel)
+        /// <summary>
+        /// Write or overwrite a Mat object into specified dataset of hdf5 file.
+        /// </summary>
+        /// <param name="array">Specify Mat data array to be written.</param>
+        /// <param name="dsLabel">Specify the target hdf5 dataset label.</param>
+        public void DsWrite(IInputArray array, String dsLabel)
         {
-            using (CvString csDsLabel = new CvString(dslabel))
+            using (CvString csDsLabel = new CvString(dsLabel))
             using (InputArray iaArray = array.GetInputArray())
             {
                 HdfInvoke.cveHDF5DsWrite(_ptr, iaArray, csDsLabel);
             }
         }
 
-        public void cveHDF5DsCreate(int rows, int cols, int type, String dslabel, int compresslevel = -1, VectorOfInt dimsChunks = null)
+        /// <summary>
+        /// Create and allocate storage for two dimensional single or multi channel dataset.
+        /// </summary>
+        /// <param name="rows">Declare amount of rows</param>
+        /// <param name="cols">Declare amount of columns</param>
+        /// <param name="depthType">The pixel depth type</param>
+        /// <param name="channels">The number of channels</param>
+        /// <param name="dsLabel">Specify the hdf5 dataset label. Existing dataset label will cause an error.</param>
+        /// <param name="compressLevel">Specify the compression level 0-9 to be used, -1 is the default value and means no compression. The value 0 also means no compression. A value 9 indicating the best compression ration. Note that a higher compression level indicates a higher computational cost. It relies on GNU gzip for compression.</param>
+        /// <param name="dimsChunks">Each array member specifies the chunking size to be used for block I/O, by default null means none at all.</param>
+        public void DsCreate(int rows, int cols, DepthType depthType, int channels, String dsLabel, int compressLevel = -1, VectorOfInt dimsChunks = null)
         {
-            using (CvString csDsLabel = new CvString(dslabel))
+            using (CvString csDsLabel = new CvString(dsLabel))
             {
-                HdfInvoke.cveHDF5DsCreate(_ptr, rows, cols, type, csDsLabel, compresslevel, dimsChunks);
+                HdfInvoke.cveHDF5DsCreate(_ptr, rows, cols, CvInvoke.MakeType(depthType, channels), csDsLabel, compressLevel, dimsChunks);
             }
         }
 
-        public bool AtExists(String atlabel)
+        /// <summary>
+        /// Check whether a given attribute exits or not in the root group.
+        /// </summary>
+        /// <param name="atLabel">The attribute name to be checked.</param>
+        /// <returns>True if the attribute exists, False otherwise.</returns>
+        public bool AtExists(String atLabel)
         {
-            using (CvString csAtLabel = new CvString(atlabel))
+            using (CvString csAtLabel = new CvString(atLabel))
                 return HdfInvoke.cveHDF5AtExists(_ptr, csAtLabel);
         }
 
-        public void AtDelete(String atlabel)
+        /// <summary>
+        /// Delete an attribute from the root group.
+        /// </summary>
+        /// <param name="atLabel">The attribute to be deleted.</param>
+        public void AtDelete(String atLabel)
         {
-            using (CvString csAtLabel = new CvString(atlabel))
+            using (CvString csAtLabel = new CvString(atLabel))
                 HdfInvoke.cveHDF5AtDelete(_ptr, csAtLabel);
         }
 
-        public int AtReadInt(String atlabel)
+        /// <summary>
+        /// Read an attribute from the root group.
+        /// </summary>
+        /// <param name="atLabel">Attribute name</param>
+        /// <returns>The int value</returns>
+        public int AtReadInt(String atLabel)
         {
             int v = 0;
-            using (CvString csAtLabel = new CvString(atlabel))
+            using (CvString csAtLabel = new CvString(atLabel))
                 HdfInvoke.cveHDF5AtReadInt(_ptr, ref v, csAtLabel);
             return v;
         }
 
-        public void AtWrite(int value, String atlabel)
+        /// <summary>
+        /// Write an attribute inside the root group.
+        /// </summary>
+        /// <param name="value">Attribute value.</param>
+        /// <param name="atLabel">Attribute name.</param>
+        public void AtWrite(int value, String atLabel)
         {
-            using (CvString csAtLabel = new CvString(atlabel))
+            using (CvString csAtLabel = new CvString(atLabel))
                 HdfInvoke.cveHDF5AtWriteInt(_ptr, value, csAtLabel);
         }
 
-        public double AtReadDouble(String atlabel)
+        /// <summary>
+        /// Read an attribute from the root group.
+        /// </summary>
+        /// <param name="atLabel">Attribute name</param>
+        /// <returns>The double value</returns>
+        public double AtReadDouble(String atLabel)
         {
             double v = 0;
-            using (CvString csAtLabel = new CvString(atlabel))
+            using (CvString csAtLabel = new CvString(atLabel))
                 HdfInvoke.cveHDF5AtReadDouble(_ptr, ref v, csAtLabel);
             return v;
         }
 
-        public void AtWrite(double value, String atlabel)
+        /// <summary>
+        /// Write an attribute inside the root group.
+        /// </summary>
+        /// <param name="value">Attribute value.</param>
+        /// <param name="atLabel">Attribute name.</param>
+        public void AtWrite(double value, String atLabel)
         {
-            using (CvString csAtLabel = new CvString(atlabel))
+            using (CvString csAtLabel = new CvString(atLabel))
                 HdfInvoke.cveHDF5AtWriteDouble(_ptr, value, csAtLabel);
         }
 
-        public String AtReadString(String atlabel)
+        /// <summary>
+        /// Read an attribute from the root group.
+        /// </summary>
+        /// <param name="atLabel">Attribute name</param>
+        /// <returns>The String value</returns>
+        public String AtReadString(String atLabel)
         {
             using (CvString cvOut = new CvString())
-            using (CvString csAtLabel = new CvString(atlabel))
+            using (CvString csAtLabel = new CvString(atLabel))
             {
                 HdfInvoke.cveHDF5AtReadString(_ptr, cvOut, csAtLabel);
                 return cvOut.ToString();
             }
         }
 
-        public void AtWrite(String value, String atlabel)
+        /// <summary>
+        /// Write an attribute inside the root group.
+        /// </summary>
+        /// <param name="value">Attribute value.</param>
+        /// <param name="atLabel">Attribute name.</param>
+        public void AtWrite(String value, String atLabel)
         {
             using (CvString csValue = new CvString(value))
-            using (CvString csAtLabel = new CvString(atlabel))
+            using (CvString csAtLabel = new CvString(atLabel))
                 HdfInvoke.cveHDF5AtWriteString(_ptr, csValue, csAtLabel);
         }
 
 
-        public void AtReadArray(IOutputArray array, String atlabel)
+        /// <summary>
+        /// Read an attribute from the root group.
+        /// </summary>
+        /// <param name="atLabel">Attribute name</param>
+        /// <param name="array">The output value</param>
+        public void AtReadArray(IOutputArray array, String atLabel)
         {
             using (OutputArray oaArray = array.GetOutputArray())
-            using (CvString csAtLabel = new CvString(atlabel))
+            using (CvString csAtLabel = new CvString(atLabel))
                 HdfInvoke.cveHDF5AtReadArray(_ptr, oaArray, csAtLabel);
         }
 
-        public void AtWrite(IInputArray value, String atlabel)
+        /// <summary>
+        /// Write an attribute inside the root group.
+        /// </summary>
+        /// <param name="value">Attribute value.</param>
+        /// <param name="atLabel">Attribute name.</param>
+        public void AtWrite(IInputArray value, String atLabel)
         {
             using (InputArray iaValue = value.GetInputArray())
-            using (CvString csAtLabel = new CvString(atlabel))
+            using (CvString csAtLabel = new CvString(atLabel))
                 HdfInvoke.cveHDF5AtWriteArray(_ptr, iaValue, csAtLabel);
         }
 
+        /// <summary>
+        /// Read specific keypoint dataset from hdf5 file into VectorOfKeyPoint object.
+        /// </summary>
+        /// <param name="keypoints">Container where data reads will be returned.</param>
+        /// <param name="kpLabel">Specify the source hdf5 dataset label.</param>
+        /// <param name="offset">Specify the offset location over dataset from where read starts.</param>
+        /// <param name="counts">Specify the amount of keypoints from dataset to read.</param>
         public void KpRead(
             VectorOfKeyPoint keypoints, 
-            String kplabel,
+            String kpLabel,
             int offset = -1,
             int counts = -1)
         {
-            using (CvString csKpLabel = new CvString(kplabel))
+            using (CvString csKpLabel = new CvString(kpLabel))
                 HdfInvoke.cveHDF5KpRead(_ptr, keypoints, csKpLabel, offset, counts);
         }
 
+        /// <summary>
+        /// Write or overwrite list of KeyPoint into specified dataset of hdf5 file.
+        /// </summary>
+        /// <param name="keypoints">Specify keypoints data list to be written.</param>
+        /// <param name="atLabel">Specify the target hdf5 dataset label.</param>
+        /// <param name="offset">Specify the offset location on dataset from where keypoints will be (over)written into dataset.</param>
+        /// <param name="counts">Specify the amount of keypoints that will be written into dataset.</param>
         public void KpWrite(
             VectorOfKeyPoint keypoints, 
-            String atlabel,
+            String atLabel,
             int offset = -1,
             int counts = -1)
         {
-            using (CvString csAtLabel = new CvString(atlabel))
+            using (CvString csAtLabel = new CvString(atLabel))
                 HdfInvoke.cveHDF5KpWrite(_ptr, keypoints, csAtLabel, offset, counts);
         }
 
