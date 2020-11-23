95c2a36211d8463d4d0b6f3c15185935b0548125
Canming Huang <support@emgu.com>
2020-11-23 11:01:29 -0500

    Remove x86 simulator, added arm64 simulator for iOS

--- platforms/ios/build.sh
+++ platforms/ios/build.sh
@@ -44,16 +44,16 @@ if [ \( "$1" != "simulator" \) -a \( "$1" != "simulator_x86_64" \) ]; then
 fi
 
 if [ "$1" != "simulator_x86_64" ]; then
-    mkdir -p platforms/ios/i386
-    cd platforms/ios/i386
+    mkdir -p platforms/ios/arm64_simulator
+    cd platforms/ios/arm64_simulator
     #skip the first two parameter
-    ../configure_xcode.sh $CV_CONTRIB_OPTION simulator -DIOS_ARCH="i386" -DBUILD_IPP_IW:BOOL=FALSE -DWITH_IPP:BOOL=FALSE ${@:3}
+    ../configure_xcode.sh $CV_CONTRIB_OPTION simulator -DIOS_ARCH="arm64" -DBUILD_IPP_IW:BOOL=FALSE -DWITH_IPP:BOOL=FALSE ${@:3}
     ./xcodebuild_wrapper -parallelizeTargets -jobs ${JOB_COUNT} -configuration Release -target ALL_BUILD clean build
     cd ../../..
 fi
 
-mkdir -p platforms/ios/x86_64
-cd platforms/ios/x86_64
+mkdir -p platforms/ios/x86_64_simulator
+cd platforms/ios/x86_64_simulator
 #skip the first two parameter    
 ../configure_xcode.sh $CV_CONTRIB_OPTION simulator -DIOS_ARCH="x86_64" -DBUILD_IPP_IW:BOOL=FALSE -DWITH_IPP:BOOL=FALSE ${@:3}
 
@@ -64,7 +64,7 @@ cd ../../..
 cd Emgu.CV.Platform/iOS
 #compile Emgu.CV.World.iOS.dll
 msbuild /p:Configuration=Release
-cd ../../platforms/ios/x86_64
+cd ../../platforms/ios/x86_64_simulator
 #build the package this time
 ./xcodebuild_wrapper WARNING_CFLAGS=-Wno-implicit-function-declaration -parallelizeTargets -jobs ${JOB_COUNT} -configuration Release -target package build
 
