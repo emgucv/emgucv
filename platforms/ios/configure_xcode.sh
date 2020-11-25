6276adb3f8c8bd69587534ccbeb96d2a158623bb
Canming Huang <support@emgu.com>
2020-11-25 17:50:37 -0500

    Build freetype module for iOS

--- platforms/ios/configure_xcode.sh
+++ platforms/ios/configure_xcode.sh
@@ -7,7 +7,7 @@ CURRENT_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && 
 if [ "$1" == "core" ]; then
     CV_CONTRIB_OPTION=( -DEMGU_CV_WITH_FREETYPE:BOOL=FALSE -DEMGU_CV_WITH_TESSERACT:BOOL=FALSE )
 else
-    CV_CONTRIB_OPTION=( -DOPENCV_EXTRA_MODULES_PATH=$CURRENT_SCRIPT_DIR/../../opencv_contrib/modules -DEMGU_CV_WITH_FREETYPE:BOOL=FALSE -DEMGU_CV_WITH_TESSERACT:BOOL=TRUE )
+    CV_CONTRIB_OPTION=( -DOPENCV_EXTRA_MODULES_PATH=$CURRENT_SCRIPT_DIR/../../opencv_contrib/modules -DEMGU_CV_WITH_FREETYPE:BOOL=TRUE -DEMGU_CV_WITH_TESSERACT:BOOL=TRUE )
 fi
 
 #-DHB_HAVE_FREETYPE:BOOL=TRUE -DHB_BUILD_TESTS:BOOL=FALSE
@@ -39,6 +39,42 @@ cmake \
     ..
 cmake --build . --config Release --target install
 popd
+
+pushd $CURRENT_SCRIPT_DIR/../../freetype2
+mkdir -p build_$3
+cd build_$3
+cmake \
+    -GXcode \
+    ${CV_TOOLCHAIN_OPTION[@]} \
+    ${INSTALL_PREFIX_OPTION[@]} \
+    -DIOS_ARCH="$3"\
+    -DCMAKE_FIND_ROOT_PATH:STRING="$INSTALL_FOLDER" \
+    -DIPHONEOS_DEPLOYMENT_TARGET:STRING=9.0 \
+    -DCMAKE_DISABLE_FIND_PACKAGE_ZLIB:BOOL=TRUE \
+    -DCMAKE_DISABLE_FIND_PACKAGE_BZip2:BOOL=TRUE \
+    -DCMAKE_DISABLE_FIND_PACKAGE_PNG:BOOL=TRUE \
+    -DCMAKE_DISABLE_FIND_PACKAGE_HarfBuzz:BOOL=TRUE \
+    ${@:4} \
+    ..
+cmake --build . --config Release --target install
+popd
+
+pushd $CURRENT_SCRIPT_DIR/../../harfbuzz
+mkdir -p build_$3
+cd build_$3
+cmake \
+    -GXcode \
+    ${CV_TOOLCHAIN_OPTION[@]} \
+    ${INSTALL_PREFIX_OPTION[@]} \
+    -DIOS_ARCH="$3"\
+    -DHB_HAVE_FREETYPE:BOOL=TRUE \
+    -DCMAKE_FIND_ROOT_PATH:STRING="$INSTALL_FOLDER" \
+    -DIPHONEOS_DEPLOYMENT_TARGET:STRING=9.0 \
+    ${@:4} \
+    ..
+cmake --build . --config Release --target install
+popd
+
 
 cmake \
 -GXcode \
