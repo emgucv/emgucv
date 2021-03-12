13dc6bbe1231276c7d984c86de234f84a50342f8
Canming Huang <support@emgu.com>
2021-03-12 09:53:32 -0500

    Added libgeotiff dependency for ubuntu 20.04. see issue #447

--- platforms/ubuntu/20.04/apt_install_dependency.sh
+++ platforms/ubuntu/20.04/apt_install_dependency.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo apt-get install build-essential libgtk-3-dev libgstreamer1.0-dev libavcodec-dev libswscale-dev libavformat-dev libdc1394-22-dev libv4l-dev cmake-curses-gui ocl-icd-dev freeglut3-dev 
+sudo apt-get install build-essential libgtk-3-dev libgstreamer1.0-dev libavcodec-dev libswscale-dev libavformat-dev libdc1394-22-dev libv4l-dev cmake-curses-gui ocl-icd-dev freeglut3-dev libgeotiff-dev
