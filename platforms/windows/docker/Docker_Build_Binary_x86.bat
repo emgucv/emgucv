d8922146826e8dcc786fb2b12be60860ec6a93cd
Canming Huang <support@emgu.com>
2021-05-05 09:31:39 -0400

    Use a tag to build emgu cv with docker.

--- platforms/windows/docker/Docker_Build_Binary_x86.bat
+++ platforms/windows/docker/Docker_Build_Binary_x86.bat
@@ -3,5 +3,5 @@ pushd %~p0
 cd ..
 cd ..
 cd ..
-docker run -v %cd%:c:\src -w c:\src\platforms\windows\Docker --rm -m 3G emgu/vs2019_buildtools_cuda_openvino:latest Docker_Build_Helper.bat %*
+docker run -v %cd%:c:\src -w c:\src\platforms\windows\Docker --rm -m 3G emgu/vs2019_buildtools_cuda_openvino:openvino_2021.2.185 Docker_Build_Helper.bat %*
 popd
