b205539508bb30b17426bb6ab1c1b319602da0d1
Canming Huang <support@emgu.com>
2021-05-25 17:45:51 -0400

    Added NearestExact interpolation type.

--- Emgu.CV/PInvoke/CvEnum/Inter.cs
+++ Emgu.CV/PInvoke/CvEnum/Inter.cs
@@ -15,25 +15,36 @@ namespace Emgu.CV.CvEnum
         /// Nearest-neighbor interpolation
         /// </summary>
         Nearest = 0,
+
         /// <summary>
         /// Bilinear interpolation
         /// </summary>
         Linear = 1,
+
         /// <summary>
         /// Bicubic interpolation
         /// </summary>
         Cubic = 2,
+
         /// <summary>
         /// Resampling using pixel area relation. It is the preferred method for image decimation that gives moire-free results. In case of zooming it is similar to CV_INTER_NN method
         /// </summary>
         Area = 3,
+
         /// <summary>
         /// Lanczos interpolation over 8x8 neighborhood
         /// </summary>
         Lanczos4 = 4,
+
         /// <summary>
         /// Bit exact bilinear interpolation
         /// </summary>
         LinearExact = 5,
+
+        /// <summary>
+        /// Bit exact nearest neighbor interpolation. This will produce same results as
+        /// the nearest neighbor method in PIL, scikit-image or Matlab.
+        /// </summary>
+        NearestExact = 6,
     }
 }
