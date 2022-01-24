10fdbd44fed43ff59c19613a67f6c9bd3aa9f85f
Canming Huang <support@emgu.com>
2022-01-24 13:43:11 -0500

    Fixed the code to create PCAFlow

--- Emgu.CV.Extern/optflow/optflow_c.cpp
+++ Emgu.CV.Extern/optflow/optflow_c.cpp
@@ -45,8 +45,9 @@ cv::DenseOpticalFlow* cveOptFlowDeepFlowCreate(cv::Algorithm** algorithm, cv::Pt
 #ifdef HAVE_OPENCV_OPTFLOW
 	cv::Ptr<cv::DenseOpticalFlow> ptr = cv::optflow::createOptFlow_DeepFlow();
 	*sharedPtr = new cv::Ptr<cv::DenseOpticalFlow>(ptr);
-	*algorithm = dynamic_cast<cv::Algorithm*>(ptr.get());
-	return ptr.get();
+	cv::DenseOpticalFlow* flowPtr = (*sharedPtr)->get();
+	*algorithm = dynamic_cast<cv::Algorithm*>(flowPtr);
+	return flowPtr;
 #else
 	throw_no_optflow();
 #endif
@@ -58,8 +59,9 @@ cv::DenseOpticalFlow* cveOptFlowPCAFlowCreate(cv::Algorithm** algorithm, cv::Ptr
 #ifdef HAVE_OPENCV_OPTFLOW
 	cv::Ptr<cv::DenseOpticalFlow> ptr = cv::optflow::createOptFlow_PCAFlow();
 	*sharedPtr = new cv::Ptr<cv::DenseOpticalFlow>(ptr);
-	*algorithm = dynamic_cast<cv::Algorithm*>(ptr.get());
-	return ptr.get();
+	cv::DenseOpticalFlow* flowPtr = (*sharedPtr)->get();
+	*algorithm = dynamic_cast<cv::Algorithm*>(flowPtr);
+	return flowPtr;
 #else
 	throw_no_optflow();
 #endif
@@ -71,7 +73,7 @@ cv::optflow::DualTVL1OpticalFlow* cveDenseOpticalFlowCreateDualTVL1(cv::DenseOpt
 #ifdef HAVE_OPENCV_OPTFLOW
 	cv::Ptr<cv::optflow::DualTVL1OpticalFlow> dof = cv::optflow::createOptFlow_DualTVL1();
 	*sharedPtr = new cv::Ptr<cv::optflow::DualTVL1OpticalFlow>(dof);
-	cv::optflow::DualTVL1OpticalFlow* ptr = dof.get();
+	cv::optflow::DualTVL1OpticalFlow* ptr = (*sharedPtr)->get();
 	*denseOpticalFlow = dynamic_cast<cv::DenseOpticalFlow*>(ptr);
 	*algorithm = dynamic_cast<cv::Algorithm*>(ptr);
 	return ptr;
