5560fdc7f0d6897619a0dcd985261f8b26fb00dd
Canming Huang <support@emgu.com>
2022-03-28 19:10:24 -0400

    Added TF.Lite.Interop demo.

---
+++ Emgu.CV.Example/TF.Lite.Interop/MainForm.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace CVInterop.Lite.Net
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fileNameTextBox = new System.Windows.Forms.TextBox();
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cameraButton = new System.Windows.Forms.Button();
+            this.messageLabel = new System.Windows.Forms.Label();
+            this.imageBox = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Recognized Image File:";
+            // 
+            // fileNameTextBox
+            // 
+            this.fileNameTextBox.Location = new System.Drawing.Point(228, 13);
+            this.fileNameTextBox.Name = "fileNameTextBox";
+            this.fileNameTextBox.Size = new System.Drawing.Size(718, 31);
+            this.fileNameTextBox.TabIndex = 1;
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Location = new System.Drawing.Point(973, 13);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(81, 34);
+            this.openFileButton.TabIndex = 2;
+            this.openFileButton.Text = "...";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(212, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Recognize camera image:";
+            // 
+            // cameraButton
+            // 
+            this.cameraButton.Location = new System.Drawing.Point(243, 74);
+            this.cameraButton.Name = "cameraButton";
+            this.cameraButton.Size = new System.Drawing.Size(230, 34);
+            this.cameraButton.TabIndex = 4;
+            this.cameraButton.Text = "Start Camera";
+            this.cameraButton.UseVisualStyleBackColor = true;
+            this.cameraButton.Click += new System.EventHandler(this.cameraButton_Click);
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.AutoSize = true;
+            this.messageLabel.Location = new System.Drawing.Point(13, 136);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(123, 25);
+            this.messageLabel.TabIndex = 5;
+            this.messageLabel.Text = "messageLabel";
+            // 
+            // imageBox
+            // 
+            this.imageBox.Location = new System.Drawing.Point(13, 178);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(1084, 703);
+            this.imageBox.TabIndex = 2;
+            this.imageBox.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1109, 893);
+            this.Controls.Add(this.imageBox);
+            this.Controls.Add(this.messageLabel);
+            this.Controls.Add(this.cameraButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.openFileButton);
+            this.Controls.Add(this.fileNameTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "Emgu CV Interop";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox fileNameTextBox;
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button cameraButton;
+        private System.Windows.Forms.Label messageLabel;
+        private Emgu.CV.UI.ImageBox imageBox;
+    }
+}
+
---
+++ Emgu.CV.Example/TF.Lite.Interop/MainForm.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.TF.Lite;
+using Emgu.TF.Lite.Models;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+
+namespace CVInterop.Lite.Net
+{
+    public partial class MainForm : Form
+    {
+        private CocoSsdMobilenetV3 _mobileNet;
+        private String _startCameraText = "Start Camera";
+        private String _stopCameraText = "Stop Camera";
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            TfLiteInvoke.Init();
+            messageLabel.Text = String.Empty;
+            cameraButton.Text = _startCameraText;
+
+            _mobileNet = new CocoSsdMobilenetV3();
+            _mobileNet.OnDownloadProgressChanged += OnDownloadProgressChangedEventHandler;
+        }
+
+        public void OnDownloadProgressChangedEventHandler(object sender, System.Net.DownloadProgressChangedEventArgs e)
+        {
+            String msg = String.Format("Downloading models, please wait... {0} of {1} bytes ({2}%) downloaded.", e.BytesReceived, e.TotalBytesToReceive, e.ProgressPercentage);
+            if (InvokeRequired)
+            {
+                this.Invoke((MethodInvoker)(() =>
+                {
+                    messageLabel.Text = msg;
+                }));
+            }
+            else
+            {
+                messageLabel.Text = msg;
+            }
+        }
+
+        private bool _coldSession = true;
+
+        private Mat _renderMat;
+
+        //private Tensor _imageTensor;
+
+        public void Recognize(Mat m)
+        {
+            int[] dim = new int[] { 1, m.Height, m.Width, 3 };
+
+            Tensor inputTensor = _mobileNet.Interpreter.Inputs[0];
+
+            int height = inputTensor.Dims[1];
+            int width = inputTensor.Dims[2];
+            using (Mat inputTensorMat = new Mat(new Size(width, height), DepthType.Cv8U, 3, inputTensor.DataPointer, 3 * width))
+            {
+                CvInvoke.Resize(m, inputTensorMat, inputTensorMat.Size);
+            }
+
+            if (_coldSession)
+            {
+                //First run of the recognition graph, here we will compile the graph and initialize the session
+                //This is expected to take much longer time than consecutive runs.
+                _mobileNet.Interpreter.Invoke();
+                _coldSession = false;
+            }
+
+            //Here we are trying to time the execution of the graph after it is loaded
+            Stopwatch sw = Stopwatch.StartNew();
+            _mobileNet.Interpreter.Invoke();
+            var results = _mobileNet.GetResults(0.5f);
+            sw.Stop();
+            int goodResultCount = 0;
+            foreach (var r in results)
+            {
+
+                float x1 = r.Rectangle[0] * m.Width;
+                float y1 = r.Rectangle[1] * m.Height;
+                float x2 = r.Rectangle[2] * m.Width;
+                float y2 = r.Rectangle[3] * m.Height;
+
+                RectangleF rectf = new RectangleF(x1, y1, x2 - x1, y2 - y1);
+                Rectangle rect = Rectangle.Round(rectf);
+
+                rect.Intersect(new Rectangle(Point.Empty, m.Size)); //only keep the region that is inside the image
+                if (rect.IsEmpty)
+                    continue;
+
+                //draw the rectangle around the region
+                CvInvoke.Rectangle(m, rect, new Emgu.CV.Structure.MCvScalar(0, 0, 255), 2);
+
+                //draw the label
+                CvInvoke.PutText(m, r.Label, Point.Round(rect.Location), Emgu.CV.CvEnum.FontFace.HersheyComplex, 1.0, new Emgu.CV.Structure.MCvScalar(0, 255, 0), 1);
+
+                goodResultCount++;
+
+            }
+
+            String resStr = String.Format("{0} objects detected in {1} milliseconds.", goodResultCount, sw.ElapsedMilliseconds);
+
+            if (_renderMat == null)
+                _renderMat = new Mat();
+            m.CopyTo(_renderMat);
+            //Bitmap bmp = _renderMat.ToBitmap();
+
+            if (InvokeRequired)
+            {
+                this.Invoke((MethodInvoker)(() =>
+                {
+                    messageLabel.Text = resStr;
+                    imageBox.Image = _renderMat;
+                }));
+            }
+            else
+            {
+                messageLabel.Text = resStr;
+                imageBox.Image = _renderMat;
+            }
+        }
+
+
+        public void Recognize(String fileName)
+        {
+            fileNameTextBox.Text = fileName;
+
+            using (Mat m = CvInvoke.Imread(fileName))
+            {
+                Recognize(m);
+            }
+        }
+
+        public async Task Init()
+        {
+            //IDelegate d1 = TfLiteInvoke.DefaultGpuDelegate;
+            //IDelegate d2 = TfLiteInvoke.DefaultGpuDelegateV2;
+            //await _mobileNet.Init(null, null, "CocoSsdMobilenetV3", TfLiteInvoke.DefaultGpuDelegateV2);
+
+            await _mobileNet.Init();
+        }
+
+        private async void openFileButton_Click(object sender, EventArgs e)
+        {
+            await Init();
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.CheckFileExists = true;
+            ofd.Multiselect = false;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                String fileName = ofd.FileName;
+                Recognize(fileName);
+            }
+        }
+
+        private VideoCapture _capture = null;
+
+        private async void cameraButton_Click(object sender, EventArgs e)
+        {
+            if (cameraButton.Text.Equals(_startCameraText))
+            {
+                await Init();
+                if (_capture == null)
+                {
+                    _capture = new VideoCapture(0);
+                    if (!_capture.IsOpened)
+                    {
+                        _capture = null;
+                        MessageBox.Show("Failed to open camera");
+                        return;
+                    }
+                    _capture.ImageGrabbed += _capture_ImageGrabbed;
+                    _capture.Start();
+                }
+                cameraButton.Text = _stopCameraText;
+            }
+            else
+            {
+                if (_capture != null)
+                {
+                    _capture.Stop();
+                    _capture.Dispose();
+                }
+
+                _capture = null;
+                cameraButton.Text = _startCameraText;
+            }
+        }
+
+        private Mat _captureFrame = new Mat();
+
+        private void _capture_ImageGrabbed(object sender, EventArgs e)
+        {
+            _capture.Retrieve(_captureFrame);
+            Recognize(_captureFrame);
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (cameraButton.Text.Equals(_stopCameraText))
+            {
+                //stop the camera if we are in a recording session.
+                cameraButton_Click(this, null);
+            }
+        }
+
+    }
+}
