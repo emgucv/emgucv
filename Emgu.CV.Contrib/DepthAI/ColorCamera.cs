1ac73336fc4d57f9dcb0111d682f4ca73394f29a
Canming Huang <support@emgu.com>
2022-06-05 12:53:13 -0400

    Added missing ColorCamera class.

---
+++ Emgu.CV.Contrib/DepthAI/ColorCamera.cs
@@ -0,0 +1,88 @@
+﻿//----------------------------------------------------------------------------
+//  Copyright (C) 2004-2022 by EMGU Corporation. All rights reserved.       
+//----------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using Emgu.CV.Structure;
+using Emgu.CV.Text;
+using Emgu.CV.Util;
+using Emgu.Util;
+using System.Diagnostics;
+using System.Drawing;
+using Emgu.CV.CvEnum;
+
+namespace Emgu.CV.Dai
+{
+
+    /// <summary>
+    /// DepthAI ColorCamera
+    /// </summary>
+    public partial class ColorCamera : SharedPtrObject, INode
+    {
+        private IntPtr _nodePtr;
+
+        internal ColorCamera(IntPtr colorCameraPtr, IntPtr colorCameraSharedPtr, IntPtr nodePtr)
+        {
+            _ptr = colorCameraPtr;
+            _sharedPtr = colorCameraSharedPtr;
+            _nodePtr = nodePtr;
+        }
+
+        /// <summary>
+        /// Release all unmanaged memory associated with the ColorCamera.
+        /// </summary>
+        protected override void DisposeObject()
+        {
+            if (_sharedPtr != IntPtr.Zero)
+            {
+                DaiInvoke.daiColorCameraRelease(ref _sharedPtr);
+				_ptr = IntPtr.Zero;
+                _nodePtr = IntPtr.Zero;
+            }
+        }
+
+        public NodeOutput GetPreview()
+        {
+            return new NodeOutput(DaiInvoke.daiColorCameraGetPreview(_ptr), false);
+        }
+
+        /// <summary>
+        /// The camera image orientation
+        /// </summary>
+        public CameraImageOrientation Orientation
+        {
+            get { return DaiInvoke.daiColorCameraGetImageOrientation(_ptr); }
+            set { DaiInvoke.daiColorCameraSetImageOrientation(_ptr, value); }
+        }
+
+        public IntPtr NodePtr
+        {
+            get { return _nodePtr; }
+        }
+    }
+
+    /// <summary>
+    /// Entry points for the DepthAI module.
+    /// </summary>
+    public static partial class DaiInvoke
+    {
+        [DllImport(CvInvoke.ExternLibrary, CallingConvention = CvInvoke.CvCallingConvention)]
+        internal static extern void daiColorCameraRelease(ref IntPtr colorCameraSharedPtr);
+
+
+        [DllImport(CvInvoke.ExternLibrary, CallingConvention = CvInvoke.CvCallingConvention)]
+        internal static extern IntPtr daiColorCameraGetPreview(IntPtr colorCamera);
+
+        [DllImport(CvInvoke.ExternLibrary, CallingConvention = CvInvoke.CvCallingConvention)]
+        internal static extern CameraImageOrientation daiColorCameraGetImageOrientation(IntPtr colorCamera);
+
+        [DllImport(CvInvoke.ExternLibrary, CallingConvention = CvInvoke.CvCallingConvention)]
+        internal static extern void daiColorCameraSetImageOrientation(
+            IntPtr colorCamera,
+            CameraImageOrientation val);
+
+    }
+}
