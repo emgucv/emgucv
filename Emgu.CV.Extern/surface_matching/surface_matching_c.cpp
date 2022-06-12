28da57d01099d7029cad683d5e8688a517be2b89
Canming Huang <support@emgu.com>
2022-06-12 08:51:23 -0400

    Added function overload for RegisterModelToScene with multiple initial poses

--- Emgu.CV.Extern/surface_matching/surface_matching_c.cpp
+++ Emgu.CV.Extern/surface_matching/surface_matching_c.cpp
@@ -7,15 +7,15 @@
 #include "surface_matching_c.h"
 
 cv::ppf_match_3d::ICP* cveICPCreate(
-	int iterations, 
-	float tolerence, 
-	float rejectionScale, 
-	int numLevels, 
-	int sampleType, 
+	int iterations,
+	float tolerance,
+	float rejectionScale,
+	int numLevels,
+	int sampleType,
 	int numMaxCorr)
 {
 #ifdef HAVE_OPENCV_SURFACE_MATCHING
-	return new cv::ppf_match_3d::ICP(iterations, tolerence, rejectionScale, numLevels, sampleType, numMaxCorr);
+	return new cv::ppf_match_3d::ICP(iterations, tolerance, rejectionScale, numLevels, sampleType, numMaxCorr);
 #else
 	throw_no_surface_matching();
 #endif
@@ -36,13 +36,104 @@ int cveICPRegisterModelToScene(cv::ppf_match_3d::ICP* icp, cv::Mat* srcPC, cv::M
 #endif
 }
 
+int cveICPRegisterModelToScene2(cv::ppf_match_3d::ICP* icp, cv::Mat* srcPC, cv::Mat* dstPC, std::vector< cv::ppf_match_3d::Pose3D >* poses)
+{
+#ifdef HAVE_OPENCV_SURFACE_MATCHING
+	std::vector< cv::ppf_match_3d::Pose3DPtr > posePtrs;
+	for (int i = 0; i < poses->size(); ++i)
+	{
+		cv::ppf_match_3d::Pose3D* ptr = &(*poses)[i];
+		cv::Ptr<cv::ppf_match_3d::Pose3D> cvPtr(ptr);
+		posePtrs.push_back(cvPtr);
+	}
+	return icp->registerModelToScene(*srcPC, *dstPC, posePtrs);
+#else
+	throw_no_surface_matching();
+#endif	
+}
+
 void cveICPRelease(cv::ppf_match_3d::ICP** icp)
 {
 #ifdef HAVE_OPENCV_SURFACE_MATCHING
-	delete *icp;
+	delete* icp;
 	*icp = 0;
 #else
 	throw_no_surface_matching();
 #endif
 }
 
+cv::ppf_match_3d::Pose3D* cvePose3DCreate()
+{
+#ifdef HAVE_OPENCV_SURFACE_MATCHING
+	return new cv::ppf_match_3d::Pose3D();
+#else
+	throw_no_surface_matching();
+#endif	
+}
+void cvePose3DUpdatePose(cv::ppf_match_3d::Pose3D* pose3d, cv::Mat* pose)
+{
+#ifdef HAVE_OPENCV_SURFACE_MATCHING
+	cv::Matx44d p(*pose);
+	pose3d->updatePose(p);
+#else
+	throw_no_surface_matching();
+#endif		
+}
+void cvePose3DRelease(cv::ppf_match_3d::Pose3D** pose3d)
+{
+#ifdef HAVE_OPENCV_SURFACE_MATCHING
+	delete* pose3d;
+	*pose3d = 0;
+#else
+	throw_no_surface_matching();
+#endif
+}
+void cvePose3DGetT(cv::ppf_match_3d::Pose3D* pose3d, CvPoint3D64f* t)
+{
+#ifdef HAVE_OPENCV_SURFACE_MATCHING
+	cv::Vec3d translation = pose3d->t;
+	t->x = translation[0];
+	t->y = translation[1];
+	t->z = translation[2];
+#else
+	throw_no_surface_matching();
+#endif
+}
+void cvePose3DSetT(cv::ppf_match_3d::Pose3D* pose3d, CvPoint3D64f* t)
+{
+#ifdef HAVE_OPENCV_SURFACE_MATCHING
+	cv::Vec3d translation;
+	translation[0] = t->x;
+	translation[1] = t->y;
+	translation[2] = t->z;
+	pose3d->t = translation;
+#else
+	throw_no_surface_matching();
+#endif
+}
+
+void cvePose3DGetQ(cv::ppf_match_3d::Pose3D* pose3d, CvScalar* q)
+{
+#ifdef HAVE_OPENCV_SURFACE_MATCHING
+	cv::Vec4d quat = pose3d->q;
+	q->val[0] = quat[0];
+	q->val[1] = quat[1];
+	q->val[2] = quat[2];
+	q->val[3] = quat[3];
+#else
+	throw_no_surface_matching();
+#endif
+}
+void cvePose3DSetQ(cv::ppf_match_3d::Pose3D* pose3d, CvScalar* q)
+{
+#ifdef HAVE_OPENCV_SURFACE_MATCHING
+	cv::Vec4d quat;
+	quat[0] = q->val[0];
+	quat[1] = q->val[1];
+	quat[2] = q->val[2];
+	quat[2] = q->val[2];
+	pose3d->q = quat;
+#else
+	throw_no_surface_matching();
+#endif
+}
--- Emgu.CV.Extern/surface_matching/surface_matching_c.h
+++ Emgu.CV.Extern/surface_matching/surface_matching_c.h
@@ -17,6 +17,7 @@ static inline CV_NORETURN void throw_no_surface_matching() { CV_Error(cv::Error:
 namespace cv {
 	namespace ppf_match_3d {
 		class ICP {};
+		class Pose3D {};
 	}
 }
 
@@ -24,7 +25,7 @@ namespace cv {
 
 CVAPI(cv::ppf_match_3d::ICP*) cveICPCreate(
 	int iterations, 
-	float tolerence, 
+	float tolerance, 
 	float rejectionScale, 
 	int numLevels, 
 	int sampleType, 
@@ -32,6 +33,16 @@ CVAPI(cv::ppf_match_3d::ICP*) cveICPCreate(
 
 CVAPI(int) cveICPRegisterModelToScene(cv::ppf_match_3d::ICP* icp, cv::Mat* srcPC, cv::Mat* dstPC, double* residual, cv::Mat* pose);
 
+CVAPI(int) cveICPRegisterModelToScene2(cv::ppf_match_3d::ICP* icp, cv::Mat* srcPC, cv::Mat* dstPC, std::vector< cv::ppf_match_3d::Pose3D >* poses);
+
 CVAPI(void) cveICPRelease(cv::ppf_match_3d::ICP** icp);
 
+
+CVAPI(cv::ppf_match_3d::Pose3D*) cvePose3DCreate();
+CVAPI(void) cvePose3DUpdatePose(cv::ppf_match_3d::Pose3D* pose3d, cv::Mat* pose);
+CVAPI(void) cvePose3DRelease(cv::ppf_match_3d::Pose3D** pose3d);
+CVAPI(void) cvePose3DGetT(cv::ppf_match_3d::Pose3D* pose3d, CvPoint3D64f* t);
+CVAPI(void) cvePose3DSetT(cv::ppf_match_3d::Pose3D* pose3d, CvPoint3D64f* t);
+CVAPI(void) cvePose3DGetQ(cv::ppf_match_3d::Pose3D* pose3d, CvScalar* q);
+CVAPI(void) cvePose3DSetQ(cv::ppf_match_3d::Pose3D* pose3d, CvScalar* q);
 #endif
