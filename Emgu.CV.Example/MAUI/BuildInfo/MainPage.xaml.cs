3e283c7bf9c8d7eeaeef06f17ec7911f33042f5a
Canming Huang <support@emgu.com>
2022-09-26 13:07:29 -0400

    Added CvInvokeIOS.cs to Maui Mac project.

--- Emgu.CV.Example/MAUI/BuildInfo/MainPage.xaml.cs
+++ Emgu.CV.Example/MAUI/BuildInfo/MainPage.xaml.cs
@@ -9,6 +9,12 @@ public partial class MainPage : ContentPage
     public MainPage()
     {
         InitializeComponent();
+
+#if __IOS__
+        CvInvokeIOS.Init();
+#elif __ANDROID__
+        CvInvokeAndroid.Init();
+#endif
     }
 
     private void OnCounterClicked(object sender, EventArgs e)
