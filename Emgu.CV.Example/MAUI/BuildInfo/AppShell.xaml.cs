248d10d11ec6225199b55a5b89c47ae95ae8e72e
Canming Huang <support@emgu.com>
2022-09-26 10:10:57 -0400

    Added a simple MAUI app to display BuildInfo

---
+++ Emgu.CV.Example/MAUI/BuildInfo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BuildInfo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Emgu.CV.Example/MAUI/BuildInfo/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Emgu.CV;
+
+namespace BuildInfo;
+
+public partial class MainPage : ContentPage
+{
+
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        DetailLabel.Text = CvInvoke.BuildInformation;
+    }
+}
+
