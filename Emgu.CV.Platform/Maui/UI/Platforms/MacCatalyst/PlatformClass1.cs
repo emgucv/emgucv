2a5291228731db12dfdefd002af12af621492158
Canming Huang <support@emgu.com>
2023-02-03 10:25:39 -0500

    Added MauiInvoke class.

--- Emgu.CV.Platform/Maui/UI/Platforms/MacCatalyst/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Emgu.CV.Platform.Maui.UI
-{
-    // All the code in this file is only included on Mac Catalyst.
-    public class PlatformClass1
-    {
-    }
-}
--- Emgu.CV.Platform/Maui/UI/Platforms/iOS/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Emgu.CV.Platform.Maui.UI
-{
-    // All the code in this file is only included on iOS.
-    public class PlatformClass1
-    {
-    }
-}
