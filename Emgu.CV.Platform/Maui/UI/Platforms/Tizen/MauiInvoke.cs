8b56f79ed64d61759cbd6090f2bed5095f2fd520
Canming Huang <support@emgu.com>
2023-02-03 10:25:39 -0500

    Added MauiInvoke class.

---
+++ Emgu.CV.Platform/Maui/UI/Platforms/Tizen/MauiInvoke.cs
@@ -0,0 +1,28 @@
+﻿//----------------------------------------------------------------------------
+//  Copyright (C) 2004-2023 by EMGU Corporation. All rights reserved.       
+//----------------------------------------------------------------------------
+
+namespace Emgu.CV.Platform.Maui
+{
+    /// <summary>
+    /// PInvoke for MAUI
+    /// </summary>
+    public partial class MauiInvoke
+    {
+        private static readonly bool _libraryLoaded;
+
+        /// <summary>
+        /// Check to make sure all the unmanaged libraries are loaded
+        /// </summary>
+        /// <returns>True if library loaded</returns>
+        public static bool Init()
+        {
+            return _libraryLoaded;
+        }
+
+        static MauiInvoke()
+        {
+            _libraryLoaded = Emgu.CV.CvInvoke.Init();
+        }
+    }
+}
--- Emgu.CV.Platform/Maui/UI/Platforms/Tizen/PlatformClass1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Emgu.CV.Platform.Maui.UI
-{
-    // All the code in this file is only included on Tizen.
-    public class PlatformClass1
-    {
-    }
-}
