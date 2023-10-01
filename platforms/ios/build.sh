efd1b76ed9dd15c00921e3cc02a8e803123e76cd
Canming Huang <support@emgu.com>
2023-09-30 20:17:14 -0400

    Fix the ios build script.

--- platforms/ios/build.sh
+++ platforms/ios/build.sh
@@ -41,7 +41,8 @@ if [ \( "$1" != "simulator_arm64" \) -a \( "$1" != "simulator_x86_64" \) ]; then
     mkdir -p platforms/ios/iphoneos_arm64
     cd platforms/ios/iphoneos_arm64
     ../configure_xcode.sh $CV_CONTRIB_OPTION device arm64 ${@:3}
-    ./xcodebuild_wrapper -parallelizeTargets -jobs ${JOB_COUNT} -configuration Release -target ALL_BUILD build
+    #./xcodebuild_wrapper -parallelizeTargets -jobs ${JOB_COUNT} -configuration Release -target ALL_BUILD build
+    xcodebuild -parallelizeTargets -jobs ${JOB_COUNT} -configuration Release -target ALL_BUILD build
     cd ../../..
 fi
 
@@ -50,7 +51,8 @@ if [ "$1" = "simulator_arm64" ]; then
     cd platforms/ios/simulator_arm64
     #skip the first two parameter
     ../configure_xcode.sh $CV_CONTRIB_OPTION simulator arm64 -DBUILD_IPP_IW:BOOL=FALSE -DWITH_IPP:BOOL=FALSE ${@:3}
-    ./xcodebuild_wrapper -parallelizeTargets -jobs ${JOB_COUNT} -configuration Release -target ALL_BUILD build
+    #./xcodebuild_wrapper -parallelizeTargets -jobs ${JOB_COUNT} -configuration Release -target ALL_BUILD build
+    xcodebuild -parallelizeTargets -jobs ${JOB_COUNT} -configuration Release -target ALL_BUILD build
     cd ../../..
 fi
 
@@ -58,14 +60,15 @@ mkdir -p platforms/ios/simulator_x86_64
 cd platforms/ios/simulator_x86_64
 #skip the first two parameter    
 ../configure_xcode.sh $CV_CONTRIB_OPTION simulator x86_64 -DBUILD_IPP_IW:BOOL=FALSE -DWITH_IPP:BOOL=FALSE ${@:3}
-./xcodebuild_wrapper WARNING_CFLAGS=-Wno-implicit-function-declaration -parallelizeTargets -jobs ${JOB_COUNT} -configuration Release -target ALL_BUILD build
+#./xcodebuild_wrapper WARNING_CFLAGS=-Wno-implicit-function-declaration -parallelizeTargets -jobs ${JOB_COUNT} -configuration Release -target ALL_BUILD build
+xcodebuild WARNING_CFLAGS=-Wno-implicit-function-declaration -parallelizeTargets -jobs ${JOB_COUNT} -configuration Release -target ALL_BUILD build
 cd ../../..
 
 cd Emgu.CV.Platform/Xamarin/iOS
 msbuild /p:Configuration=Release
 cd ../../../platforms/ios/simulator_x86_64
 #build the package this time
-./xcodebuild_wrapper WARNING_CFLAGS=-Wno-implicit-function-declaration -parallelizeTargets -jobs ${JOB_COUNT} -configuration Release -target package build
-
+#./xcodebuild_wrapper WARNING_CFLAGS=-Wno-implicit-function-declaration -parallelizeTargets -jobs ${JOB_COUNT} -configuration Release -target package build
+xcodebuild WARNING_CFLAGS=-Wno-implicit-function-declaration -parallelizeTargets -jobs ${JOB_COUNT} -configuration Release -target package build
 cd ..
 
