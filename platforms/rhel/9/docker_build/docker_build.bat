6577998c71f8beeb4ed9b8b129e4e35f54e049f6
Canming Huang <support@emgu.com>
2024-03-03 13:29:20 -0500

    Added script to build emgucv with ubi9.

---
+++ platforms/rhel/9/docker_build/docker_build.bat
@@ -0,0 +1,10 @@
+rm -rf package/*.nupkg
+
+docker build -t emgu/emgucv-ubi-build-nuget .
+
+FOR /F "tokens=* USEBACKQ" %%F IN (`git rev-parse HEAD`) DO (
+SET GIT_HEAD_HASH=%%F
+)
+ECHO %GIT_HEAD_HASH%
+
+docker run --mount type=bind,source=%cd%\package,target=/package emgu/emgucv-ubi-build-nuget:latest bash -c "cd emgucv;git fetch;git reset --hard %GIT_HEAD_HASH%;cd platforms/rhel/9;./cmake_configure;cd build;make;cp /emgucv/platforms/nuget/*.nupkg /package/"
