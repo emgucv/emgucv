7079ccfee737b39294328ab4dd844c0127d4509a
Canming Huang <support@emgu.com>
2024-03-04 08:39:48 -0500

    Added script to demo how to use Emgu.CV.runtime.rhel nuget package

--- platforms/rhel/9/docker_build/docker_build.bat
+++ platforms/rhel/9/docker_build/docker_build.bat
@@ -8,3 +8,5 @@ SET GIT_HEAD_HASH=%%F
 ECHO %GIT_HEAD_HASH%
 
 docker run --mount type=bind,source=%cd%\package,target=/package emgu/emgucv-ubi-build-nuget:latest bash -c "cd emgucv;git fetch;git reset --hard %GIT_HEAD_HASH%;cd platforms/rhel/9;./cmake_configure;cd build;make;cp /emgucv/platforms/nuget/*.nupkg /package/"
+
+cp package/*.nupkg ../../../nuget/
