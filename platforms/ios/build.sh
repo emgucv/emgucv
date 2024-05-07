37ba8823d68db814ec9fbe7be9fa56bf3910d59e
canmingh <canming@brightappsllc.com>
2024-05-07 14:41:04 -0400

    update ios build script

--- platforms/ios/build.sh
+++ platforms/ios/build.sh
@@ -25,7 +25,7 @@ fi
 
 JOB_COUNT=1
 
-if [ \( "$1" != "simulator_arm64" \) -a \( "$1" != "simulator_x86_64" \) ]; then    
+
 #    mkdir -p platforms/ios/iphoneos_armv7s     
 #    cd platforms/ios/iphoneos_armv7s
 #    ../configure_xcode.sh $CV_CONTRIB_OPTION device armv7s ${@:3}
@@ -38,15 +38,16 @@ if [ \( "$1" != "simulator_arm64" \) -a \( "$1" != "simulator_x86_64" \) ]; then
 #    ./xcodebuild_wrapper -parallelizeTargets -jobs ${JOB_COUNT} -configuration Release -target ALL_BUILD build
 #    cd ../../..
 
+if [ "$1" = "device_arm64" ] || [ "$1" = "" ] || [ "$1" = "all" ]; then    
     mkdir -p platforms/ios/iphoneos_arm64
     cd platforms/ios/iphoneos_arm64
     ../configure_xcode.sh $CV_CONTRIB_OPTION device arm64 ${@:3}
     #./xcodebuild_wrapper -parallelizeTargets -jobs ${JOB_COUNT} -configuration Release -target ALL_BUILD build
-    xcodebuild -parallelizeTargets -jobs ${JOB_COUNT} -configuration Release -target ALL_BUILD build
+    xcodebuild ARCHS=arm64 ONLY_ACTIVE_ARCH=NO -parallelizeTargets -jobs ${JOB_COUNT} -configuration Release -target ALL_BUILD build
     cd ../../..
 fi
 
-if [ "$1" = "simulator_arm64" ]; then
+if [ "$1" = "simulator_arm64" ] || [ "$1" = "" ] || [ "$1" = "all" ]; then
     mkdir -p platforms/ios/simulator_arm64
     cd platforms/ios/simulator_arm64
     #skip the first two parameter
@@ -56,19 +57,23 @@ if [ "$1" = "simulator_arm64" ]; then
     cd ../../..
 fi
 
-mkdir -p platforms/ios/simulator_x86_64
-cd platforms/ios/simulator_x86_64
-#skip the first two parameter    
-../configure_xcode.sh $CV_CONTRIB_OPTION simulator x86_64 -DBUILD_IPP_IW:BOOL=FALSE -DWITH_IPP:BOOL=FALSE ${@:3}
-#./xcodebuild_wrapper WARNING_CFLAGS=-Wno-implicit-function-declaration -parallelizeTargets -jobs ${JOB_COUNT} -configuration Release -target ALL_BUILD build
-xcodebuild WARNING_CFLAGS=-Wno-implicit-function-declaration -parallelizeTargets -jobs ${JOB_COUNT} -configuration Release -target ALL_BUILD build
-cd ../../..
+if [ "$1" = "simulator_x86_64" ] || [ "$1" = "" ] || [ "$1" = "all" ]; then
+    mkdir -p platforms/ios/simulator_x86_64
+    cd platforms/ios/simulator_x86_64
+    #skip the first two parameter    
+    ../configure_xcode.sh $CV_CONTRIB_OPTION simulator x86_64 -DBUILD_IPP_IW:BOOL=FALSE -DWITH_IPP:BOOL=FALSE ${@:3}
+    #./xcodebuild_wrapper WARNING_CFLAGS=-Wno-implicit-function-declaration -parallelizeTargets -jobs ${JOB_COUNT} -configuration Release -target ALL_BUILD build
+    xcodebuild ARCHS=x86_64 ONLY_ACTIVE_ARCH=NO WARNING_CFLAGS=-Wno-implicit-function-declaration -parallelizeTargets -jobs ${JOB_COUNT} -configuration Release -target ALL_BUILD build
+    cd ../../..
+fi
 
-cd Emgu.CV.Platform/Xamarin/iOS
-msbuild /p:Configuration=Release
-cd ../../../platforms/ios/simulator_x86_64
-#build the package this time
-#./xcodebuild_wrapper WARNING_CFLAGS=-Wno-implicit-function-declaration -parallelizeTargets -jobs ${JOB_COUNT} -configuration Release -target package build
-xcodebuild WARNING_CFLAGS=-Wno-implicit-function-declaration -parallelizeTargets -jobs ${JOB_COUNT} -configuration Release -target package build
-cd ..
+if [ "$1" = "simulator_x86_64" ] || [ "$1" = "" ] || [ "$1" = "all" ]; then
+    cd Emgu.CV.Platform/Xamarin/iOS
+    msbuild /p:Configuration=Release
+    cd ../../../platforms/ios/simulator_x86_64
+    #build the package this time
+    #./xcodebuild_wrapper WARNING_CFLAGS=-Wno-implicit-function-declaration -parallelizeTargets -jobs ${JOB_COUNT} -configuration Release -target package build
+    xcodebuild WARNING_CFLAGS=-Wno-implicit-function-declaration -parallelizeTargets -jobs ${JOB_COUNT} -configuration Release -target package build
+    cd ..
+fi
 
