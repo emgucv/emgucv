b6f86c9218686d1c206afa0e857d8d43a066fa90
Canming Huang <support@emgu.com>
2024-05-28 16:07:45 -0400

    Remove Xamarin projects.
    # Conflicts:
    #	Emgu.CV.Example/Mac/AppDelegate.cs
    #	Emgu.CV.Example/Mac/Main.cs
    #	Emgu.CV.Example/Mac/NSImageExtension.cs
    #	Emgu.CV.Example/Mac/ViewController.cs

--- platforms/ios/build.sh
+++ platforms/ios/build.sh
@@ -68,8 +68,10 @@ if [ "$1" = "simulator_x86_64" ] || [ "$1" = "" ] || [ "$1" = "all" ]; then
 fi
 
 if [ "$1" = "simulator_x86_64" ] || [ "$1" = "" ] || [ "$1" = "all" ]; then
-    cd Emgu.CV.Platform/Xamarin/iOS
-    msbuild /p:Configuration=Release
+    cd Emgu.CV.Platform/Maui/iOS
+    #msbuild /p:Configuration=Release
+    dotnet restore
+    dotnet build
     cd ../../../platforms/ios/simulator_x86_64
     #build the package this time
     #./xcodebuild_wrapper WARNING_CFLAGS=-Wno-implicit-function-declaration -parallelizeTargets -jobs ${JOB_COUNT} -configuration Release -target package build
