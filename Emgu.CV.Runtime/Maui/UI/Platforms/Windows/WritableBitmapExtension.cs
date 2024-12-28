9fbe8cd5feaf373b82dda53fc8bef7ec266969dd
Canming Huang <support@emgu.com>
2024-12-28 17:32:29 -0500

    Added NetStandard2.0 TargetFramework to Emgu.CV.runtime.maui project.

--- Emgu.CV.Runtime/Maui/UI/Platforms/Windows/WritableBitmapExtension.cs
+++ Emgu.CV.Runtime/Maui/UI/Platforms/Windows/WritableBitmapExtension.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if WINDOWS
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -101,3 +102,4 @@ namespace Emgu.CV.Platform.Maui.UI
     }
 
 }
+#endif
