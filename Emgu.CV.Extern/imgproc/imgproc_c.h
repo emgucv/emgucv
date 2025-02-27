bb2cbf56aae205445eb44e3d8cd8f80d4ba0a06b
Canming Huang <support@emgu.com>
2025-02-27 17:03:11 -0500

    Added flag to cvtColor for faster but not bit-exact implementation

--- Emgu.CV.Extern/imgproc/imgproc_c.h
+++ Emgu.CV.Extern/imgproc/imgproc_c.h
@@ -52,8 +52,18 @@ CVAPI(void) cveCornerHarris(cv::_InputArray* src, cv::_OutputArray* dst, int blo
 CVAPI(double) cveThreshold(cv::_InputArray* src, cv::_OutputArray* dst, double thresh, double maxval, int type);
 CVAPI(void) cveWatershed(cv::_InputArray* image, cv::_InputOutputArray* markers);
 CVAPI(void) cveAdaptiveThreshold(cv::_InputArray* src, cv::_OutputArray* dst, double maxValue, int adaptiveMethod, int thresholdType, int blockSize, double c);
-CVAPI(void) cveCvtColor(cv::_InputArray* src, cv::_OutputArray* dst, int code, int dstCn);
-CVAPI(void) cveCvtColorTwoPlane(cv::_InputArray* src1, cv::_InputArray* src2, cv::_OutputArray* dst, int code);
+CVAPI(void) cveCvtColor(
+	cv::_InputArray* src,
+	cv::_OutputArray* dst,
+	int code,
+	int dstCn,
+	int hint);
+CVAPI(void) cveCvtColorTwoPlane(
+	cv::_InputArray* src1,
+	cv::_InputArray* src2,
+	cv::_OutputArray* dst,
+	int code,
+	int hint);
 CVAPI(void) cveDemosaicing(cv::_InputArray* src, cv::_OutputArray* dst, int code, int dstCn);
 CVAPI(void) cveCopyMakeBorder(cv::_InputArray* src, cv::_OutputArray* dst, int top, int bottom, int left, int right, int borderType, CvScalar* value);
 
---
+++ Emgu.CV/PInvoke/CvEnum/AlgorithmHint.cs
@@ -0,0 +1,29 @@
+//----------------------------------------------------------------------------
+//  Copyright (C) 2004-2025 by EMGU Corporation. All rights reserved.       
+//----------------------------------------------------------------------------
+
+using System;
+
+namespace Emgu.CV.CvEnum
+{
+    /// <summary>
+    /// Flags that allow to modify some functions behavior. Used as set of flags.
+    /// </summary>
+    public enum AlgorithmHint
+    {
+        /// <summary>
+        /// Default algorithm behaviour defined during OpenCV build
+        /// </summary>
+        Default = 0,
+        /// <summary>
+        /// Use generic portable implementation
+        /// </summary>
+        Accurate = 1,
+        /// <summary>
+        /// Allow alternative approximations to get faster implementation. Behaviour and result depends on a platform
+        /// </summary>
+        Approx = 2,
+
+    }
+
+}
