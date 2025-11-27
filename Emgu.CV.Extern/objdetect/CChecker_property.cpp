c51c566c640ae09a5c9c3dfb1a92f32e4fd27a39
Canming Huang <support@emgu.com>
2025-11-27 15:24:43 -0500

    Delete files from repo. They are generated.

--- Emgu.CV.Extern/objdetect/CChecker_property.cpp
+++
@@ -1,9 +0,0 @@
- 
-   #include "objdetect/CChecker_property.h"
-cv::mcc::ColorChart cveCCheckerGetTarget(void* obj) { CV_Error(cv::Error::StsBadFunc, "This function is not implemented in the current platform"); }
-void cveCCheckerSetTarget(void* obj, cv::mcc::ColorChart value) { CV_Error(cv::Error::StsBadFunc, "This function is not implemented in the current platform"); }     
-     
-float cveCCheckerGetCost(void* obj) { CV_Error(cv::Error::StsBadFunc, "This function is not implemented in the current platform"); }
-void cveCCheckerSetCost(void* obj, float value) { CV_Error(cv::Error::StsBadFunc, "This function is not implemented in the current platform"); }     
-      
-  
--- Emgu.CV.Extern/objdetect/CChecker_property.h
+++
@@ -1,7 +0,0 @@
-#include "mcc_c.h"
-CVAPI(cv::mcc::ColorChart) cveCCheckerGetTarget(void* obj);
-CVAPI(void) cveCCheckerSetTarget(void* obj, cv::mcc::ColorChart value);     
-     
-CVAPI(float) cveCCheckerGetCost(void* obj);
-CVAPI(void) cveCCheckerSetCost(void* obj, float value);     
-     
