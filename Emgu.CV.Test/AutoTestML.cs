89ff44591850b1a93fece09fa7346b746ee20dae
Canming Huang <support@emgu.com>
2025-11-30 17:01:55 -0500

    Fix unit test compilation.

--- Emgu.CV.Test/AutoTestML.cs
+++ Emgu.CV.Test/AutoTestML.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
-using System.Drawing.Imaging.Effects;
+//using System.Drawing.Imaging.Effects;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
