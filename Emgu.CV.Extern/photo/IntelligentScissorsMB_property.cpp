d3877ed1ae49e2c0b1db473cabd49171efbe1bd1
Canming Huang <support@emgu.com>
2026-06-10 16:14:37 -0400

    Remove two source files, they are generated.

--- Emgu.CV.Extern/photo/IntelligentScissorsMB_property.cpp
+++
@@ -1,7 +0,0 @@
- 
-   #include "photo/IntelligentScissorsMB_property.h"
-void cveIntelligentScissorsMBSetEdgeFeatureZeroCrossingParameters(cv::segmentation::IntelligentScissorsMB* obj, float value) { obj->setEdgeFeatureZeroCrossingParameters( value ); }     
-     
-void cveIntelligentScissorsMBSetGradientMagnitudeMaxLimit(cv::segmentation::IntelligentScissorsMB* obj, float value) { obj->setGradientMagnitudeMaxLimit( value ); }     
-      
-  
